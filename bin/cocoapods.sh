50c97ad5423d00ca4a532ce547ca0930e8153314
Salakar <mike.diarmid@gmail.com>
2017-03-17 14:57:22 +0000

    misc build script changes

--- bin/cocoapods.sh
+++ bin/cocoapods.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-## https://github.com/auth0/react-native-lock/blob/master/bin/cocoapods.sh
-
 ios_dir=`pwd`/ios
 if [ -d ios_dir ]
   then
@@ -9,9 +7,11 @@ if [ -d ios_dir ]
 fi
 
 podfile="$ios_dir/Podfile"
-template=`pwd`/node_modules/react-native-firebase/ios/Podfile.template
+template=`pwd`/node_modules/react-native-firebase/Podfile.template
 
-echo "Checking Podfile in iOS project ($podfile)"
+project_name=$(node -pe "require('./package.json').name")
+
+echo "Checking Podfile in iOS project $project_name ($podfile)"
 
 if [ -f $podfile ]
   then
@@ -21,22 +21,6 @@ if [ -f $podfile ]
 
   if [ "$generate_env_file" != "y" ]
     then
-
-    # pod outdated | grep "The following pod updates are available:"
-    # status=$?
-    # if [ $status -eq 0 ]; then
-    #   echo "From what I can tell, there look to be updates available..."
-    #   echo "Do you want to update your cocoapods? [N/y]"
-    #   read update_pods
-    #   if [ "$update_pods" != "y" ]
-    #     then
-    #       pod update --project-directory=ios
-    #       exit 0
-    #     else
-    #       exit 0
-    #   fi
-    # fi
-
     echo "Add the following pods":
     echo ""
     echo ""
@@ -53,10 +37,26 @@ fi
 
 echo "Adding Podfile to iOS project"
 
-cd ios
-pod init >/dev/null 2>&1
-cat $template >> $podfile
-cd ..
+touch ios/Podfile
+cat >ios/Podfile <<EOL
+target '${project_name}' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+  # Pods for '${project_name}'
+end
+
+# RNFirebase
+pod 'Firebase/Auth'
+pod 'Firebase/Analytics'
+pod 'Firebase/AppIndexing'
+pod 'Firebase/Core'
+pod 'Firebase/Crash'
+pod 'Firebase/Database'
+pod 'Firebase/DynamicLinks'
+pod 'Firebase/Messaging'
+pod 'Firebase/RemoteConfig'
+pod 'Firebase/Storage'
+EOL
 
 echo "Installing Pods"
 
