66df0b0463634dc5f574757e0612950ec4dba37c
Elliot Hesp <elliot.hesp@gmail.com>
2017-05-25 10:39:54 +0100

    [pref][ios] Implement performance monitoring

---
+++ ios/RNFirebase/RNFirebasePerformance.h
@@ -0,0 +1,18 @@
+#ifndef RNFirebasePerformance_h
+#define RNFirebasePerformance_h
+
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else // Compatibility for RN version < 0.40
+#import "RCTBridgeModule.h"
+#endif
+
+@interface RNFirebasePerformance : NSObject <RCTBridgeModule> {
+
+}
+
+@property NSMutableDictionary *traces;
+
+@end
+
+#endif
