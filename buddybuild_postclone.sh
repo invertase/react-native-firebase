524d00d1b3548dbc192c96e0ce7d079b1ff2d87d
Salakar <mike.diarmid@gmail.com>
2017-06-22 14:35:23 +0100

    [build/ci] update buddybuild postclone script & build tools version

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Copy firebase lib into tests directory
-cp -R $(node --eval "console.log(require('path').resolve('./lib'));")/* $(node --eval "console.log(require('path').resolve('./tests/firebase'));")/
+mkdir tests/firebase
+cp -R lib/* tests/firebase/*
 
 # Install /tests npm packages
 cd tests && npm install
