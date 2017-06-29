e4c61f61da6df269e6622c8e54cdc530d9d0309c
nico1510 <Nicolas-Beck@gmx.de>
2017-06-29 19:34:15 +0200

    removed unused import

--- android/src/main/java/io/invertase/firebase/storage/RNFirebaseStorage.java
+++ android/src/main/java/io/invertase/firebase/storage/RNFirebaseStorage.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
