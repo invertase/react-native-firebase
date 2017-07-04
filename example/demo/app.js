0fbbae031a547146d4efe2d616d49a4fcf86891d
ugiacoman <ulises.giacoman@forrent.com>
2017-07-04 18:57:25 -0400

    Add app.js

---
+++ example/demo/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <View
+        style={{flex: 1, backgroundColor: 'red'}}
+      />
+    )
+  }
+}
