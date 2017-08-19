aebd1835bc975290aed39919efe48ec313185dc0
Salakar <mike.diarmid@gmail.com>
2017-08-19 05:46:27 +0100

    [tests] switch index.android back to main.js

--- tests/index.android.js
+++ tests/index.android.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import bootstrap from './src/bench';
+import bootstrap from './src/main';
 
 
 AppRegistry.registerComponent('ReactNativeFirebaseDemo', () => bootstrap);
