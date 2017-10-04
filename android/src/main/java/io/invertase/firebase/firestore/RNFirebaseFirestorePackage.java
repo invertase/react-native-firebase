25314483d740f90742d642b61bd015aa3456604c
Salakar <mike.diarmid@gmail.com>
2017-10-04 19:17:41 +0100

    Revert "[android] Remove backwards compatibility for 0.47.0"
    This reverts commit f68c6d168e87e8b2128f65be7d89915d62d6a6a5.

--- android/src/main/java/io/invertase/firebase/firestore/RNFirebaseFirestorePackage.java
+++ android/src/main/java/io/invertase/firebase/firestore/RNFirebaseFirestorePackage.java
@@ -29,6 +29,18 @@ public class RNFirebaseFirestorePackage implements ReactPackage {
   }
 
   /**
+   * @return list of JS modules to register with the newly created catalyst instance.
+   * <p/>
+   * IMPORTANT: Note that only modules that needs to be accessible from the native code should be
+   * listed here. Also listing a native module here doesn't imply that the JS implementation of it
+   * will be automatically included in the JS bundle.
+   */
+  // TODO: Removed in 0.47.0. Here for backwards compatability
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
+  /**
    * @param reactContext
    * @return a list of view managers that should be registered with {@link UIManagerModule}
    */
