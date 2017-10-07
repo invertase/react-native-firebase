1d2549556b2deaab914d650dfeaa644c2a4a6e48
Salakar <mike.diarmid@gmail.com>
2017-10-07 01:53:05 +0100

    [tests][android] add perf package back into initialized modules

--- tests/android/app/src/main/java/com/reactnativefirebasedemo/MainApplication.java
+++ tests/android/app/src/main/java/com/reactnativefirebasedemo/MainApplication.java
@@ -12,6 +12,7 @@ import io.invertase.firebase.crash.RNFirebaseCrashPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.perf.RNFirebasePerformancePackage;
 import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -44,7 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
           new RNFirebaseDatabasePackage(),
           new RNFirebaseFirestorePackage(),
           new RNFirebaseMessagingPackage(),
-          // new RNFirebasePerformancePackage(),
+          new RNFirebasePerformancePackage(),
           new RNFirebaseStoragePackage()
       );
     }
