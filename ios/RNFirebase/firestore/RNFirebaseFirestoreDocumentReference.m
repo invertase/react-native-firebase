403a7eefa0108113b74e59d9e3b5fbf404cde669
Chris Bianca <me@chrisbianca.com>
2017-10-24 09:09:48 +0100

    [firestore] Use ms instead of ISOString for date conversion

--- ios/RNFirebase/firestore/RNFirebaseFirestoreDocumentReference.m
+++ ios/RNFirebase/firestore/RNFirebaseFirestoreDocumentReference.m
@@ -205,9 +205,9 @@ static NSMutableDictionary *_listeners;
         typeMap[@"value"] = geopoint;
     } else if ([value isKindOfClass:[NSDate class]]) {
         typeMap[@"type"] = @"date";
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
-        typeMap[@"value"] = [dateFormatter stringFromDate:(NSDate *)value];
+        // NOTE: The round() is important as iOS ends up giving .999 otherwise,
+        // and loses a millisecond when going between native and JS
+        typeMap[@"value"] = @(round([(NSDate *)value timeIntervalSince1970] * 1000.0));
     } else if ([value isKindOfClass:[NSNumber class]]) {
         NSNumber *number = (NSNumber *)value;
         if (number == (void*)kCFBooleanFalse || number == (void*)kCFBooleanTrue) {
@@ -262,9 +262,7 @@ static NSMutableDictionary *_listeners;
         NSNumber *longitude = geopoint[@"longitude"];
         return [[FIRGeoPoint alloc] initWithLatitude:[latitude doubleValue] longitude:[longitude doubleValue]];
     } else if ([type isEqualToString:@"date"]) {
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
-        return [dateFormatter dateFromString:value];
+        return [NSDate dateWithTimeIntervalSince1970:([(NSNumber *)value doubleValue] / 1000.0)];
     } else if ([type isEqualToString:@"fieldvalue"]) {
         NSString *string = (NSString*)value;
         if ([string isEqualToString:@"delete"]) {
