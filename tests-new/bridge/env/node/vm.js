e19e87b0aaace977f2f127c7f74d273375ef1c01
Salakar <mike.diarmid@gmail.com>
2018-03-26 19:47:54 +0100

    [tests] misc type coercion

--- tests-new/bridge/env/node/vm.js
+++ tests-new/bridge/env/node/vm.js
@@ -91,7 +91,7 @@ module.exports = {
 
       case EXECUTE: {
         const script = await getBundle(request);
-        if (global.bridge.context == null) {
+        if (global.bridge.context === null) {
           throw new Error('VM context was not prepared.');
         }
         if (request.inject) {
@@ -109,7 +109,7 @@ module.exports = {
         let returnValue = [[], [], [], 0];
         try {
           if (
-            global.bridge.context != null &&
+            global.bridge.context !== null &&
             typeof global.bridge.context.__fbBatchedBridge === 'object'
           ) {
             returnValue = global.bridge.context.__fbBatchedBridge[method].apply(
