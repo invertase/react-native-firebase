87e822cb53049c2fb038bfd0926c4542e1e8bdf0
Salakar <mike.diarmid@gmail.com>
2018-03-26 20:13:51 +0100

    [tests] make detox + mocha optional peer deps

--- tests-new/bridge/env/node/index.js
+++ tests-new/bridge/env/node/index.js
@@ -1,72 +1,81 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable no-param-reassign,global-require */
 global.bridge = {};
 require('./source-map');
-
-const detox = require('detox');
 const ws = require('./ws');
 const ready = require('./ready');
 const coverage = require('./coverage');
 
-/* ---------------------
- *   DEVICE OVERRIDES
- * --------------------- */
+let detox;
+try {
+  detox = require('detox');
+} catch (e) {
+  // ignore
+}
 
-let device;
-Object.defineProperty(global, 'device', {
-  get() {
-    return device;
-  },
-  set(originalDevice) {
-    // device.reloadReactNative({ ... })
-    // todo detoxOriginalReloadReactNative currently broken it seems
-    // const detoxOriginalReloadReactNative = originalDevice.reloadReactNative.bind(originalDevice);
-    originalDevice.reloadReactNative = async () => {
-      ready.reset();
-      global.bridge.reload();
-      return ready.wait();
-    };
+if (detox) {
+  /* ---------------------
+   *   DEVICE OVERRIDES
+   * --------------------- */
 
-    // device.launchApp({ ... })
-    const detoxOriginalLaunchApp = originalDevice.launchApp.bind(
-      originalDevice
-    );
-    originalDevice.launchApp = async (...args) => {
-      ready.reset();
-      await detoxOriginalLaunchApp(...args);
-      return ready.wait();
-    };
+  let device;
+  Object.defineProperty(global, 'device', {
+    get() {
+      return device;
+    },
+    set(originalDevice) {
+      // device.reloadReactNative({ ... })
+      // todo detoxOriginalReloadReactNative currently broken it seems
+      // const detoxOriginalReloadReactNative = originalDevice.reloadReactNative.bind(originalDevice);
+      originalDevice.reloadReactNative = async () => {
+        ready.reset();
+        global.bridge.reload();
+        return ready.wait();
+      };
 
-    device = originalDevice;
-    return originalDevice;
-  },
-});
+      // device.launchApp({ ... })
+      const detoxOriginalLaunchApp = originalDevice.launchApp.bind(
+        originalDevice
+      );
+      originalDevice.launchApp = async (...args) => {
+        ready.reset();
+        await detoxOriginalLaunchApp(...args);
+        return ready.wait();
+      };
 
-/* -------------------
- *   DETOX OVERRIDES
- * ------------------- */
+      device = originalDevice;
+      return originalDevice;
+    },
+  });
 
-// detox.init()
-const detoxOriginalInit = detox.init.bind(detox);
-detox.init = async (...args) => {
-  ready.reset();
-  await detoxOriginalInit(...args);
-  return ready.wait();
-};
+  /* -------------------
+   *   DETOX OVERRIDES
+   * ------------------- */
 
-// detox.cleanup()
-const detoxOriginalCleanup = detox.cleanup.bind(detox);
-detox.cleanup = async (...args) => {
-  try {
-    ws.close();
-  } catch (e) {
-    // do nothing
-  }
-  await detoxOriginalCleanup(...args);
-};
+  // detox.init()
+  const detoxOriginalInit = detox.init.bind(detox);
+  detox.init = async (...args) => {
+    ready.reset();
+    await detoxOriginalInit(...args);
+    return ready.wait();
+  };
+
+  // detox.cleanup()
+  const detoxOriginalCleanup = detox.cleanup.bind(detox);
+  detox.cleanup = async (...args) => {
+    try {
+      ws.close();
+    } catch (e) {
+      // do nothing
+    }
+    await detoxOriginalCleanup(...args);
+  };
+}
 
 // setup after hook to ensure final context coverage is captured
 process.nextTick(() => {
-  after(() => {
-    coverage.collect();
-  });
+  if (global.after) {
+    after(() => {
+      coverage.collect();
+    });
+  }
 });
