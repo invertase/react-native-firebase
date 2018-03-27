dd940b953bfd641f9fe1b5645c8d3fb166e52df5
Chris Bianca <me@chrisbianca.com>
2018-03-27 17:31:25 +0100

    [firestore] Add support for enable / disable network

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1149,7 +1149,9 @@ declare module "react-native-firebase" {
         readonly app: App;
         batch(): WriteBatch;
         collection(collectionPath: string): CollectionReference;
+        disableNetwork(): Promise<void>
         doc(documentPath: string): DocumentReference;
+        enableNetwork(): Promise<void>
 
         /** NOT SUPPORTED YET */
         // enablePersistence(): Promise<void>;
