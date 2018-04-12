a3f5b01d6735a5cf9215b01f47744e3bb0333b33
Salakar <mike.diarmid@gmail.com>
2018-04-12 12:54:29 +0100

    [tests][firestore] start of migrating firestore tests

---
+++ bridge/e2e/firestore/documentSnapshot.e2e.js
@@ -0,0 +1,42 @@
+describe.only('firestore()', () => {
+  describe('DocumentSnapshot', () => {
+    before(async () => {
+      await TestHelpers.firestore.resetTestCollectionDoc();
+    });
+
+    describe('get()', () => {
+      it('using a dot notated path string', async () => {
+        const { testCollectionDoc } = TestHelpers.firestore;
+        const snapshot = await testCollectionDoc().get();
+
+        should.equal(snapshot.get('foo'), 'bar');
+        should.equal(snapshot.get('object.daz'), 123);
+        should.equal(snapshot.get('nonexistent.object'), undefined);
+      });
+
+      it('using a FieldPath instance', async () => {
+        const { testCollectionDoc } = TestHelpers.firestore;
+        const snapshot = await testCollectionDoc().get();
+
+        should.equal(snapshot.get('foo'), 'bar');
+
+        should.equal(
+          snapshot.get(new firebase.firestore.FieldPath('foo')),
+          'bar'
+        );
+
+        should.equal(
+          snapshot.get(new firebase.firestore.FieldPath('object', 'daz')),
+          123
+        );
+
+        should.equal(
+          snapshot.get(
+            new firebase.firestore.FieldPath('nonexistent', 'object')
+          ),
+          undefined
+        );
+      });
+    });
+  });
+});
