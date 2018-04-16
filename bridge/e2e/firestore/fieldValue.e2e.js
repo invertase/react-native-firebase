c03993503b8f1ef4d4bb87d85ed0b9c9eb0dd3b0
Salakar <mike.diarmid@gmail.com>
2018-04-16 17:47:50 +0100

    [tests][firestore] remove logging

--- bridge/e2e/firestore/fieldValue.e2e.js
+++ bridge/e2e/firestore/fieldValue.e2e.js
@@ -15,17 +15,14 @@ describe('firestore()', () => {
       it('should delete a field', async () => {
         const { data } = await testCollectionDoc(DOC_2_PATH).get();
         should.equal(data().title, DOC_2.title);
-        console.log('after 1st get');
 
         await testCollectionDoc(DOC_2_PATH).update({
           title: firebase.firestore.FieldValue.delete(),
         });
-        console.log('after update');
 
         const { data: dataAfterUpdate } = await testCollectionDoc(
           DOC_2_PATH
         ).get();
-        console.log('after 2nd get');
 
         should.equal(dataAfterUpdate().title, undefined);
       });
@@ -35,17 +32,14 @@ describe('firestore()', () => {
       it('should set timestamp', async () => {
         const { data } = await testCollectionDoc(DOC_2_PATH).get();
         should.equal(data().creationDate, undefined);
-        console.log('after 1st get');
 
         await testCollectionDoc(DOC_2_PATH).update({
           creationDate: firebase.firestore.FieldValue.serverTimestamp(),
         });
-        console.log('after update');
 
         const { data: dataAfterUpdate } = await testCollectionDoc(
           DOC_2_PATH
         ).get();
-        console.log('after 2nd get');
 
         dataAfterUpdate().creationDate.should.be.instanceof(
           bridge.context.window.Date
