a9ab3227a8c5ef73313078a30042e7a0e136e001
Salakar <mike.diarmid@gmail.com>
2018-05-05 18:32:56 +0100

    [tests][functions] update cloud function to support primitive types of data and return a string value of the type that was sent

--- bridge/functions/index.js
+++ bridge/functions/index.js
@@ -3,6 +3,30 @@ const functions = require('firebase-functions');
 const TEST_DATA = require('./test-data');
 
 exports.runTest = functions.https.onCall(data => {
+  if (typeof data === 'undefined') {
+    return 'undefined';
+  }
+
+  if (typeof data === 'string') {
+    return 'string';
+  }
+
+  if (typeof data === 'number') {
+    return 'number';
+  }
+
+  if (typeof data === 'boolean') {
+    return 'boolean';
+  }
+
+  if (data === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(data)) {
+    return 'array';
+  }
+
   const { type, asError, inputData } = data;
   if (!Object.hasOwnProperty.call(TEST_DATA, type)) {
     throw new functions.https.HttpsError(
