96783d46ebda9f3480e45c3eaef0b4f0d22b26af
Case Taintor <case.taintor@klarna.com>
2018-06-18 15:59:21 +0200

    adds getToken(string,string) and deleteToken(string,string) to typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1088,6 +1088,8 @@ declare module 'react-native-firebase' {
       interface InstanceId {
         delete(): Promise<void>;
         get(): Promise<string>;
+        getToken(authorizedEntity: string, scope: string): Promise<string>;
+        deleteToken(authorizedEntity: string, scope: string): Promise<void>;
       }
     }
 
