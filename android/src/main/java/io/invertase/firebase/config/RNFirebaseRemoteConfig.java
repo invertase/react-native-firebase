54743d945d6fbc1a76550efe792386e225f5d5e6
Elliot Hesp <elliot.hesp@gmail.com>
2018-07-12 13:28:58 +0100

    [config][android] Explicitly handle task exception

--- android/src/main/java/io/invertase/firebase/config/RNFirebaseRemoteConfig.java
+++ android/src/main/java/io/invertase/firebase/config/RNFirebaseRemoteConfig.java
@@ -16,6 +16,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigFetchThrottledException;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigValue;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
@@ -121,7 +122,11 @@ class RNFirebaseRemoteConfig extends ReactContextBaseJavaModule {
           if (task.isSuccessful()) {
             promise.resolve("remoteConfigFetchStatusSuccess");
           } else {
-            promise.reject("config/failure", task.getException().getMessage(), task.getException());
+            if (task.getException() instanceof FirebaseRemoteConfigFetchThrottledException) {
+              promise.reject("config/throttled", "fetch() operation cannot be completed successfully, due to throttling.", task.getException());
+            } else {
+              promise.reject("config/failure", "fetch() operation cannot be completed successfully.", task.getException());
+            }
           }
         }
       });
