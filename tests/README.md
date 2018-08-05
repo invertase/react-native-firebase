998376773f9c4a063c253c0021e22e440925009a
Salakar <mike.diarmid@gmail.com>
2018-08-05 02:20:26 +0100

    remove old tests app - no longer in use / migrated to bridge

--- tests/README.md
+++
@@ -1,1 +0,0 @@
-See https://rnfirebase.io/docs/master/testing
--- tests/src/tests/admob/index.js
+++
@@ -1,9 +0,0 @@
-import firebase from '../../firebase';
-import TestSuite from '../../../lib/TestSuite';
-import admobTests from './admob';
-
-const suite = new TestSuite('AdMob', 'firebase.admob()', firebase);
-
-suite.addTests(admobTests);
-
-export default suite;
