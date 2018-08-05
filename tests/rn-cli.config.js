2cce62361207e4c2b43393f81d0823dff47b5a73
Salakar <mike.diarmid@gmail.com>
2018-08-05 02:20:26 +0100

    remove old tests app - no longer in use / migrated to bridge

--- tests/rn-cli.config.js
+++
@@ -1,7 +0,0 @@
-const blacklist = require('metro/src/blacklist');
-
-module.exports = {
-  getBlacklistRE() {
-    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
-  },
-};
--- tests/src/reducers/focusedTestIdsReducers.js
+++
@@ -1,9 +0,0 @@
-import { initialState } from '../tests/index';
-
-const initState = initialState();
-
-function focusedTestIdsReducers(state = initState.focusedTestIds): State {
-  return state;
-}
-
-export default focusedTestIdsReducers;
