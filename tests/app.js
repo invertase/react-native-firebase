d5eab4d39216d4308c9c5fc62b7fcdd1a1e97881
Salakar <mike.diarmid@gmail.com>
2018-08-25 13:40:25 +0100

    add tests project

---
+++ tests/app.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/extensions,import/no-unresolved */
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+import bridge from 'jet/platform/react-native';
+import firebase from 'react-native-firebase';
+
+require('sinon');
+require('should-sinon');
+require('should');
+
+class Root extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+
+    bridge.setBridgeProperty('module', firebase);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text testID="messageText">{this.state.message}</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('testing', () => Root);
