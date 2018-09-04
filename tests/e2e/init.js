3e9295e6073b8c418081970ded12c81e6a4467f7
Salakar <mike.diarmid@gmail.com>
2018-09-04 18:22:48 +0100

    [build][ci] use different mocha.opts file in CI

--- tests/e2e/init.js
+++ tests/e2e/init.js
@@ -1,6 +1,12 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
 
+if (process.env.CIRCLECI) {
+  config['runner-config'] = 'e2e/mocha.ci.opts';
+} else {
+  config['runner-config'] = 'e2e/mocha.local.opts';
+}
+
 before(async () => {
   await detox.init(config);
   // needs to be called before any usage of firestore
@@ -12,6 +18,6 @@ after(async () => {
   console.log('Cleaning up...');
   await TestHelpers.firestore.cleanup();
   console.log('Firestore cleaned up...');
-  await detox.cleanup();
+  // await detox.cleanup(); // TODO hangs - most likely jet internals interfering
   console.log('Detox cleaned up...');
 });
