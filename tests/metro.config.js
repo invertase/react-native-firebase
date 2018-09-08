77636a99dae6382cde1dd4644a375be52672a424
Salakar <mike.diarmid@gmail.com>
2018-09-08 22:26:11 +0100

    [tests][build] cleanup metro config

--- tests/metro.config.js
+++ tests/metro.config.js
@@ -1,31 +1,29 @@
 const { resolve, join } = require('path');
 const { createBlacklist } = require('metro');
-
 const { mergeConfig } = require('metro-config');
 const { DEFAULT } = require('react-native/local-cli/util/Config');
+
 // https://github.com/facebook/react-native/blob/master/local-cli/core/Constants.js
 // https://github.com/facebook/react-native/blob/master/local-cli/util/Config.js
-
-const extraNodeModulesGetter = {
-  get: (target, name) => join(__dirname, `node_modules/${name}`),
-};
 
 const config = {
   resolver: {
     blackListRE: createBlacklist([
       new RegExp(`^${escape(resolve(__dirname, '..', 'node_modules'))}\\/.*$`),
     ]),
-    extraNodeModules: new Proxy({}, extraNodeModulesGetter),
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => join(__dirname, `node_modules/${name}`),
+      }
+    ),
   },
   watchFolders: [resolve(__dirname, '../src')],
-  transformer: {
-    // TODO
-  },
-  serializer: {
-    getModulesRunBeforeMainModule: () => [
-      // TODO inject Jet
-    ],
-  },
+  // serializer: {
+  //   getModulesRunBeforeMainModule: () => [
+  //     require.resolve('react-native/Libraries/Core/InitializeCore'),
+  //   ],
+  // },
 };
 
 module.exports = mergeConfig(DEFAULT, config);
