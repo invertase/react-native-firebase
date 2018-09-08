4ae88e3055f02fcff0792b8c25f0597f8937adcb
Salakar <mike.diarmid@gmail.com>
2018-09-08 22:53:00 +0100

    [tests][build] cleanup babel config

--- tests/babel.config.js
+++ tests/babel.config.js
@@ -9,17 +9,13 @@ module.exports = {
         alias: {
           // import library code from parent
           'react-native-firebase': resolve(__dirname, '../src'),
-
-          // TODO - errors without this =/ rn57 not correctly providing babelHelpers?
-          '@babel/runtime': resolve(__dirname, 'node_modules/@babel/runtime'),
         },
         extensions: ['.js', '.ts', '.tsx', '.css'],
-        root: ['./'],
       },
     ],
     // TODO - errors without this =/ rn57 not correctly providing babelHelpers?
     [
-      require('@babel/plugin-transform-runtime'),
+      'module:@babel/plugin-transform-runtime',
       {
         helpers: true,
         regenerator: true,
