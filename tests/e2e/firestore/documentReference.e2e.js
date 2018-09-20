c75b722c5373d10fbb5aa17bb8c71579be276d8c
Salakar <mike.diarmid@gmail.com>
2018-09-21 00:13:05 +0100

    [tests][firestore] add document SnapshotError tests

--- tests/e2e/firestore/documentReference.e2e.js
+++ tests/e2e/firestore/documentReference.e2e.js
@@ -4,9 +4,13 @@ const {
   COL2_DOC_1_ID,
   cleanCollection,
   COL2_DOC_1_PATH,
-  TEST_COLLECTION_NAME_DYNAMIC,
   resetTestCollectionDoc,
+  TEST_COLLECTION_NAME_DYNAMIC,
 } = TestHelpers.firestore;
+
+function getSnapshotErrorClass() {
+  return jet.require('src/modules/firestore/SnapshotError');
+}
 
 describe('firestore()', () => {
   describe('DocumentReference', () => {
@@ -131,6 +135,34 @@ describe('firestore()', () => {
         await sleep(50);
         await cleanCollection(TEST_COLLECTION_NAME_DYNAMIC);
         await sleep(50);
+      });
+
+      it('snapshot error returns instance of SnapshotError', () => {
+        let unsubscribe;
+        const { reject, resolve, promise } = Promise.defer();
+        const collection = firebase.firestore().doc('blocked-collection/nope');
+
+        const observer = {
+          next: () => {
+            unsubscribe();
+            reject(new Error('Did not error!'));
+          },
+          error: snapshotError => {
+            snapshotError.should.be.instanceOf(getSnapshotErrorClass());
+            snapshotError.code.should.be.a.String();
+            snapshotError.path.should.be.a.String();
+            snapshotError.appName.should.be.a.String();
+            snapshotError.message.should.be.a.String();
+            snapshotError.nativeErrorMessage.should.be.a.String();
+            snapshotError.appName.should.equal('[DEFAULT]');
+            snapshotError.path.should.equal('blocked-collection/nope');
+            snapshotError.code.should.equal('firestore/permission-denied');
+            resolve();
+          },
+        };
+
+        unsubscribe = collection.onSnapshot(observer);
+        return promise;
       });
 
       it('calls callback with the initial data and then when value changes', async () => {
