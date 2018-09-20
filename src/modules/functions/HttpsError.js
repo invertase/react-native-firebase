6dcba98e145e3d6bc1ecf22bd0d6c044e06a9157
Salakar <mike.diarmid@gmail.com>
2018-09-21 00:03:00 +0100

    [functions][js] HttpError can now correctly extend Error

--- src/modules/functions/HttpsError.js
+++ src/modules/functions/HttpsError.js
@@ -1,7 +1,6 @@
 import type { FunctionsErrorCode } from './types.flow';
 
-export default class HttpsError {
-  // TODO extends Error
+export default class HttpsError extends Error {
   +details: ?any;
 
   +message: string;
@@ -9,14 +8,9 @@ export default class HttpsError {
   +code: FunctionsErrorCode;
 
   constructor(code: FunctionsErrorCode, message?: string, details?: any) {
-    // this.code = code;
-    // this.details = details;
-    // this.message = message;
-    // TODO babel 7 issue... can't extend builtin classes properly.
-    this._error = new Error(message);
-    this._error.code = code;
-    this._error.details = details;
-    this._error.constructor = HttpsError;
-    return this._error;
+    super(message);
+    this.code = code;
+    this.details = details;
+    this.message = message;
   }
 }
