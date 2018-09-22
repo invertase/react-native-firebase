04fb9e003f4712a3d06cc0830a663ef68bdfec2c
Salakar <mike.diarmid@gmail.com>
2018-09-22 20:31:55 +0100

    [tests] add in app ui for current test status + enable test retrying

--- tests/e2e/init.js
+++ tests/e2e/init.js
@@ -8,6 +8,31 @@ before(async () => {
   // await firebase.firestore().settings({ persistence: false });
 });
 
+beforeEach(async function beforeEach() {
+  if (jet.context && jet.root && jet.root.setState) {
+    jet.root.setState({
+      currentTest: this.currentTest,
+    });
+  }
+
+  const retry = this.currentTest.currentRetry();
+
+  if (retry > 0) {
+    if (retry === 1) {
+      console.log('');
+      console.warn(`⚠️ A test failed:`);
+      console.warn(`️   ->  ${this.currentTest.title}`);
+    }
+
+    if (retry > 1) {
+      console.warn(`   🔴  Retry #${retry - 1} failed...`);
+    }
+
+    console.warn(`️   ->  Retrying... (${retry})`);
+    await sleep(3000);
+  }
+});
+
 after(async () => {
   console.log('Cleaning up...');
   await TestHelpers.firestore.cleanup();
