fe53b42a16858b093ed915286cd719a6ec674924
Salakar <mike.diarmid@gmail.com>
2018-10-16 18:14:32 +0100

    [ios][notifications] fix completion crash: #1576

--- ios/RNFirebase/converters/RCTConvert+UIBackgroundFetchResult.m
+++ ios/RNFirebase/converters/RCTConvert+UIBackgroundFetchResult.m
@@ -1,9 +1,16 @@
 #import "RCTConvert+UIBackgroundFetchResult.h"
 
 @implementation RCTConvert (UIBackgroundFetchResult)
-RCT_ENUM_CONVERTER(UIBackgroundFetchResult, (@{ @"backgroundFetchResultNoData" : @(UIBackgroundFetchResultNoData),
-                                                @"backgroundFetchResultNewData" : @(UIBackgroundFetchResultNewData),
-                                                @"backgroundFetchResultFailed" : @(UIBackgroundFetchResultFailed)}),
-                   UIBackgroundFetchResultNoData, integerValue)
+
+RCT_ENUM_CONVERTER(
+    UIBackgroundFetchResult,
+    (@{
+        @"backgroundFetchResultNoData" : @(UIBackgroundFetchResultNoData),
+        @"backgroundFetchResultNewData" : @(UIBackgroundFetchResultNewData),
+        @"backgroundFetchResultFailed" : @(UIBackgroundFetchResultFailed)}
+    ),
+    UIBackgroundFetchResultNoData,
+    integerValue
+)
 
 @end
