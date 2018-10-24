af80dcecbadff48bc096cd7d80b59fa9eb57686c
Tim Wang <timwangdev@gmail.com>
2018-10-24 14:48:15 +0800

    code style

--- src/modules/messaging/IOSMessaging.js
+++ src/modules/messaging/IOSMessaging.js
@@ -10,9 +10,9 @@ export default class IOSMessaging {
   }
 
   getAPNSToken(): Promise<string | null> {
-    if (isIOS) {
-      return getNativeModule(this._messaging).getAPNSToken();
+    if (!isIOS) {
+      return null;
     }
-    return null;
+    return getNativeModule(this._messaging).getAPNSToken();
   }
 }
