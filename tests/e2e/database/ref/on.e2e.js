97a7b0c25d3d610c59167891d398a6b2d6e3b313
rakannimer <rakannimer@gmail.com>
2018-11-17 08:16:32 +0200

    Apply Salakar's comments. Add child_added 1st test.

--- tests/e2e/database/ref/on.e2e.js
+++ tests/e2e/database/ref/on.e2e.js
@@ -1,31 +1,32 @@
 const { CONTENTS, setDatabaseContents } = TestHelpers.database;
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const waitForValueAtRef = ref =>
+  new Promise(resolve => {
+    const unsub = ref.on('value', v => {
+      if (!v) return;
+      const nodeValue = v.val();
+      unsub();
+      resolve(nodeValue);
+    });
+  });
 
 describe('database()', () => {
   before(() => setDatabaseContents());
-  describe('ref().on()', () => {
+  describe('ref().on("value")', () => {
     it('throws without second argument', () => {
       const ref = firebase.database().ref('tests/types/number');
-      try {
-        const returnValue = ref.on('value');
-        return Promise.reject(new Error('No missing argument error'));
-      } catch (err) {
-        return Promise.resolve();
-      }
+      (() => ref.on('value')).should.throw(
+        'Query.on failed: Function called with 1 argument. Expects at least 2.'
+      );
     });
     it('returns a function', () => {
       const ref = firebase.database().ref('tests/types/number');
-      const returnValue = ref.on('value', () => {});
-      returnValue.should.be.Function();
+      const unsub = ref.on('value', () => {});
+      unsub.should.be.Function();
     });
     it('resolves with the correct value', async () => {
       const ref = firebase.database().ref('tests/types/number');
-      const unsub = ref.on('value', v => {
-        if (!v) return;
-        const nodeValue = v.val();
-        nodeValue.should.be.Number();
-        unsub();
-      });
+      const nodeValue = await waitForValueAtRef(ref);
+      nodeValue.should.be.Number();
     });
 
     it('is called again when the value changes', async () => {
@@ -40,6 +41,13 @@ describe('database()', () => {
       await ref.once('value');
       callback.should.be.calledTwice();
       unsub();
+    });
+  });
+  describe('ref().on("child_added")', () => {
+    it('returns a function', () => {
+      const ref = firebase.database().ref('tests/types/number');
+      const unsub = ref.on('child_added', () => {});
+      unsub.should.be.Function();
     });
   });
 });
