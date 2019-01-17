87f18a035e0c29fd69f24f9538983c450623f772
Salakar <mike.diarmid@gmail.com>
2019-01-17 08:01:52 +0000

    typedocs

---
+++ scripts/docs-version-name.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const { version } = require('./../lerna.json');
+
+// eslint-disable-next-line no-console
+console.log(`v${version.substr(0, version.indexOf('.'))}.x.x`);
