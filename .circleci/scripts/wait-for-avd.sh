378f19156fadbc60d4f6b747c7e812f26bd3bd34
Salakar <mike.diarmid@gmail.com>
2019-01-23 05:41:24 +0000

    [mono][ci]

--- .circleci/scripts/wait-for-avd.sh
+++ .circleci/scripts/wait-for-avd.sh
@@ -8,7 +8,6 @@ export PATH=$(dirname $(dirname $(command -v android)))/platform-tools:$PATH
 until [[ "$bootanim" =~ "stopped" ]]; do
   sleep 5
   bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
-  echo "Android device boot animation status is '$bootanim'"
 done
 
 sleep 5
