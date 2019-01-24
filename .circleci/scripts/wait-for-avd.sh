c3b2fce94903bfc76f667f0db54cf5dea2d2ef29
Salakar <mike.diarmid@gmail.com>
2019-01-24 02:00:36 +0000

    [mono][ci]

--- .circleci/scripts/wait-for-avd.sh
+++ .circleci/scripts/wait-for-avd.sh
@@ -6,10 +6,10 @@ echo "Waiting for AVD to finish booting"
 export PATH=$(dirname $(dirname $(command -v android)))/platform-tools:$PATH
 
 until [[ "$bootanim" =~ "stopped" ]]; do
-  sleep 5
+  sleep 10
   bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
 done
 
-sleep 15
+sleep 20
 
 echo "Android Virtual Device is now ready."
