c35ec3f9bc81ae782a149a3a905c557a5ad2fcbc
Salakar <mike.diarmid@gmail.com>
2019-02-05 04:50:07 +0000

    update notices

--- tests/app.js
+++ tests/app.js
@@ -1,13 +1,23 @@
 /* eslint-disable import/extensions,import/no-unresolved,import/first,import/no-extraneous-dependencies */
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  NativeModules,
-  Text,
-  View,
-  Image,
-  StyleSheet,
-} from 'react-native';
+import { AppRegistry, NativeModules, Text, View, Image, StyleSheet } from 'react-native';
 
 import jet from 'jet/platform/react-native';
 import NativeEventEmitter from '@react-native-firebase/app/lib/internal/RNFBNativeEventEmitter';
