780ad02749161322a5a1b126cc46b076dbc2fe9d
Salakar <mike.diarmid@gmail.com>
2019-02-05 04:24:45 +0000

    [dev] add multi-app test to module template

--- scripts/_TEMPLATE_/e2e/_template_.e2e.js
+++ scripts/_TEMPLATE_/e2e/_template_.e2e.js
@@ -5,9 +5,23 @@ describe('_template_()', () => {
       should.exist(app._template_);
       app._template_().app.should.equal(app);
     });
+
+    // removing as pending if module.options.hasMultiAppSupport = true
+    xit('supports multiple apps', async () => {
+      firebase._template_().app.name.should.equal('[DEFAULT]');
+
+      firebase
+        ._template_(firebase.app('secondaryFromNative'))
+        .app.name.should.equal('secondaryFromNative');
+
+      firebase
+        .app('secondaryFromNative')
+        ._template_()
+        .app.name.should.equal('secondaryFromNative');
+    });
   });
 
   describe('aMethod()', () => {
-
+    // TODO
   });
 });
