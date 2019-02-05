642e330a80ba024e5592b22358c78fbdc1b0f942
Salakar <mike.diarmid@gmail.com>
2019-02-05 03:59:05 +0000

    [tests][iid] add tests for instance id

--- packages/iid/e2e/iid.e2e.js
+++ packages/iid/e2e/iid.e2e.js
@@ -1,13 +1,94 @@
-describe('iid()', () => {
+android.describe('iid()', () => {
   describe('namespace', () => {
     it('accessible from firebase.app()', () => {
       const app = firebase.app();
       should.exist(app.iid);
       app.iid().app.should.equal(app);
     });
+
+    it('supports multiple apps', async () => {
+      firebase.iid().app.name.should.equal('[DEFAULT]');
+
+      firebase
+        .iid(firebase.app('secondaryFromNative'))
+        .app.name.should.equal('secondaryFromNative');
+
+      firebase
+        .app('secondaryFromNative')
+        .iid()
+        .app.name.should.equal('secondaryFromNative');
+
+      const defaultToken = await firebase.iid().getToken();
+
+      const secondaryToken = await firebase
+        .app('secondaryFromNative')
+        .iid()
+        .getToken();
+
+      defaultToken.should.be.a.String();
+      secondaryToken.should.be.a.String();
+
+      // same token as currently using the same messagingSenderId for both apps
+      secondaryToken.should.equal(defaultToken);
+    });
   });
 
-  describe('aMethod()', () => {
+  describe('get()', () => {
+    it('returns instance id string', async () => {
+      const iid = await firebase.iid().get();
+      iid.should.be.a.String();
+    });
+  });
 
+  describe('delete()', () => {
+    it('deletes the current instance id', async () => {
+      const iidBefore = await firebase.iid().get();
+      iidBefore.should.be.a.String();
+      await firebase.iid().delete();
+
+      const iidAfter = await firebase.iid().get();
+      iidAfter.should.be.a.String();
+      iidBefore.should.not.equal(iidAfter);
+      await sleep(4000);
+    });
+  });
+
+  describe('getToken()', () => {
+    it('should return an FCM token from getToken with arguments', async () => {
+      const authorizedEntity = firebase.iid().app.options.messagingSenderId;
+      const token = await firebase.iid().getToken(authorizedEntity, '*');
+      token.should.be.a.String();
+    });
+
+    it('should return an FCM token from getToken without arguments', async () => {
+      const token = await firebase.iid().getToken();
+      token.should.be.a.String();
+    });
+
+    it('should return an FCM token from getToken with 1 argument', async () => {
+      const authorizedEntity = firebase.iid().app.options.messagingSenderId;
+
+      const token = await firebase.iid().getToken(authorizedEntity);
+      token.should.be.a.String();
+    });
+  });
+
+  describe('deleteToken()', () => {
+    it('should return nil from deleteToken with arguments', async () => {
+      const authorizedEntity = firebase.iid().app.options.messagingSenderId;
+      const token = await firebase.iid().deleteToken(authorizedEntity, '*');
+      should.not.exist(token);
+    });
+
+    it('should return nil from deleteToken without arguments', async () => {
+      const token = await firebase.iid().deleteToken();
+      should.not.exist(token);
+    });
+
+    it('should return nil from deleteToken with 1 argument', async () => {
+      const authorizedEntity = firebase.iid().app.options.messagingSenderId;
+      const token = await firebase.iid().deleteToken(authorizedEntity);
+      should.not.exist(token);
+    });
   });
 });
