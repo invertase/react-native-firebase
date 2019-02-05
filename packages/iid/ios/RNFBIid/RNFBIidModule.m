6c797b1826e2a303d1644d1ea4920ce7098bdcf9
Salakar <mike.diarmid@gmail.com>
2019-02-05 23:35:20 +0000

    [ios][iid] implement module methods

--- packages/iid/ios/RNFBIid/RNFBIidModule.m
+++ packages/iid/ios/RNFBIid/RNFBIidModule.m
@@ -21,6 +21,9 @@
 #import "RNFBIidModule.h"
 #import "RNFBApp/RNFBSharedUtils.h"
 
+#if __has_include(<FirebaseMessaging/FirebaseMessaging.h>)
+#define HAS_MESSAGING
+#endif
 
 @implementation RNFBIidModule
 #pragma mark -
@@ -32,46 +35,106 @@
 #pragma mark Firebase Iid Methods
 
   RCT_EXPORT_METHOD(get:
-    (FIRApp *) firebaseApp // unused, iOS does not have multi-app support per instance
+    (FIRApp *) firebaseApp // unused, iOS does not have multi-app support per instance, keep for android
         resolver:
         (RCTPromiseResolveBlock) resolve
         rejecter:
         (RCTPromiseRejectBlock) reject) {
     [[FIRInstanceID instanceID] getIDWithHandler:^(NSString *_Nullable identity, NSError *_Nullable error) {
       if (error) {
-        NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
-        userInfo[@"code"] = [self getErrorCodeName:error];
-        userInfo[@"message"] = [error localizedDescription];
-        [RNFBSharedUtils rejectPromiseWithUserInfo:reject userInfo:userInfo];
+        [self rejectErrorWithCode:error reject:reject];
       } else {
         resolve(identity);
       }
     }];
   }
 
+  RCT_EXPORT_METHOD(getToken:
+    (FIRApp *) firebaseApp // unused, iOS does not have multi-app support per instance, keep for android
+        authorizedEntity:
+        (NSString *) authorizedEntity
+        scope:
+        (NSString *) scope
+        resolver:
+        (RCTPromiseResolveBlock) resolve
+        rejecter:
+        (RCTPromiseRejectBlock) reject) {
+    NSDictionary *options = nil;
+#ifdef HAS_MESSAGING
+    if ([FIRMessaging messaging].APNSToken) {
+      options = @{@"apns_token": [FIRMessaging messaging].APNSToken};
+    }
+#endif
+    [[FIRInstanceID instanceID] tokenWithAuthorizedEntity:authorizedEntity scope:scope options:options handler:^(NSString *_Nullable identity, NSError *_Nullable error) {
+      if (error) {
+        [self rejectErrorWithCode:error reject:reject];
+      } else {
+        resolve(identity);
+      }
+    }];
+  }
+
+  RCT_EXPORT_METHOD(delete:
+    (FIRApp *) firebaseApp // unused, iOS does not have multi-app support per instance, keep for android
+        resolver:
+        (RCTPromiseResolveBlock) resolve
+        rejecter:
+        (RCTPromiseRejectBlock) reject) {
+    [[FIRInstanceID instanceID] deleteIDWithHandler:^(NSError *_Nullable error) {
+      if (error) {
+        [self rejectErrorWithCode:error reject:reject];
+      } else {
+        resolve([NSNull null]);
+      }
+    }];
+  }
+
+  RCT_EXPORT_METHOD(deleteToken:
+    (FIRApp *) firebaseApp // unused, iOS does not have multi-app support per instance, keep for android
+        authorizedEntity:
+        (NSString *) authorizedEntity
+        scope:
+        (NSString *) scope
+        resolver:
+        (RCTPromiseResolveBlock) resolve
+        rejecter:
+        (RCTPromiseRejectBlock) reject) {
+    [[FIRInstanceID instanceID] deleteTokenWithAuthorizedEntity:authorizedEntity scope:scope handler:^(NSError *_Nullable error) {
+      if (error) {
+        [self rejectErrorWithCode:error reject:reject];
+      } else {
+        resolve([NSNull null]);
+      }
+    }];
+  }
+
+  - (void)rejectErrorWithCode:(NSError *)error reject:(RCTPromiseRejectBlock)reject {
+    NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
+    userInfo[@"code"] = [self getErrorCodeName:error];
+    userInfo[@"message"] = [error localizedDescription];
+    [RNFBSharedUtils rejectPromiseWithUserInfo:reject userInfo:userInfo];
+  }
+
   - (NSString *)getErrorCodeName:(NSError *)error {
-    NSString *code = @"UNKNOWN";
+    NSString *code = @"iid/unknown";
     switch (error.code) {
-      case FIRInstanceIDErrorUnknown:
-        code = @"UNKNOWN";
-        break;
       case FIRInstanceIDErrorAuthentication:
-        code = @"FAILED-GCM-AUTH";
+        code = @"iid/failed-gcm-auth";
         break;
       case FIRInstanceIDErrorNoAccess:
-        code = @"NO-ACCESS";
+        code = @"iid/no-access";
         break;
       case FIRInstanceIDErrorTimeout:
-        code = @"TIMEOUT";
+        code = @"iid/timeout";
         break;
       case FIRInstanceIDErrorNetwork:
-        code = @"NO-NETWORK";
+        code = @"iid/no-network";
         break;
       case FIRInstanceIDErrorOperationInProgress:
-        code = @"OPERATION-IN-PROGRESS";
+        code = @"iid/operation-in-progress";
         break;
       case FIRInstanceIDErrorInvalidRequest:
-        code = @"INVALID-REQUEST";
+        code = @"iid/invalid-request";
         break;
       default:
         break;
