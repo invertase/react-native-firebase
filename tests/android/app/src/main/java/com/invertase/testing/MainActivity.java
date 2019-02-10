88095a7a4852579c5bf1757ac9c75da79ef994e5
Salakar <mike.diarmid@gmail.com>
2019-02-10 01:47:12 +0000

    [tests][android] rename package/applicationId

--- tests/android/app/src/main/java/com/invertase/testing/MainActivity.java
+++ tests/android/app/src/main/java/com/invertase/testing/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testing;
+package com.invertase.testing;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 
 import com.facebook.react.ReactActivity;
-
-import io.invertase.firebase.app.ReactNativeFirebaseApp;
 
 public class MainActivity extends ReactActivity {
 
