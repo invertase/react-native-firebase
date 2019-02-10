255789045c4e9fe047f4d6a7d532b88946f2f8cc
Salakar <mike.diarmid@gmail.com>
2019-02-10 03:50:44 +0000

    [utils][android] move utils module to own package

--- packages/utils/android/src/main/java/io/invertase/firebase/utils/ReactNativeFirebaseUtilsModule.java
+++ packages/utils/android/src/main/java/io/invertase/firebase/utils/ReactNativeFirebaseUtilsModule.java
@@ -18,17 +18,22 @@ package io.invertase.firebase.utils;
  */
 
 import android.app.Activity;
+import android.content.IntentSender;
+import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.WritableMap;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
-import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.invertase.firebase.common.ReactNativeFirebaseModule;
+import io.invertase.firebase.common.SharedUtils;
 
 public class ReactNativeFirebaseUtilsModule extends ReactNativeFirebaseModule {
   private static final String TAG = "Utils";
@@ -37,4 +42,95 @@ public class ReactNativeFirebaseUtilsModule extends ReactNativeFirebaseModule {
     super(reactContext, TAG);
   }
 
+  @ReactMethod
+  public void androidGetPlayServicesStatus(Promise promise) {
+    promise.resolve(getPlayServicesStatusMap());
+  }
+
+  /**
+   * Prompt the device user to update play services
+   */
+  @ReactMethod
+  public void androidPromptForPlayServices() {
+    int status = isGooglePlayServicesAvailable();
+
+    if (
+      status != ConnectionResult.SUCCESS &&
+        GoogleApiAvailability.getInstance().isUserResolvableError(status)
+    ) {
+      Activity activity = getActivity();
+      if (activity != null) {
+        GoogleApiAvailability.getInstance()
+          .getErrorDialog(activity, status, status)
+          .show();
+      }
+    }
+  }
+
+  /**
+   * Prompt the device user to update play services
+   */
+  @ReactMethod
+  public void androidResolutionForPlayServices() {
+    int status = isGooglePlayServicesAvailable();
+    ConnectionResult connectionResult = new ConnectionResult(status);
+
+    if (!connectionResult.isSuccess() && connectionResult.hasResolution()) {
+      Activity activity = getActivity();
+      if (activity != null) {
+        try {
+          connectionResult.startResolutionForResult(activity, status);
+        } catch (IntentSender.SendIntentException error) {
+          Log.d(TAG, "resolutionForPlayServices", error);
+        }
+      }
+    }
+  }
+
+  /**
+   * Prompt the device user to update Play Services
+   */
+  @ReactMethod
+  public void androidMakePlayServicesAvailable() {
+    int status = isGooglePlayServicesAvailable();
+
+    if (status != ConnectionResult.SUCCESS) {
+      Activity activity = getActivity();
+      if (activity != null) {
+        GoogleApiAvailability.getInstance().makeGooglePlayServicesAvailable(activity);
+      }
+    }
+  }
+
+  private int isGooglePlayServicesAvailable() {
+    GoogleApiAvailability gapi = GoogleApiAvailability.getInstance();
+    return gapi.isGooglePlayServicesAvailable(getContext());
+  }
+
+  private WritableMap getPlayServicesStatusMap() {
+    WritableMap result = Arguments.createMap();
+    GoogleApiAvailability gapi = GoogleApiAvailability.getInstance();
+
+    int status = gapi.isGooglePlayServicesAvailable(getContext());
+    result.putInt("status", status);
+
+    if (status == ConnectionResult.SUCCESS) {
+      result.putBoolean("isAvailable", true);
+    } else {
+      result.putBoolean("isAvailable", false);
+      result.putString("error", gapi.getErrorString(status));
+      result.putBoolean("isUserResolvableError", gapi.isUserResolvableError(status));
+      result.putBoolean("hasResolution", new ConnectionResult(status).hasResolution());
+    }
+
+    return result;
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    Map<String, Object> constants = new HashMap<>();
+    constants.put("androidPlayServices", getPlayServicesStatusMap());
+    constants.put("isFirebaseTestLab", SharedUtils.isFirebaseTestLab());
+    return constants;
+  }
 }
