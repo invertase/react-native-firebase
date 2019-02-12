ee5bee9f4a3e87e6cb31e832bae0c5e4ab9ebc6d
IljaDaderko <idaderko@gmail.com>
2019-02-12 16:06:08 +0200

    API proposal

--- packages/config/lib/index.js
+++ packages/config/lib/index.js
@@ -20,7 +20,6 @@ import {
   FirebaseModule,
   getFirebaseRoot,
 } from '@react-native-firebase/app/lib/internal';
-
 import version from './version';
 
 const statics = {};
@@ -30,7 +29,74 @@ const namespace = 'config';
 const nativeModuleName = 'RNFBConfigModule';
 
 class FirebaseConfigModule extends FirebaseModule {
+  /**
+   * Activates the Fetched Config, so that the fetched key-values take effect.
+   * @returns {boolean}
+   */
+  activateFetched() {}
 
+  /**
+   * Fetches parameter values for your app.
+   * @param {number} cacheExpirationSeconds
+   * @returns {Promise}
+   */
+  async fetch(cacheExpirationSeconds = null) {}
+
+  /**
+   * Returns FirebaseRemoteConfig singleton
+   *  lastFetchTime,
+   *  lastFetchStatus.
+   *  isDeveloperModeEnabled
+   * @returns {Object}
+   */
+  async getConfigSettings() {}
+
+  /**
+   * Gets the set of keys that start with the given prefix.
+   * Optionally in the given namespace.
+   * @param {string} prefix
+   * @param {string} namespace
+   * @returns {string[]}
+   */
+  async getKeysByPrefix(prefix, namespace = null) {}
+
+  /**
+   * Gets the FirebaseRemoteConfigValue corresponding to the specified key.
+   * Optionally in the specified namespace.
+   * @param {string} key
+   * @param {string} namespace
+   */
+  async getValue(key, namespace = null) {}
+
+  /**
+   * Gets the FirebaseRemoteConfigValue array corresponding to the specified keys.
+   * Optionally in the specified namespace.
+   * @param {string[]} key
+   * @param {string} namespace
+   */
+  async getValues(keys, namespace = null) {}
+
+  /**
+   * Changes the settings for the FirebaseRemoteConfig object's operations,
+   * such as turning the developer mode on.
+   * @param {object} settings
+   * @description Android & iOS
+   */
+  setConfigSettings(settings) {}
+
+  /**
+   * Sets defaults.
+   * Optionally in the default namespace.
+   * @param {object} defaults
+   * @param {string} namespace
+   */
+  setDefaults(defaults, namespace = null) {}
+
+  /**
+   * Sets defaults based on resource.
+   * @param {string | number} resource
+   */
+  setDefaultsFromSource(resource, namespace = null) {}
 }
 
 // import { SDK_VERSION } from '@react-native-firebase/config';
