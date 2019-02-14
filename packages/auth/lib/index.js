ba53e8c6bc217c0d88dd04fd1ed2cc3b75f6346b
Salakar <mike.diarmid@gmail.com>
2019-02-14 20:32:34 +0000

    [auth] create auth package

---
+++ packages/auth/lib/index.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {
+  createModuleNamespace,
+  FirebaseModule,
+  getFirebaseRoot,
+} from '@react-native-firebase/app/lib/internal';
+
+import version from './version';
+
+const statics = {};
+
+const namespace = 'auth';
+
+const nativeModuleName = 'RNFBAuthModule';
+
+class FirebaseAuthModule extends FirebaseModule {
+
+}
+
+// import { SDK_VERSION } from '@react-native-firebase/auth';
+export const SDK_VERSION = version;
+
+// import auth from '@react-native-firebase/auth';
+// auth().X(...);
+export default createModuleNamespace({
+  statics,
+  version,
+  namespace,
+  nativeModuleName,
+  nativeEvents: false,
+  hasMultiAppSupport: false,
+  hasCustomUrlOrRegionSupport: false,
+  ModuleClass: FirebaseAuthModule,
+});
+
+// import auth, { firebase } from '@react-native-firebase/auth';
+// auth().X(...);
+// firebase.auth().X(...);
+export const firebase = getFirebaseRoot();
