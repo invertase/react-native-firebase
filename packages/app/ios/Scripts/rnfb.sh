d7ea0eecf5e1b7f6a19b514ad994f7768594db86
Salakar <mike.diarmid@gmail.com>
2019-03-10 00:46:44 +0000

    [skip ci]

--- packages/app/ios/Scripts/rnfb.sh
+++ packages/app/ios/Scripts/rnfb.sh
@@ -1,43 +1,53 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2016-present Invertase Limited & Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this library except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 set -e
 
+_MAX_LOOKUPS=3;
+_SEARCH_RESULT=''
+_CURRENT_LOOKUPS=1
 _JSON_ROOT="'react-native'"
 _JSON_FILE_NAME='firebase.json'
+_CURRENT_SEARCH_DIR=${PROJECT_DIR}
 _PLIST_BUDDY=/usr/libexec/PlistBuddy
 _TARGET_PLIST="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"
 _DSYM_PLIST="${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist"
 
 _PLIST_ENTRY_KEYS=(
   'firebase_crashlytics_collection_enabled'            #1
-  'rnfb_firebase_json'                                 #2 - must always be last entry
+  'firebase_json_raw'                                 #2 - must always be last entry
 )
+
 _PLIST_ENTRY_TYPES=(
   'bool'                                               #1
-  'string'                                             #2
+  'string'                                             #2 - must always be last entry
 )
+
 _PLIST_ENTRY_VALUES=(
   'NO'                                                 #1
-  '{}'                                                 #2
+  '{}'                                                 #2 - must always be last entry
 )
 
 function setPlistValue {
-  echo "info: setting plist entry '$1' of type '$2' in file '$4'"
+  echo "info:      setting plist entry '$1' of type '$2' in file '$4'"
   ${_PLIST_BUDDY} -c "Add :$1 $2 $3" $4 >/dev/null
 }
 
-echo "info: -------- START - RNFB Build Script ----------"
-for plist in ${_TARGET_PLIST} ${_DSYM_PLIST} ; do
-  if [[ -f ${plist} ]]; then
-    for i in "${!_PLIST_ENTRY_KEYS[@]}"; do
-      setPlistValue ${_PLIST_ENTRY_KEYS[$i]} ${_PLIST_ENTRY_TYPES[$i]} ${_PLIST_ENTRY_VALUES[$i]} ${plist}
-    done
-  fi
-done
-
-_MAX_LOOKUPS=5;
-_SEARCH_RESULT=''
-_CURRENT_LOOKUPS=1
-_CURRENT_SEARCH_DIR=${PROJECT_DIR}
+echo "info: -> RNFB build script started"
+echo "info: 1) Locating ${_JSON_FILE_NAME} file:"
 
 if [[ -z ${_CURRENT_SEARCH_DIR} ]]; then
   _CURRENT_SEARCH_DIR=`pwd`
@@ -47,11 +57,11 @@ while true; do
   _CURRENT_SEARCH_DIR=`dirname "$_CURRENT_SEARCH_DIR"`
   if [[ "$_CURRENT_SEARCH_DIR" == "/" ]] || [[ ${_CURRENT_LOOKUPS} -gt ${_MAX_LOOKUPS} ]]; then break; fi;
 
-  echo "info: ($_CURRENT_LOOKUPS of $_MAX_LOOKUPS) Searching in '$_CURRENT_SEARCH_DIR' for a ${_JSON_FILE_NAME} file."
+  echo "info:      ($_CURRENT_LOOKUPS of $_MAX_LOOKUPS) Searching in '$_CURRENT_SEARCH_DIR' for a ${_JSON_FILE_NAME} file."
   _SEARCH_RESULT=`find "$_CURRENT_SEARCH_DIR" -maxdepth 2 -name ${_JSON_FILE_NAME} -print | head -n 1`
 
   if [[ ${_SEARCH_RESULT} ]]; then
-    echo "info: ${_JSON_FILE_NAME} found at $_SEARCH_RESULT"
+    echo "info:      ${_JSON_FILE_NAME} found at $_SEARCH_RESULT"
     break;
   fi;
 
@@ -62,9 +72,24 @@ if [[ ${_SEARCH_RESULT} ]]; then
   _SEARCH_RESULT="'$_SEARCH_RESULT'"
   _JSON_OUTPUT=`python -c 'import json,sys; print(json.dumps(json.loads(open('${_SEARCH_RESULT}').read())['${_JSON_ROOT}']))' || echo "{}"`
   _PLIST_ENTRY_VALUES[${#_PLIST_ENTRY_VALUES[@]}-1]=${_JSON_OUTPUT}
-  echo ${_PLIST_ENTRY_VALUES[1]}
+  echo "info:      firebase.json value: ${_JSON_OUTPUT}"
+else
+  echo "warning:   A firebase.json file was not found, whilst this file is optional it is recommended to include it to configure firebase services in React Native Firebase."
 fi;
 
-echo "info: --------- END - RNFB Build Script -----------"
+echo "info: 2) Adding Info.plist entries: "
+
+for plist in ${_TARGET_PLIST} ${_DSYM_PLIST} ; do
+  if [[ -f ${plist} ]]; then
+    for i in "${!_PLIST_ENTRY_KEYS[@]}"; do
+      setPlistValue ${_PLIST_ENTRY_KEYS[$i]} ${_PLIST_ENTRY_TYPES[$i]} ${_PLIST_ENTRY_VALUES[$i]} ${plist}
+    done
+  else
+    echo "warning:   A plist file was not found"
+  fi
+done
+
+echo "info: <- RNFB build script finished"
+
 
 
