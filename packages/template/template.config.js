66d651bfb83588eff7f6472b28329238f717ef44
Salakar <mike.diarmid@gmail.com>
2019-04-20 21:08:02 +0100

    [template] create template

---
+++ packages/template/template.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "HelloWorld",
+
+  // Directory with template
+  templateDir: "./project",
+
+  // TODO currently breaks on cli
+  // Path to script, which will be executed after init
+  // postInitScript: "./post-init.js"
+};
