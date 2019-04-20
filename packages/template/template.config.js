26cf06b0d33f85ce18411bc1e66017274b751958
Salakar <mike.diarmid@gmail.com>
2019-04-20 21:24:21 +0100

    [eslint] ignore template project

--- packages/template/template.config.js
+++ packages/template/template.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "HelloWorld",
+  placeholderName: 'HelloWorld',
 
   // Directory with template
-  templateDir: "./project",
+  templateDir: './project',
 
   // TODO currently breaks on cli
   // Path to script, which will be executed after init
