56056e2d83a1cb30203772ec6bd4d88470944e87
Salakar <mike.diarmid@gmail.com>
2019-04-23 16:08:42 +0100

    [skip ci] types

--- packages/storage/lib/index.d.ts
+++ packages/storage/lib/index.d.ts
@@ -28,7 +28,7 @@ import {
  */
 export namespace Storage {
   /**
-   * Possible string formats used for uploading via `putString`
+   * Possible string formats used for uploading via `StorageReference.putString()`
    *
    * ```js
    * firebase.storage.StringFormat;
@@ -36,36 +36,82 @@ export namespace Storage {
    */
   export interface StringFormat {
     /**
+     * Raw string format.
+     *
+     * #### Usage
+     *
      * ```js
      * firebase.storage.StringFormat.RAW;
+     * ```
+     *
+     * #### Example String Format
+     *
+     * ```js
+     * const sampleString = '<Foo Bar>';
      * ```
      */
     RAW: 'raw';
 
     /**
+     * Base64 string format.
+     *
+     * Learn more about Base64 [on the Mozilla Web Docs](https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding)
+     *
+     * #### Usage
+     *
      * ```js
      * firebase.storage.StringFormat.BASE64;
      * ```
+     *
+     * #### Example String Format
+     *
+     * ```js
+     * const sampleString = 'PEZvbyBCYXI+';
+     * ```
+     *
      */
     BASE64: 'base64';
 
     /**
+     * Base64Url string format.
+     *
+     * #### Usage
+     *
      * ```js
      * firebase.storage.StringFormat.BASE64URL;
      * ```
+     *
+     * #### Example String Format
+     *
+     * ```js
+     * const sampleString = 'PEZvbyBCYXI-';
+     * ```
+     *
      */
     BASE64URL: 'base64url';
 
     /**
+     * Data URL string format.
+     *
+     * #### Usage
+     *
      * ```js
      * firebase.storage.StringFormat.DATA_URL;
+     * ```
+     *
+     * #### Example String Format
+     *
+     * ```js
+     * const sampleString = 'data:text/plain;base64,PEZvbyBCYXI+';
      * ```
      */
     DATA_URL: 'data_url';
   }
 
   /**
-   * An event to subscribe to that is triggered on a task.
+   * An event to subscribe to that is triggered on a Upload or Download task.
+   *
+   * Event subscription is created via `StorageTask.on()`.
    *
    * ```js
    * firebase.storage.TaskEvent;
@@ -83,7 +129,13 @@ export namespace Storage {
   }
 
   /**
+   * A collection of properties that indicates the
    *
+   * Event subscription is created via `StorageTask.on()`.
+   *
+   * ```js
+   * firebase.storage.TaskEvent;
+   * ```
    */
   export interface TaskState {
     /**
