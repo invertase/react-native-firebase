aa60832164ae24c1528af8e99d0615507226245f
Salakar <mike.diarmid@gmail.com>
2019-05-21 14:18:54 +0100

    [template] add getting started comments to app.js
    [skip ci]

--- packages/template/project/App.js
+++ packages/template/project/App.js
@@ -10,26 +10,39 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 import firebase from '@react-native-firebase/app';
-import '@react-native-firebase/analytics';
+
+// TODO(you): import any additional firebase services that you require for your app, e.g for auth:
+//    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
+//       run linking commands - this happens automatically at build time now
+//    2) rebuild your app via `yarn run run:android` or `yarn run run:ios`
+//    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
+//    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
 });
 
+const firebaseCredentials = Platform.select({
+  ios: 'https://invertase.io/oss/react-native-firebase/quick-start/ios-firebase-credentials',
+  android:
+    'https://invertase.io/oss/react-native-firebase/quick-start/android-firebase-credentials',
+});
+
 type Props = {};
 
 export default class App extends Component<Props> {
-  async componentDidMount() {
-    await firebase.analytics().setCurrentScreen('HomeScreen', 'RootView');
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        {!firebase.apps.length && (
+          <Text style={styles.instructions}>
+            {`You currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials, see \n ${firebaseCredentials} \n to learn how to add them.`}
+          </Text>
+        )}
       </View>
     );
   }
