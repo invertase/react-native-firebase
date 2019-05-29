893b352a0aa31562ec413675107e5dcc0162f896
tsemerad <tsemerad@users.noreply.github.com>
2019-05-29 03:05:18 -0700

    [JS][FEATURE][FIRESTORE] - Implement isEqual in Query :fire: (#2174)

--- src/index.d.ts
+++ src/index.d.ts
@@ -2592,6 +2592,8 @@ declare module 'react-native-firebase' {
 
         get(options?: Types.GetOptions): Promise<QuerySnapshot>;
 
+        isEqual(otherQuery: Query): boolean;
+
         limit(limit: number): Query;
 
         onSnapshot(
