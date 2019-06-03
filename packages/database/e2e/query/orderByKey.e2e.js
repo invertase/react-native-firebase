b1c3a57237a6ecd7e4549a0b0fe0b025897ebb23
Salakar <mike.diarmid@gmail.com>
2019-06-03 18:15:27 +0100

    -

---
+++ packages/database/e2e/query/orderByKey.e2e.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const { PATH, seed, wipe } = require('../helpers');
+
+const TEST_PATH = `${PATH}/orderByKey`;
+
+describe('database().ref().orderByKey()', () => {
+  before(() => seed(TEST_PATH));
+  after(() => wipe(TEST_PATH));
+
+  it('throws if an orderBy call has already been set', async () => {
+    try {
+      await firebase
+        .database()
+        .ref()
+        .orderByChild('foo')
+        .orderByKey();
+      return Promise.reject(new Error('Did not throw an Error.'));
+    } catch (error) {
+      error.message.should.containEql(`You can't combine multiple orderBy calls`);
+      return Promise.resolve();
+    }
+  });
+
+  it('order by a key', async () => {
+    const ref = firebase.database().ref(TEST_PATH);
+
+    try {
+      const snapshot = await ref
+        .child('query')
+        .orderByKey()
+        .once('value');
+
+      const expected = ['a', 'b', 'c'];
+
+      snapshot.forEach((childSnapshot, i) => {
+        childSnapshot.key.should.eql(expected[i]);
+      });
+
+      return Promise.resolve();
+    } catch (error) {
+      throw error;
+    }
+  });
+});
---
+++ packages/database/e2e/reference/child.e2e.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+describe('database().ref().child()', () => {
+  it('throws if path is not a string', async () => {
+    try {
+      firebase
+        .database()
+        .ref()
+        .child({ foo: 'bar' });
+      return Promise.reject(new Error('Did not throw an Error.'));
+    } catch (error) {
+      error.message.should.containEql(`'path' must be a string value`);
+      return Promise.resolve();
+    }
+  });
+
+  it('throws if path is not a valid string', async () => {
+    try {
+      firebase
+        .database()
+        .ref()
+        .child('$$$$$');
+      return Promise.reject(new Error('Did not throw an Error.'));
+    } catch (error) {
+      error.message.should.containEql(`Paths must be non-empty strings and can't contain`);
+      return Promise.resolve();
+    }
+  });
+});
