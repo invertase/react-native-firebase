fbe4c240e097da0591a33f433b682c47be7c54dd
IljaDaderko <idaderko@gmail.com>
2019-06-08 17:41:01 +0300

    Update iOS linking restorationHandler implementation

--- ios/RNFirebase/links/RNFirebaseLinks.m
+++ ios/RNFirebase/links/RNFirebaseLinks.m
@@ -55,7 +55,12 @@ RCT_EXPORT_MODULE();
 
 - (BOOL)application:(UIApplication *)application
 continueUserActivity:(NSUserActivity *)userActivity
- restorationHandler:(void (^)(NSArray *))restorationHandler {
+restorationHandler:
+    #if defined(__IPHONE_12_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_12_0)
+        (nonnull void (^)(NSArray<id<UIUserActivityRestoring>> *_Nullable))restorationHandler {
+    #else
+        (nonnull void (^)(NSArray *_Nullable))restorationHandler {
+    #endif  // __IPHONE_12_0
     if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb]) {
         return [[FIRDynamicLinks dynamicLinks]
                 handleUniversalLink:userActivity.webpageURL
