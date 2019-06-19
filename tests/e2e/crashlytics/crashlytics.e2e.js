95026db037cac0571b27d6c3c02f60a381263439
Max Komarychev <maxkomarychev@gmail.com>
2019-06-19 10:03:07 +0300

    Remove platform checks in e2e

--- tests/e2e/crashlytics/crashlytics.e2e.js
+++ tests/e2e/crashlytics/crashlytics.e2e.js
@@ -8,11 +8,9 @@ describe('crashlytics()', () => {
   xdescribe('crash()', () => {
     it('should force an app crash', async () => {
       await firebase.crashlytics().crash();
-      if (device.getPlatform() === 'ios') {
-        // ios responds quicker after a fatal exception if we re-install
-        await device.uninstallApp();
-        await device.installApp();
-      }
+      // ios responds quicker after a fatal exception if we re-install
+      await device.uninstallApp();
+      await device.installApp();
 
       await device.launchApp({ newInstance: true });
       await firebase.crashlytics().log('app relaunched from a crash');
@@ -22,18 +20,14 @@ describe('crashlytics()', () => {
   describe('log()', () => {
     it('should set a string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().log('123abc');
-      }
+      await firebase.crashlytics().log('123abc');
     });
 
     it('should error on a non string value', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().log(123456);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().log(123456);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -41,106 +35,87 @@ describe('crashlytics()', () => {
   describe('recordError()', () => {
     it('should record an error with a code and message', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().recordError(1234, 'Test error');
-      }
+      await firebase.crashlytics().recordError(1234, 'Test error');
     });
 
     it('should error on invalid args', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().recordError({}, []);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().recordError({}, []);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
-
   });
 
   describe('recordCustomError()', () => {
     it('should record an error with a name and message', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase
-          .crashlytics()
-          .recordCustomError('Test Error', 'Really bad error!');
-      }
+      await firebase
+        .crashlytics()
+        .recordCustomError('Test Error', 'Really bad error!');
     });
 
     it('should record an error with a name and message and customError', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase
-          .crashlytics()
-          .recordCustomError('Test Error', 'Really bad error!', [
-            { fileName: 'TestFile.js' },
-          ]);
-      }
+      await firebase
+        .crashlytics()
+        .recordCustomError('Test Error', 'Really bad error!', [
+          { fileName: 'TestFile.js' },
+        ]);
     });
 
     it('should record an error with a name and message and multiple customErrors', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase
-          .crashlytics()
-          .recordCustomError('Test Error', 'Really bad error!', [
-            { fileName: 'TestFile.js' },
-            { fileName: 'TestFile1.js' },
-          ]);
-      }
+      await firebase
+        .crashlytics()
+        .recordCustomError('Test Error', 'Really bad error!', [
+          { fileName: 'TestFile.js' },
+          { fileName: 'TestFile1.js' },
+        ]);
     });
 
     it('should error on invalid args', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().recordCustomError({}, []);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().recordCustomError({}, []);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
 
     it('should error on missing required customError property', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase
-            .crashlytics()
-            .recordCustomError('Test Error', 'Really bad error!', [{}]);
-        } catch (e) {
-          e.message.should.containEql('Missing required argument');
-        }
+      try {
+        await firebase
+          .crashlytics()
+          .recordCustomError('Test Error', 'Really bad error!', [{}]);
+      } catch (e) {
+        e.message.should.containEql('Missing required argument');
       }
     });
 
     it('should error on missing required customError property with multiple customErrors', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase
-            .crashlytics()
-            .recordCustomError('Test Error', 'Really bad error!', [
-              {fileName:'TestFile.js'},
-              {}
-            ]);
-        } catch (e) {
-          e.message.should.containEql('Missing required argument');
-        }
+      try {
+        await firebase
+          .crashlytics()
+          .recordCustomError('Test Error', 'Really bad error!', [
+            { fileName: 'TestFile.js' },
+            {},
+          ]);
+      } catch (e) {
+        e.message.should.containEql('Missing required argument');
       }
     });
 
     it('should error on invalid customError arg', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase
-            .crashlytics()
-            .recordCustomError('Test Error', 'Really bad error!', 1234);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase
+          .crashlytics()
+          .recordCustomError('Test Error', 'Really bad error!', 1234);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -148,17 +123,13 @@ describe('crashlytics()', () => {
   describe('setBoolValue()', () => {
     it('should set a boolean value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setBoolValue('boolKey', true);
-      }
+      await firebase.crashlytics().setBoolValue('boolKey', true);
     });
     it('should error on a non boolean value', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setFloatValue('boolKey', '123456');
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setFloatValue('boolKey', '123456');
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -166,23 +137,17 @@ describe('crashlytics()', () => {
   describe('setFloatValue()', () => {
     it('should set a float value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setFloatValue('floatKey', 1.23);
-      }
+      await firebase.crashlytics().setFloatValue('floatKey', 1.23);
     });
     it('should set a float value from int value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setFloatValue('floatKey', 1234);
-      }
+      await firebase.crashlytics().setFloatValue('floatKey', 1234);
     });
     it('should error on a non number value', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setFloatValue('floatKey', '123456');
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setFloatValue('floatKey', '123456');
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -190,23 +155,17 @@ describe('crashlytics()', () => {
   describe('setIntValue()', () => {
     it('should set a integer value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setIntValue('intKey', 123);
-      }
+      await firebase.crashlytics().setIntValue('intKey', 123);
     });
     it('should set a integer value from float', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setIntValue('intKey', 12.36);
-      }
+      await firebase.crashlytics().setIntValue('intKey', 12.36);
     });
     it('should error on a non number value', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setIntValue('intKey', '123456');
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setIntValue('intKey', '123456');
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -214,17 +173,13 @@ describe('crashlytics()', () => {
   describe('setStringValue()', () => {
     it('should set a string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setStringValue('stringKey', 'test');
-      }
+      await firebase.crashlytics().setStringValue('stringKey', 'test');
     });
     it('should error on a non string value', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setStringValue('stringKey', 123456);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setStringValue('stringKey', 123456);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -232,18 +187,14 @@ describe('crashlytics()', () => {
   describe('setUserIdentifier()', () => {
     it('should set a string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setUserIdentifier('123abc');
-      }
+      await firebase.crashlytics().setUserIdentifier('123abc');
     });
 
     it('should error on a non string value', async () => {
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setUserIdentifier(123456);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setUserIdentifier(123456);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -251,19 +202,15 @@ describe('crashlytics()', () => {
   describe('setUserName()', () => {
     it('should set a string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setUserName('123abc');
-      }
+      await firebase.crashlytics().setUserName('123abc');
     });
 
     it('should error on a non string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setUserName(123456);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setUserName(123456);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -271,19 +218,15 @@ describe('crashlytics()', () => {
   describe('setUserEmail()', () => {
     it('should set a string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().setUserEmail('123abc');
-      }
+      await firebase.crashlytics().setUserEmail('123abc');
     });
 
     it('should error on a non string value', async () => {
       // failing from XCode 10.1 -> 10.2
-      if (device.getPlatform() !== 'ios') {
-        try {
-          await firebase.crashlytics().setUserEmail(123456);
-        } catch (e) {
-          e.message.should.containEql('Invalid parameter');
-        }
+      try {
+        await firebase.crashlytics().setUserEmail(123456);
+      } catch (e) {
+        e.message.should.containEql('Invalid parameter');
       }
     });
   });
@@ -291,9 +234,7 @@ describe('crashlytics()', () => {
   describe('enableCrashlyticsCollection()', () => {
     // failing from XCode 10.1 -> 10.2
     it('should not throw', async () => {
-      if (device.getPlatform() !== 'ios') {
-        await firebase.crashlytics().enableCrashlyticsCollection();
-      }
+      await firebase.crashlytics().enableCrashlyticsCollection();
     });
   });
 });
