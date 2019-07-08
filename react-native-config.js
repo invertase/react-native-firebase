bb75a3568e61dad5f7a0be688f2ccb9d409cfb5c
Mathieu Acthernoene <zoontek@gmail.com>
2019-07-08 13:13:07 +0200

    Add react-native cli android config (even if it's seems ignored in the current release)

--- react-native-config.js
+++ react-native-config.js
@@ -4,6 +4,10 @@ module.exports = {
   dependency: {
     platforms: {
       ios: { podspecPath: path.join(__dirname, 'ios', 'RNFirebase.podspec') },
+      android: {
+        packageImportPath: 'import io.invertase.firebase.RNFirebasePackage;',
+        packageInstance: 'new RNFirebasePackage()',
+      },
     },
   },
 };
