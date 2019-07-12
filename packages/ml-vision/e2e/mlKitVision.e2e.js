a73d2651f48ea581c94da9f2da1b328caeef6e61
Salakar <mike.diarmid@gmail.com>
2019-07-12 16:35:22 +0100

    [tests] fix broken tests

--- packages/ml-vision/e2e/mlKitVision.e2e.js
+++ packages/ml-vision/e2e/mlKitVision.e2e.js
@@ -15,30 +15,25 @@
  *
  */
 
-describe('mlkit()', () => {
+describe('mlKitVision()', () => {
   describe('namespace', () => {
     it('accessible from firebase.app()', () => {
       const app = firebase.app();
-      should.exist(app.mlkit);
-      app.mlkit().app.should.equal(app);
+      should.exist(app.mlKitVision);
+      app.mlKitVision().app.should.equal(app);
     });
 
-    // removing as pending if module.options.hasMultiAppSupport = true
-    xit('supports multiple apps', async () => {
-      firebase.mlkit().app.name.should.equal('[DEFAULT]');
+    it('supports multiple apps', async () => {
+      firebase.mlKitVision().app.name.should.equal('[DEFAULT]');
 
       firebase
-        .mlkit(firebase.app('secondaryFromNative'))
+        .mlKitVision(firebase.app('secondaryFromNative'))
         .app.name.should.equal('secondaryFromNative');
 
       firebase
         .app('secondaryFromNative')
-        .mlkit()
+        .mlKitVision()
         .app.name.should.equal('secondaryFromNative');
     });
-  });
-
-  describe('aMethod()', () => {
-    // TODO
   });
 });
