f882c05bf50a4e929a964f72293069db547ff999
ehesp <elliot.hesp@gmail.com>
2019-07-30 10:21:45 +0100

    Rework MessageBuilder

--- packages/messaging/e2e/messaging.e2e.js
+++ packages/messaging/e2e/messaging.e2e.js
@@ -24,7 +24,73 @@ describe('messaging()', () => {
     });
   });
 
+  describe('setAutoInitEnabled()', () => {
+    it('throws if enabled is not a boolean', () => {
+      try {
+        firebase.messaging().setAutoInitEnabled(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'enabled' expected a boolean value`);
+        return Promise.resolve();
+      }
+    });
+  });
+
+  describe('getToken()', () => {
+    it('throws if authorizedEntity is not a string', () => {
+      try {
+        firebase.messaging().getToken(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'authorizedEntity' expected a string value`);
+        return Promise.resolve();
+      }
+    });
+
+    it('throws if scope is not a string', () => {
+      try {
+        firebase.messaging().getToken('123', 123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'scope' expected a string value`);
+        return Promise.resolve();
+      }
+    });
+  });
+
+  describe('deleteToken()', () => {
+    it('throws if authorizedEntity is not a string', () => {
+      try {
+        firebase.messaging().deleteToken(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'authorizedEntity' expected a string value`);
+        return Promise.resolve();
+      }
+    });
+
+    it('throws if scope is not a string', () => {
+      try {
+        firebase.messaging().deleteToken('123', 123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'scope' expected a string value`);
+        return Promise.resolve();
+      }
+    });
+  });
+
   describe('onMessage()', () => {
+    it('throws if listener is not a function', () => {
+      try {
+        firebase.messaging().onMessage({});
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'listener' expected a function`);
+        return Promise.resolve();
+      }
+    });
+
     android.it('receives messages when the app is in the foreground', async () => {
       const spy = sinon.spy();
       const unsubscribe = firebase.messaging().onMessage(spy);
@@ -43,7 +109,65 @@ describe('messaging()', () => {
     });
   });
 
+  describe('onTokenRefresh()', () => {
+    it('throws if listener is not a function', () => {
+      try {
+        firebase.messaging().onTokenRefresh({});
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'listener' expected a function`);
+        return Promise.resolve();
+      }
+    });
+  });
+
+  describe('onDeletedMessages()', () => {
+    it('throws if listener is not a function', () => {
+      try {
+        firebase.messaging().onDeletedMessages(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'listener' expected a function`);
+        return Promise.resolve();
+      }
+    });
+  });
+
+  describe('onMessageSent()', () => {
+    it('throws if listener is not a function', () => {
+      try {
+        firebase.messaging().onMessageSent(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'listener' expected a function`);
+        return Promise.resolve();
+      }
+    });
+  });
+
+  describe('onSendError()', () => {
+    it('throws if listener is not a function', () => {
+      try {
+        firebase.messaging().onSendError('123');
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'listener' expected a function`);
+        return Promise.resolve();
+      }
+    });
+  });
+
   describe('setBackgroundMessageHandler()', () => {
+    it('throws if handler is not a function', () => {
+      try {
+        firebase.messaging().setBackgroundMessageHandler('123');
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'handler' expected a function`);
+        return Promise.resolve();
+      }
+    });
+
     android.it('receives messages when the app is in the background', async () => {
       const spy = sinon.spy();
       const token = await firebase.messaging().getToken();
@@ -64,6 +188,50 @@ describe('messaging()', () => {
       spy.firstCall.args[0].should.be.an.Object();
       spy.firstCall.args[0].data.should.be.an.Object();
       spy.firstCall.args[0].data.foo.should.eql('bar');
+    });
+  });
+
+  describe('subscribeToTopic()', () => {
+    it('throws if topic is not a string', () => {
+      try {
+        firebase.messaging().subscribeToTopic(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'topic' expected a string value`);
+        return Promise.resolve();
+      }
+    });
+
+    it('throws if topic contains a /', () => {
+      try {
+        firebase.messaging().subscribeToTopic('foo/bar');
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'topic' must not include "/"`);
+        return Promise.resolve();
+      }
+    });
+  });
+
+  describe('unsubscribeFromTopic()', () => {
+    it('throws if topic is not a string', () => {
+      try {
+        firebase.messaging().unsubscribeFromTopic(123);
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'topic' expected a string value`);
+        return Promise.resolve();
+      }
+    });
+
+    it('throws if topic contains a /', () => {
+      try {
+        firebase.messaging().unsubscribeFromTopic('foo/bar');
+        return Promise.reject(new Error('Did not throw Error.'));
+      } catch (e) {
+        e.message.should.containEql(`'topic' must not include "/"`);
+        return Promise.resolve();
+      }
     });
   });
 });
