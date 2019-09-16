daa3c3cb204757a83fec66e2046d42c3eeb60699
ehesp <elliot.hesp@gmail.com>
2019-09-16 09:15:07 +0100

    [ts] database

--- packages/database/type-test.ts
+++ packages/database/type-test.ts
@@ -1,5 +1,5 @@
 import firebase from '@react-native-firebase/app';
-import defaultExport, { firebase as firebaseFromModule } from '@react-native-firebase/database';
+import * as database from '@react-native-firebase/database';
 
 // checks module exists at root
 console.log(firebase.database().app.name);
@@ -7,12 +7,6 @@ console.log(firebase.database().ref('foo/bar'));
 
 // checks module exists at app level
 console.log(firebase.app().database().app.name);
-console.log(
-  firebase
-    .app()
-    .database()
-    .ref('foo/bar'),
-);
 
 // check module accepts a string arg
 console.log(firebase.app().database('some-string').app.name);
@@ -21,16 +15,51 @@ console.log(firebase.app().database('some-string').app.name);
 console.log(firebase.database.SDK_VERSION);
 
 // checks statics exist on defaultExport
-console.log(defaultExport.SDK_VERSION);
+console.log(database.firebase.SDK_VERSION);
 
 // checks root exists
 console.log(firebase.SDK_VERSION);
 
 // checks firebase named export exists on module
-console.log(firebaseFromModule.SDK_VERSION);
+console.log(database.firebase.SDK_VERSION);
 
 // checks multi-app support exists
 console.log(firebase.database(firebase.app()).app.name);
 
 // checks default export supports app arg
-console.log(defaultExport(firebase.app()).app.name);
+console.log(database.firebase(firebase.app('foo')).app.name);
+
+console.log(database.ServerValue.TIMESTAMP);
+
+console.log(
+  firebase
+    .app()
+    .database()
+    .ref('foo/bar'),
+);
+
+firebase
+  .database()
+  .ref('foo')
+  .push({
+    foo: 'bar',
+  });
+
+firebase
+  .database()
+  .goOnline()
+  .then();
+
+const r = firebase.database().ref('foo');
+r.child('foo')
+  .update({ foo: 'bar' }, () => {})
+  .then();
+
+r.once('value').then(ds => {
+  console.log(ds.exists());
+  console.log(ds.forEach);
+  console.log(ds.val());
+  console.log(ds.hasChildren());
+  console.log(ds.hasChild('foo'));
+  console.log(ds.getPriority());
+});
