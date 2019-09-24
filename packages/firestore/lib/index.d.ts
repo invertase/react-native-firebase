71b8087be7d20bcd48754d3aa1ac19a13625d81e
Elliot Hesp <elliot.hesp@gmail.com>
2019-09-24 20:17:45 +0100

    [firestore] Fix SetOption types

--- packages/firestore/lib/index.d.ts
+++ packages/firestore/lib/index.d.ts
@@ -1287,13 +1287,13 @@ export namespace FirebaseFirestoreTypes {
      * Changes the behavior of a `set()` call to only replace the values specified in its data argument.
      * Fields omitted from the `set()` call remain untouched.
      */
-    merge: boolean;
+    merge?: boolean;
 
     /**
      * Changes the behavior of `set()` calls to only replace the specified field paths.
      * Any field path that is not specified is ignored and remains untouched.
      */
-    mergeField: (string | FieldPath)[];
+    mergeFields?: (string | FieldPath)[];
   }
 
   /**
