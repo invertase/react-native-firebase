eeb90c0a376e88f4ceb20a1dc5fd3bb4ce558a61
Brian Cooley-Gilliom <bcgilliom@fullstacklabs.co>
2019-11-17 00:51:04 -0500

    fix(storage,ios): Handle null Storage metadata values (#2881)

--- packages/storage/ios/RNFBStorage/RNFBStorageCommon.m
+++ packages/storage/ios/RNFBStorage/RNFBStorageCommon.m
@@ -308,7 +308,7 @@
 
   return [@{
       @"bytesTransferred": @(task.progress.completedUnitCount),
-      @"metadata": storageMetadata,
+      @"metadata": storageMetadata ? storageMetadata : [NSNull null],
       @"state": state,
       @"totalBytes": @(task.progress.totalUnitCount)
   } mutableCopy];
