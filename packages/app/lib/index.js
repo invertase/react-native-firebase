a990f2cf503e31f30fec253cc7ed6b5a6dcc000b
Joel Gallant <joel@joelgallant.me>
2019-12-23 13:34:38 -0700

    Fixes default re-export syntax

--- packages/app/lib/index.js
+++ packages/app/lib/index.js
@@ -18,6 +18,6 @@
 import { getFirebaseRoot } from './internal/registry/namespace';
 
 export const firebase = getFirebaseRoot();
-export utils from './utils';
+export { default as utils } from './utils';
 
 export default firebase;
