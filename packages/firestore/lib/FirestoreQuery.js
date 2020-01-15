caa9612d59d45dbb4f6fe64fdb5b40ed9f16a7a0
MohammedMaaz <maazproductions25@gmail.com>
2020-01-15 16:08:20 +0500

    fixed, fieldPath:'__name__' should be added once for queries with multiple cursors

--- packages/firestore/lib/FirestoreQuery.js
+++ packages/firestore/lib/FirestoreQuery.js
@@ -72,13 +72,14 @@ export default class FirestoreQuery {
 
       for (let i = 0; i < currentOrders.length; i++) {
         const order = currentOrders[i];
+        //skip if fieldPath is '__name__'
+        if (order.fieldPath === '__name__') continue;
+
         const value = documentSnapshot.get(order.fieldPath);
 
         if (value === undefined) {
           throw new Error(
-            `firebase.firestore().collection().${cursor}(*) You are trying to start or end a query using a document for which the field '${
-              order.fieldPath
-            }' (used as the orderBy) does not exist.`,
+            `firebase.firestore().collection().${cursor}(*) You are trying to start or end a query using a document for which the field '${order.fieldPath}' (used as the orderBy) does not exist.`,
           );
         }
 
@@ -87,10 +88,14 @@ export default class FirestoreQuery {
 
       // Based on https://github.com/invertase/react-native-firebase/issues/2854#issuecomment-552986650
       if (modifiers._orders.length) {
-        modifiers._orders.push({
-          fieldPath: '__name__',
-          direction: modifiers._orders[modifiers._orders.length - 1].direction,
-        });
+        const lastOrder = modifiers._orders[modifiers._orders.length - 1];
+        //push '__name__' field only if not present already
+        if (lastOrder.fieldPath !== '__name__') {
+          modifiers._orders.push({
+            fieldPath: '__name__',
+            direction: lastOrder.direction,
+          });
+        }
       } else {
         modifiers._orders.push({
           fieldPath: '__name__',
