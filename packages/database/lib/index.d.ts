f787ce3db661e4730e302eca716ff09b0250d324
Russell Wheatley <russellwheatley85@gmail.com>
2020-01-30 12:16:38 +0000

    add key to the Reference type for realtime db (#3150)
    * add key to the Reference type for realtime db
    
    * Update packages/database/lib/index.d.ts
    
    * Update packages/database/lib/index.d.ts
    
    * Update packages/database/lib/index.d.ts
    
    * removed spacing
    
    Co-authored-by: Mike Diarmid <mike.diarmid@gmail.com>

--- packages/database/lib/index.d.ts
+++ packages/database/lib/index.d.ts
@@ -152,6 +152,13 @@ export namespace FirebaseDatabaseTypes {
     child(path: string): Reference;
 
     /**
+     * The last part of the Reference's path.
+     * For example, "ada" is the key for https://<DATABASE_NAME>.firebaseio.com/users/ada.
+     * The key of a root Reference is null.
+     */
+    key: string | null;
+
+    /**
      * Writes data to this Database location.
      *
      * This will overwrite any data at this location and all child locations.
