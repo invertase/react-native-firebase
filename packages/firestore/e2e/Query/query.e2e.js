ab31293e35ca78b41a5b9e49a8b453ddf507e64c
Mike Diarmid <mike.diarmid@gmail.com>
2020-02-18 11:10:58 +0000

    Update packages/firestore/e2e/Query/query.e2e.js

--- packages/firestore/e2e/Query/query.e2e.js
+++ packages/firestore/e2e/Query/query.e2e.js
@@ -28,7 +28,7 @@ describe('FirestoreQuery/FirestoreQueryModifiers', () => {
     queryAfter._modifiers._filters.length.should.equal(1);
   });
 
-  it('Throws if where equality operator is invoked, and the where fieldPath parameter matches any orderBy parameter ', async () => {
+  it('throws if where equality operator is invoked, and the where fieldPath parameter matches any orderBy parameter', async () => {
     try {
       firebase
         .firestore()
