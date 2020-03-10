d4636fc01ed014e15d338c817c78b3a6cdaad9fa
tanpm <tanpm@mobiletech.vn>
2020-03-10 11:13:54 +0700

    Revert "add more file"
    This reverts commit 3c6b7037263d4684284fbf0aa8318e8a1c0361c2.

--- dist/modules/firestore/SnapshotError.js
+++
@@ -1,9 +0,0 @@
-import NativeError from '../../common/NativeError';
-export default class SnapshotError extends NativeError {
-  constructor(nativeErrorMap) {
-    super(nativeErrorMap.error);
-    this.path = nativeErrorMap.path;
-    this.appName = nativeErrorMap.appName;
-  }
-
-}
--- dist/modules/functions/HttpsError.js
+++
@@ -1,9 +0,0 @@
-export default class HttpsError extends Error {
-  constructor(code, message, details) {
-    super(message);
-    this.code = code;
-    this.details = details;
-    this.message = message;
-  }
-
-}
