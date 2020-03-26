invertase/react-native-firebase

commit 72e81e5f9921cd23a50fd3d50a8d62052f907965

bug(analytics): logEvent params can be objects (#3219)
* bug(analytics): logEvent params can be objects

* test(analytics): removed unnecessary test

packages/analytics/lib/index.js
@@ -23,6 +23,7 @@ import {
   isOneOf,
   isString,
   isUndefined,
+  isArray,
 } from '@react-native-firebase/app/lib/common';
 import { validateStruct, validateCompound } from '@react-native-firebase/app/lib/common/struct';
 
@@ -90,12 +91,12 @@ class FirebaseAnalyticsModule extends FirebaseModule {
     }
 
     const entries = Object.entries(params);
+
     for (let i = 0; i < entries.length; i++) {
       const [key, value] = entries[i];
-      if (!isString(value) && !isNumber(value) && !isBoolean(value)) {
-        throw new Error(
-          `firebase.analytics().logEvent(_, *) 'params' value for parameter '${key}' is invalid, expected a string or number value.`,
-        );
+
+      if (isObject(value) || isArray(value)) {
+        params[key] = value.toString();
       }
     }
 

packages/analytics/e2e/analytics.e2e.js
@@ -111,21 +111,6 @@ describe('analytics()', () => {
       }
     });
 
-    it('errors if params contains invalid types', () => {
-      try {
-        firebase.analytics().logEvent('invertase', {
-          foo: 'bar',
-          bar: {
-            baz: 123,
-          },
-        });
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        e.message.should.containEql("'params' value for parameter 'bar' is invalid");
-        return Promise.resolve();
-      }
-    });
-
     it('log an event without parameters', async () => {
       await firebase.analytics().logEvent('invertase_event');
     });

commit 0aa409502ebad609fa0e05294260d8517d7af6fb

fix(analytics): pass logEvent params as-is broken in #3219

packages/analytics/lib/index.js
@@ -90,16 +90,6 @@ class FirebaseAnalyticsModule extends FirebaseModule {
       );
     }
 
-    const entries = Object.entries(params);
-
-    for (let i = 0; i < entries.length; i++) {
-      const [key, value] = entries[i];
-
-      if (isObject(value) || isArray(value)) {
-        params[key] = value.toString();
-      }
-    }
-
     return this.native.logEvent(name, params);
   }
 