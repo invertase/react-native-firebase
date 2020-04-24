1ff8507346db5472c801e00fcc2a06c5b9d7ac99
dackers86 <ackers86@hotmail.com>
2020-04-24 16:22:55 +0100

    Removed async from new functions tests

--- packages/functions/__tests__/functions.test.js
+++ packages/functions/__tests__/functions.test.js
@@ -2,7 +2,7 @@ import functions from '../lib';
 
 describe('functions', () => {
   describe('useFunctionsEmulator()', () => {
-    it('useFunctionsEmulator -> uses 10.0.2.2', async () => {
+    it('useFunctionsEmulator -> uses 10.0.2.2', () => {
       functions().useFunctionsEmulator('http://localhost');
       expect(functions()._useFunctionsEmulatorOrigin).toBe('http://10.0.2.2');
       functions().useFunctionsEmulator('http://127.0.0.1');
