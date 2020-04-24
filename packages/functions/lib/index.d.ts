d26ea9da99dc97c46e058e3edc352fdb1858a289
russellwheatley <russellwheatley85@gmail.com>
2020-04-24 11:24:21 +0100

    docs(remote-config): update example

--- packages/functions/lib/index.d.ts
+++ packages/functions/lib/index.d.ts
@@ -150,13 +150,13 @@ export namespace FirebaseFunctionsTypes {
   export interface HttpsCallableOptions {
     /**
      * The timeout property allows you to control how long the application will wait for the cloud function to respond in milliseconds.
-     * const instance = firebase.functions().httpsCallable('order', { timeout: 7000 });
      *
      * #### Example
      *
      *```js
      * // The below will wait 7 seconds for a response from the cloud function before an error is thrown
      * try {
+     * const instance = firebase.functions().httpsCallable('order', { timeout: 7000 });
      *  const response = await instance({
      *    id: '12345',
      *  });
