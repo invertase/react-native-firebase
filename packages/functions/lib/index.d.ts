118ca7d78ec4f0e44c474d3e42880dac199f7f24
Mike Diarmid <mike.diarmid@gmail.com>
2020-04-24 11:11:12 +0100

    Apply suggestions from code review

--- packages/functions/lib/index.d.ts
+++ packages/functions/lib/index.d.ts
@@ -148,6 +148,9 @@ export namespace FirebaseFunctionsTypes {
    * An HttpsCallableOptions object that can be passed as the second argument to `firebase.functions().httpsCallable(name, HttpsCallableOptions)`.
    * It allows you to control how long the application will wait for the cloud function to respond in milliseconds.
    *
+   * ### Example
+   *
+   * ```js
    * const instance = firebase.functions().httpsCallable('order', { timeout: 7000 });
    * // The below will wait 7 seconds for a response from the cloud function before an error is thrown
    * try {
@@ -157,6 +160,7 @@ export namespace FirebaseFunctionsTypes {
    * } catch (e) {
    *  console.log(e);
    * }
+   * ```
    */
   export interface HttpsCallableOptions {
     timeout?: number;
