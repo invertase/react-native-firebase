8212cec8e1552ec2c979145ef5e7d01efbace9fc
dackers86 <ackers86@hotmail.com>
2020-04-24 14:51:05 +0100

    Bug fix on functions js test

--- packages/functions/__tests__/functions.test.js
+++ packages/functions/__tests__/functions.test.js
@@ -1,16 +1,12 @@
 import functions from '../lib';
-import { firebase } from '@react-native-firebase/functions';
 
 describe('functions', () => {
   describe('useFunctionsEmulator()', () => {
     it('useFunctionsEmulator -> uses 10.0.2.2', async () => {
-      const region = 'europe-west2';
-      const functions = firebase.app().functions(region);
-
-      functions.useFunctionsEmulator('http://localhost');
-      expect(functions._useFunctionsEmulatorOrigin).toBe('http://10.0.2.2');
-      functions.useFunctionsEmulator('http://127.0.0.1');
-      expect(functions._useFunctionsEmulatorOrigin).toBe('http://10.0.2.2');
+      functions().useFunctionsEmulator('http://localhost');
+      expect(functions()._useFunctionsEmulatorOrigin).toBe('http://10.0.2.2');
+      functions().useFunctionsEmulator('http://127.0.0.1');
+      expect(functions()._useFunctionsEmulatorOrigin).toBe('http://10.0.2.2');
     });
   });
 });
