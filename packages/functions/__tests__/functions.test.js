9c3ca46404f000d653c693170cc7f73e77dd82e5
dackers86 <ackers86@hotmail.com>
2020-04-28 09:24:09 +0100

    Updated functions jest tests

--- packages/functions/__tests__/functions.test.js
+++ packages/functions/__tests__/functions.test.js
@@ -1,6 +1,14 @@
-import functions from '../lib';
+import functions, { firebase } from '../lib';
 
 describe('Cloud Functions', () => {
+  describe('namespace', () => {
+    it('accessible from firebase.app()', () => {
+      const app = firebase.app();
+      expect(app.functions).toBeDefined();
+      expect(app.functions().httpsCallable).toBeDefined();
+    });
+  });
+
   describe('useFunctionsEmulator()', () => {
     it('useFunctionsEmulator -> uses 10.0.2.2', () => {
       functions().useFunctionsEmulator('http://localhost');
