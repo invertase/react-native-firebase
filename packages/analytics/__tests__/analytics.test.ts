7580ca2676fe1f41741b451782fbf126ab8a673c
dackers86 <ackers86@hotmail.com>
2020-04-30 10:26:25 +0100

    tests: updated analytics try catch tests

--- packages/analytics/__tests__/analytics.test.ts
+++ packages/analytics/__tests__/analytics.test.ts
@@ -10,37 +10,27 @@ describe('Analytics', () => {
 
     it('throws if non default app arg provided to firebase.analytics(APP)', () => {
       const app = firebase.app('secondaryFromNative');
-      try {
-        // @ts-ignore
-        firebase.analytics(app);
-        return Promise.reject(new Error('Did not throw'));
-      } catch (e) {
-        expect(e.message).toMatch(
-          'You attempted to call "firebase.analytics(app)" but; analytics does not support multiple Firebase Apps.',
-        );
 
-        expect(e.message).toMatch(
-          'Ensure the app provided is the default Firebase app only and not the "secondaryFromNative" app.',
-        );
+      const expectedError = [
+        'You attempted to call "firebase.analytics(app)" but; analytics does not support multiple Firebase Apps.',
+        '',
+        'Ensure the app provided is the default Firebase app only and not the "secondaryFromNative" app.',
+      ].join('\r\n');
 
-        return Promise.resolve();
-      }
+      // @ts-ignore
+      expect(() => firebase.analytics(app)).toThrowError(expectedError);
     });
 
     it('throws if analytics access from a non default app', () => {
       const app = firebase.app('secondaryFromNative');
-      try {
-        app.analytics();
-        return Promise.reject(new Error('Did not throw'));
-      } catch (e) {
-        // prettier-ignore
-        expect(e.message).toMatch(
-          "You attempted to call \"firebase.app('secondaryFromNative').analytics\" but; analytics does not support multiple Firebase Apps.",
-        );
 
-        expect(e.message).toMatch('Ensure you access analytics from the default application only.');
-        return Promise.resolve();
-      }
+      const expectedError = [
+        'You attempted to call "firebase.app(\'secondaryFromNative\').analytics" but; analytics does not support multiple Firebase Apps.',
+        '',
+        'Ensure you access analytics from the default application only.',
+      ].join('\r\n');
+
+      expect(() => app.analytics()).toThrowError(expectedError);
     });
 
     // TODO in app/registry/namespace.js - if (!hasCustomUrlOrRegionSupport)
@@ -58,80 +48,45 @@ describe('Analytics', () => {
 
   describe('logEvent()', () => {
     it('errors if name is not a string', () => {
-      try {
-        // @ts-ignore
-        firebase.analytics().logEvent(123);
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.analytics().logEvent(*) 'name' expected a string value.",
-        );
-        return Promise.resolve();
-      }
+      // @ts-ignore
+      expect(() => firebase.analytics().logEvent(123)).toThrowError(
+        "firebase.analytics().logEvent(*) 'name' expected a string value.",
+      );
     });
 
     it('errors if params is not an object', () => {
-      try {
-        // @ts-ignore
-        firebase.analytics().logEvent('invertase_event', 'foobar');
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.analytics().logEvent(_, *) 'params' expected an object value.",
-        );
-
-        return Promise.resolve();
-      }
+      // @ts-ignore
+      expect(() => firebase.analytics().logEvent('invertase_event', 'foobar')).toThrowError(
+        "firebase.analytics().logEvent(_, *) 'params' expected an object value.",
+      );
     });
 
     it('errors on using a reserved name', () => {
-      try {
-        firebase.analytics().logEvent('session_start');
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.analytics().logEvent(*) 'name' the event name 'session_start' is reserved and can not be used.",
-        );
-        return Promise.resolve();
-      }
+      expect(() => firebase.analytics().logEvent('session_start')).toThrowError(
+        "firebase.analytics().logEvent(*) 'name' the event name 'session_start' is reserved and can not be used.",
+      );
     });
 
     it('errors if name not alphanumeric', () => {
-      try {
-        firebase.analytics().logEvent('!@£$%^&*');
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.analytics().logEvent(*) 'name' invalid event name '!@£$%^&*'. Names should contain 1 to 32 alphanumeric characters or underscores.",
-        );
-        return Promise.resolve();
-      }
+      expect(() => firebase.analytics().logEvent('!@£$%^&*')).toThrowError(
+        "firebase.analytics().logEvent(*) 'name' invalid event name '!@£$%^&*'. Names should contain 1 to 32 alphanumeric characters or underscores.",
+      );
     });
 
     it('errors if more than 25 params provided', () => {
-      try {
-        firebase.analytics().logEvent('invertase', Object.assign({}, new Array(26).fill(1)));
-        return Promise.reject(new Error('Did not throw.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.analytics().logEvent(_, *) 'params' maximum number of parameters exceeded (25).",
-        );
-        return Promise.resolve();
-      }
+      expect(() =>
+        firebase.analytics().logEvent('invertase', Object.assign({}, new Array(26).fill(1))),
+      ).toThrowError(
+        "firebase.analytics().logEvent(_, *) 'params' maximum number of parameters exceeded (25).",
+      );
     });
 
     describe('setAnalyticsCollectionEnabled()', () => {
       it('throws if not a boolean', () => {
-        try {
-          // @ts-ignore
-          firebase.analytics().setAnalyticsCollectionEnabled('foo');
-          return Promise.reject(new Error('Did not throw.'));
-        } catch (e) {
-          expect(e.message).toEqual(
-            "firebase.analytics().setAnalyticsCollectionEnabled(*) 'enabled' expected a boolean value.",
-          );
-          return Promise.resolve();
-        }
+        // @ts-ignore
+        expect(() => firebase.analytics().setAnalyticsCollectionEnabled('foo')).toThrowError(
+          "firebase.analytics().setAnalyticsCollectionEnabled(*) 'enabled' expected a boolean value.",
+        );
       });
     });
   });
