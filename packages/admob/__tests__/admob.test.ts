30540dab2108d6434a479a12cd7cb582589400b1
dackers86 <ackers86@hotmail.com>
2020-04-30 10:45:57 +0100

    tests(admob): updated try catch tests

--- packages/admob/__tests__/admob.test.ts
+++ packages/admob/__tests__/admob.test.ts
@@ -11,65 +11,47 @@ describe('Admob', () => {
 
   describe('setRequestConfiguration()', () => {
     it('throws if config is not an object', () => {
-      try {
-        // @ts-ignore
-        firebase.admob().setRequestConfiguration('123');
-        return Promise.reject(new Error('Did not throw Error.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.admob().setRequestConfiguration(*) 'requestConfiguration' expected an object value",
-        );
-        return Promise.resolve();
-      }
+      // @ts-ignore
+      expect(() => firebase.admob().setRequestConfiguration('123')).toThrowError(
+        "firebase.admob().setRequestConfiguration(*) 'requestConfiguration' expected an object value",
+      );
     });
 
     describe('maxAdContentRating', () => {
       it('throws if maxAdContentRating is invalid', () => {
-        try {
+        expect(() =>
           firebase.admob().setRequestConfiguration({
             maxAdContentRating: 'Y',
-          });
-          return Promise.reject(new Error('Did not throw Error.'));
-        } catch (e) {
-          expect(e.message).toEqual(
-            "firebase.admob().setRequestConfiguration(*) 'requestConfiguration.maxAdContentRating' expected on of MaxAdContentRating.G, MaxAdContentRating.PG, MaxAdContentRating.T or MaxAdContentRating.MA",
-          );
-          return Promise.resolve();
-        }
+          }),
+        ).toThrowError(
+          "firebase.admob().setRequestConfiguration(*) 'requestConfiguration.maxAdContentRating' expected on of MaxAdContentRating.G, MaxAdContentRating.PG, MaxAdContentRating.T or MaxAdContentRating.MA",
+        );
       });
     });
 
     describe('tagForChildDirectedTreatment', () => {
       it('throws if tagForChildDirectedTreatment not a boolean', () => {
-        try {
+        expect(() =>
           firebase.admob().setRequestConfiguration({
             // @ts-ignore
             tagForChildDirectedTreatment: 'true',
-          });
-          return Promise.reject(new Error('Did not throw Error.'));
-        } catch (e) {
-          expect(e.message).toEqual(
-            "firebase.admob().setRequestConfiguration(*) 'requestConfiguration.tagForChildDirectedTreatment' expected a boolean value",
-          );
-          return Promise.resolve();
-        }
+          }),
+        ).toThrowError(
+          "firebase.admob().setRequestConfiguration(*) 'requestConfiguration.tagForChildDirectedTreatment' expected a boolean value",
+        );
       });
     });
 
     describe('tagForUnderAgeOfConsent', () => {
       it('throws if tagForUnderAgeOfConsent not a boolean', () => {
-        try {
+        expect(() =>
           firebase.admob().setRequestConfiguration({
             // @ts-ignore
             tagForUnderAgeOfConsent: 'false',
-          });
-          return Promise.reject(new Error('Did not throw Error.'));
-        } catch (e) {
-          expect(e.message).toEqual(
-            "firebase.admob().setRequestConfiguration(*) 'requestConfiguration.tagForUnderAgeOfConsent' expected a boolean value",
-          );
-          return Promise.resolve();
-        }
+          }),
+        ).toThrowError(
+          "firebase.admob().setRequestConfiguration(*) 'requestConfiguration.tagForUnderAgeOfConsent' expected a boolean value",
+        );
       });
     });
   });
--- packages/admob/__tests__/consent.test.ts
+++ packages/admob/__tests__/consent.test.ts
@@ -1,43 +1,25 @@
-import admob, { AdsConsent, firebase } from '../lib';
+import { AdsConsent } from '../lib';
 
 describe('Admob AdsConsent', () => {
   describe('requestInfoUpdate', () => {
     it('throws if publisherIds is not an array', () => {
-      try {
-        // @ts-ignore
-        AdsConsent.requestInfoUpdate('pub-123');
-        return Promise.reject(new Error('Did not throw Error.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.admob.AdsConsent.requestInfoUpdate(*) 'publisherIds' expected an array of string values.",
-        );
-        return Promise.resolve();
-      }
+      // @ts-ignore
+      expect(() => AdsConsent.requestInfoUpdate('pub-123')).toThrowError(
+        "firebase.admob.AdsConsent.requestInfoUpdate(*) 'publisherIds' expected an array of string values.",
+      );
     });
 
     it('throws if publisherIds is empty array', () => {
-      try {
-        AdsConsent.requestInfoUpdate([]);
-        return Promise.reject(new Error('Did not throw Error.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.admob.AdsConsent.requestInfoUpdate(*) 'publisherIds' list of publisher IDs cannot be empty.",
-        );
-        return Promise.resolve();
-      }
+      expect(() => AdsConsent.requestInfoUpdate([])).toThrowError(
+        "firebase.admob.AdsConsent.requestInfoUpdate(*) 'publisherIds' list of publisher IDs cannot be empty.",
+      );
     });
 
     it('throws if publisherIds contains non-string values', () => {
-      try {
-        // @ts-ignore
-        AdsConsent.requestInfoUpdate(['foo', 123]);
-        return Promise.reject(new Error('Did not throw Error.'));
-      } catch (e) {
-        expect(e.message).toEqual(
-          "firebase.admob.AdsConsent.requestInfoUpdate(*) 'publisherIds[1]' expected a string value.",
-        );
-        return Promise.resolve();
-      }
+      // @ts-ignore
+      expect(() => AdsConsent.requestInfoUpdate(['foo', 123])).toThrowError(
+        "firebase.admob.AdsConsent.requestInfoUpdate(*) 'publisherIds[1]' expected a string value.",
+      );
     });
   });
 });
