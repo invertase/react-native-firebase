37f6ac392c17f2345aaa8d139afd695df3b46913
russellwheatley <russellwheatley85@gmail.com>
2020-06-23 12:49:46 +0100

    docs(remote-config): further updates

--- packages/remote-config/lib/index.d.ts
+++ packages/remote-config/lib/index.d.ts
@@ -475,6 +475,13 @@ export namespace FirebaseRemoteConfigTypes {
      * Deletes all activated, fetched and defaults configs and resets all Firebase Remote Config settings.
      * @android Android only - iOS returns Promise<null> but does not reset anything
      *
+     * #### Example
+     *
+     * ```js
+     * await firebase.remoteConfig().reset();
+     * // get remote config values
+     * ```
+     *
      */
     reset(): Promise<null>;
   }
