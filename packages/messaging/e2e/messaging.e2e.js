7e9d2528537f6b7d7dacd1406c0ccc25b335d074
Russell Wheatley <russellwheatley85@gmail.com>
2020-07-23 15:36:26 +0100

    refactor: remove deprecated code (#3953)
    * chore(storage): rm downloadFile api
    
    * chore(messaging): rm deprecated methods
    
    * chore(dynamic-links): rm createDynamicLink
    
    * chore(dynamic-links): rm createShortDynamicLink
    
    * rm isRegisteredForRemoteNotifications
    
    * chore(app): rm deprecrated util.paths
    
    * chore: remove comment

--- packages/messaging/e2e/messaging.e2e.js
+++ packages/messaging/e2e/messaging.e2e.js
@@ -45,14 +45,10 @@ describe('messaging()', () => {
   describe('isDeviceRegisteredForRemoteMessages', () => {
     android.it('returns true on android', () => {
       should.equal(firebase.messaging().isDeviceRegisteredForRemoteMessages, true);
-      // check deprecated method also
-      should.equal(firebase.messaging().isRegisteredForRemoteNotifications, true);
     });
     it('defaults to false on ios before registering', () => {
       if (device.getPlatform() === 'ios') {
         should.equal(firebase.messaging().isDeviceRegisteredForRemoteMessages, false);
-        // check deprecated method also
-        should.equal(firebase.messaging().isRegisteredForRemoteNotifications, false);
       }
     });
   });
@@ -60,8 +56,6 @@ describe('messaging()', () => {
   describe('unregisterDeviceForRemoteMessages', () => {
     android.it('resolves on android', async () => {
       await firebase.messaging().unregisterDeviceForRemoteMessages();
-      // check deprecated method also
-      await firebase.messaging().unregisterForRemoteNotifications();
     });
   });
 
@@ -79,8 +73,6 @@ describe('messaging()', () => {
   describe('unregisterDeviceForRemoteMessages', () => {
     android.it('resolves on android', async () => {
       await firebase.messaging().unregisterDeviceForRemoteMessages();
-      // check deprecated method also
-      await firebase.messaging().unregisterForRemoteNotifications();
     });
   });
 
@@ -176,8 +168,6 @@ describe('messaging()', () => {
       const unsubscribe = firebase.messaging().onMessage(spy);
       if (device.getPlatform() === 'ios') {
         await firebase.messaging().registerDeviceForRemoteMessages();
-        // call deprecated method also
-        await firebase.messaging().registerForRemoteNotifications();
       }
       const token = await firebase.messaging().getToken();
       await TestsAPI.messaging().sendToDevice(token, {
