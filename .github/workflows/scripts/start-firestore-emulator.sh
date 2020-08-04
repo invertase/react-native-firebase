e51f3eab46f93f4865524c9be39addf39e9343fc
russellwheatley <russellwheatley85@gmail.com>
2020-08-04 10:36:45 +0100

    chore(firestore): update script

--- .github/workflows/scripts/start-firestore-emulator.sh
+++ .github/workflows/scripts/start-firestore-emulator.sh
@@ -4,4 +4,11 @@ if ! [ -x "$(command -v firebase)" ]; then
   exit 1
 fi
 
-firebase emulators:start 
+firebase emulators:start --only firestore &
+
+until curl --output /dev/null --silent --fail http://localhost:8080; do
+  echo "Waiting for Firestore emulator to come online..."
+  sleep 2
+done
+
+echo "Firestore emulator is online!" 
