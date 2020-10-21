60eb53881380fdac2ba7ffd0499cff95d9dfd8c7
Ryoga Kitagawa <ryoga.kitagawa@gmail.com>
2020-10-21 22:01:40 +0900

    fix(crashlytics, ios): generate uncatchable crash for iOS crash testing (#4426)
    Previous solutions to test crashing not being reported did not work in either or both of debug and release

--- packages/crashlytics/ios/RNFBCrashlytics/RNFBCrashlyticsModule.m
+++ packages/crashlytics/ios/RNFBCrashlytics/RNFBCrashlyticsModule.m
@@ -54,9 +54,9 @@ RCT_EXPORT_METHOD(checkForUnsentReports:
 RCT_EXPORT_METHOD(crash) {
   if ([RNFBCrashlyticsInitProvider isCrashlyticsCollectionEnabled]) {
     // https://firebase.google.com/docs/crashlytics/test-implementation?platform=ios recommends using "@[][1]" to crash,
-    // but that gets caught by react-native and shown as a red box for debug builds. Throw a different kind exception
-    // here to generate a hard crash.
-    @throw NSInternalInconsistencyException;
+    // but that gets caught by react-native and shown as a red box for debug builds. Raise SIGSEGV here to generate a hard crash.
+    int *p = 0;
+    *p = 0;
   }
 }
 
