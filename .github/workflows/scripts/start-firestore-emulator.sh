9f8b09787117d708c359dc56239b28003c5a4fc3
Mike Hardy <github@mikehardy.net>
2020-11-09 11:42:52 -0500

    fix(tests, emulator): centralize startup, correct CWD

--- .github/workflows/scripts/start-firestore-emulator.sh
+++ .github/workflows/scripts/start-firestore-emulator.sh
@@ -4,11 +4,15 @@ if ! [ -x "$(command -v firebase)" ]; then
   exit 1
 fi
 
-firebase emulators:start --only firestore &
+EMU_START_COMMAND="firebase emulators:start --only firestore"
 
-until curl --output /dev/null --silent --fail http://localhost:8080; do
-  echo "Waiting for Firestore emulator to come online..."
-  sleep 2
-done
-
-echo "Firestore emulator is online!" 
+if [ "$1" == "--no-daemon" ]; then
+  $EMU_START_COMMAND
+else
+  $EMU_START_COMMAND &
+  until curl --output /dev/null --silent --fail http://localhost:8080; do
+    echo "Waiting for Firestore emulator to come online..."
+    sleep 2
+  done
+  echo "Firestore emulator is online!"
+fi
