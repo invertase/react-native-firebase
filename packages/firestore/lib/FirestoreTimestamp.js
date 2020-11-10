1fd019f8d56cc0445451d3dccf94126f50bed5c5
Pokey Rule <pokey.rule@gmail.com>
2020-11-10 14:20:10 +0000

    feat(firestore): add toJSON() and valueOf() to FirestoreTimestamp (#4439)
    * Add toJSON() and valueOf() to FirestoreTimestamp
    
    For parity with JS SDK, and also to make comparisons work as expected.  Copied from https://github.com/firebase/firebase-js-sdk/blob/7c1c7f182b59e0fc7d175f53e5e2360cdee0ccab/packages/firestore/src/api/timestamp.ts#L162-L182.
    
    Please lmk if this is something you'd be interested in including, and I'd be happy to add a unit test
    
    * Update packages/firestore/lib/FirestoreTimestamp.js
    
    Co-authored-by: Mike Hardy <github@mikehardy.net>

--- packages/firestore/lib/FirestoreTimestamp.js
+++ packages/firestore/lib/FirestoreTimestamp.js
@@ -98,4 +98,26 @@ export default class FirestoreTimestamp {
   toString() {
     return `FirestoreTimestamp(seconds=${this.seconds}, nanoseconds=${this.nanoseconds})`;
   }
+
+  toJSON(): { seconds: number, nanoseconds: number } {
+    return { seconds: this.seconds, nanoseconds: this.nanoseconds };
+  }
+
+  /**
+   * Converts this object to a primitive string, which allows Timestamp objects to be compared
+   * using the `>`, `<=`, `>=` and `>` operators.
+   */
+  valueOf(): string {
+    // This method returns a string of the form <seconds>.<nanoseconds> where <seconds> is
+    // translated to have a non-negative value and both <seconds> and <nanoseconds> are left-padded
+    // with zeroes to be a consistent length. Strings with this format then have a lexiographical
+    // ordering that matches the expected ordering. The <seconds> translation is done to avoid
+    // having a leading negative sign (i.e. a leading '-' character) in its string representation,
+    // which would affect its lexiographical ordering.
+    const adjustedSeconds = this.seconds - MIN_SECONDS;
+    // Note: Up to 12 decimal digits are required to represent all valid 'seconds' values.
+    const formattedSeconds = String(adjustedSeconds).padStart(12, '0');
+    const formattedNanoseconds = String(this.nanoseconds).padStart(9, '0');
+    return formattedSeconds + '.' + formattedNanoseconds;
+  }
 }
