98e63635c6d7e183e393c6a472ac634400e56d54
tihimsm <tihimsm@gmail.com>
2020-11-10 11:34:39 +0900

    refactor: the part pointed out by lint

--- packages/crashlytics/react-native.config.js
+++ packages/crashlytics/react-native.config.js
@@ -25,8 +25,8 @@ module.exports = {
             execution_position: 'after_compile',
             input_files: [
               '${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${TARGET_NAME}',
-              '$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)'
-            ]
+              '$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)',
+            ],
           },
         ],
       },
