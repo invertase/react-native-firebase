b4512d7f24d71bce9a5914c34c56ab85e9cd7361
Mike Hardy <github@mikehardy.net>
2020-12-17 20:33:02 -0500

    test(lint): add mocha recommended lint rules
    this currently causes an incredibly large number
    of failures, they will be cleaned in following
    commits

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    "plugin:react/recommended",
+    'plugin:react/recommended',
+    'plugin:mocha/recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
