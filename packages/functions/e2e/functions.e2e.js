8cc113d1fb2e4aa49d2f355e032234dcf87b37d6
dackers86 <ackers86@hotmail.com>
2021-03-01 09:23:46 +0000

    Updated function name for customUrl test

--- packages/functions/e2e/functions.e2e.js
+++ packages/functions/e2e/functions.e2e.js
@@ -54,17 +54,17 @@ describe('functions()', function () {
 
     it('accepts passing in a custom url string as first arg to an app', async function () {
       const customUrl = 'https://us-central1-react-native-firebase-testing.cloudfunctions.net';
-      const functionsForRegion = firebase.app().functions(customUrl);
+      const functionsForCustromUrl = firebase.app().functions(customUrl);
 
-      functionsForRegion._customUrlOrRegion.should.equal(customUrl);
-      functionsForRegion.app.should.equal(firebase.app());
-      functionsForRegion.app.name.should.equal(firebase.app().name);
+      functionsForCustromUrl._customUrlOrRegion.should.equal(customUrl);
+      functionsForCustromUrl.app.should.equal(firebase.app());
+      functionsForCustromUrl.app.name.should.equal(firebase.app().name);
 
       firebase.app().functions(customUrl).app.should.equal(firebase.app());
 
       firebase.app().functions(customUrl)._customUrlOrRegion.should.equal(customUrl);
 
-      const functionRunner = functionsForRegion.httpsCallable('testFunctionDefaultRegion');
+      const functionRunner = functionsForCustromUrl.httpsCallable('testFunctionDefaultRegion');
 
       const response = await functionRunner();
       response.data.should.equal('null');
