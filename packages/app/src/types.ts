650fd2109111898d242a137652845d14aeb3eca6
russellwheatley <russellwheatley85@gmail.com>
2021-03-09 10:48:00 +0000

    update types

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -1,3 +1,5 @@
+import { FirebaseModule } from "./internal";
+
 export type DeleteAppType = () => Promise<never | void>;
 
 export interface FirebaseAppTypes {
@@ -36,11 +38,32 @@ export interface FirebaseAppMyVersion {
   ): Promise<FirebaseAppMyVersion>;
 }
 
+export type NamespaceTypes = 'admob' |'auth' | 'analytics' | 'remoteConfig' | 'crashlytics' | 'database' | 'inAppMessaging' | 'firestore' | 'functions' | 'iid' | 'indexing' | 'storage' | 'dynamicLinks' | 'messaging' | 'naturalLanguage' | 'ml' | 'notifications' | 'perf' | 'utils';
+
+export interface FirebaseModuleNamespace {
+  statics:any; //this prop will have to be removed & extended for each namespace as each package has its own statics type
+  version: string;
+  namespace: NamespaceTypes;
+  nativeModuleName:any; //this prop will have to be removed & extended for each namespace as each package has its own statics type
+  nativeEvents: any;//this prop will have to be removed & extended for each namespace as each package has its own statics type
+  hasMultiAppSupport: boolean;
+  hasCustomUrlOrRegionSupport: boolean;
+  ModuleClass: FirebaseModule;
+}
 
   /**
    * A class that all React Native Firebase modules extend from to provide default behaviour.
    */
    export interface FirebaseModuleTypes {
+     /**
+   * The native module instance for this Firebase service.
+   */
+    readonly native: any;
+
+    /**
+     * Returns the shared event emitter instance used for all JS event routing.
+     */
+    readonly emitter: any;
     /**
      * The current `FirebaseApp` instance for this Firebase service.
      */
