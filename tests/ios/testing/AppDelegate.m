2378d756f6e3c217d81698a62127be4098b6c6e8
Mike Hardy <github@mikehardy.net>
2021-03-16 17:58:27 -0500

    test: update tests module to react-native 0.64

--- tests/ios/testing/AppDelegate.m
+++ tests/ios/testing/AppDelegate.m
@@ -40,7 +40,11 @@
                                                    launchOptions:launchOptions];
 
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+      rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+      rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
