ac9a8c2caea3ea392343ac18c8e99cc3da353acf
ehesp <elliot.hesp@gmail.com>
2021-04-15 09:21:19 +0100

    add missing import

--- packages/firestore/lib/FirestoreDocumentReference.js
+++ packages/firestore/lib/FirestoreDocumentReference.js
@@ -15,7 +15,7 @@
  *
  */
 
-import { isObject, isString, isUndefined } from '@react-native-firebase/app/lib/common';
+import { isObject, isString, isUndefined, isNull } from '@react-native-firebase/app/lib/common';
 import NativeError from '@react-native-firebase/app/lib/internal/NativeFirebaseError';
 import {
   parseSetOptions,
