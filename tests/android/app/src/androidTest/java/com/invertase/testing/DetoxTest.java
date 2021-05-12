6ef437cf5523353df95d57b8631a9fef0b33bfd3
Mike Hardy <github@mikehardy.net>
2021-05-11 23:58:16 -0500

    test(android, jacoco): dump coverage file after running Detox tests
    This should not be necessary but in practice relying on the InstrumentationRunner
    to dump coverage during it's finish lifecycle transition is unreliable because of
    a UIAutomator bug that crashes the process before coverage is dumped
    
    Rather than rely on that, we dump it ourselves manually

--- tests/android/app/src/androidTest/java/com/invertase/testing/DetoxTest.java
+++ tests/android/app/src/androidTest/java/com/invertase/testing/DetoxTest.java
@@ -1,5 +1,9 @@
 package com.invertase.testing;
 
+import java.io.File;
+import java.lang.reflect.Method;
+
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -23,5 +27,26 @@ public class DetoxTest {
   @Test
   public void runDetoxTests() {
     Detox.runTests(mActivityRule);
+    dumpCoverageData();
+  }
+
+  // If you send '-e coverage' as part of the instrumentation command line, it should dump coverage as the Instrumentation finishes.
+  // However, there is a long-standing UIAutomator bug that crashes the process during Instrumentation.finish, before dumping coverage.
+  // It is trivial to dump it ourselves though, using the same mechanism the AndroidJUnit4Runner woud use
+  // Code reference: https://android.googlesource.com/platform/frameworks/testing/+/refs/heads/master/support/src/android/support/test/internal/runner/listener/CoverageListener.java#68
+  // UIAutomator issue: https://github.com/android/testing-samples/issues/89
+  private void dumpCoverageData() {
+    String coverageFilePath = InstrumentationRegistry.getInstrumentation().getTargetContext().getFilesDir().getAbsolutePath() +
+                    File.separator + "coverage.ec";
+    File coverageFile = new File(coverageFilePath);
+    try {
+        Class<?> emmaRTClass = Class.forName("com.vladium.emma.rt.RT");
+        Method dumpCoverageMethod = emmaRTClass.getMethod("dumpCoverageData",
+                coverageFile.getClass(), boolean.class, boolean.class);
+        dumpCoverageMethod.invoke(null, coverageFile, false, false);
+        System.out.println("Dumped code coverage data to " + coverageFilePath);
+    } catch (Exception e) {
+      System.err.println("Unable to dump coverage report: " + e);
+    }
   }
 }
