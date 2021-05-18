50bd1067496391ec8d0c1699108c7f9c63ee6904
Mike Hardy <github@mikehardy.net>
2021-05-18 17:40:05 -0500

    test(e2d, emulator): fix failure detection on emulator startup
    This was contributing to E2E flakiness, Related #4058

--- .github/workflows/scripts/start-firebase-emulator.sh
+++ .github/workflows/scripts/start-firebase-emulator.sh
@@ -5,14 +5,36 @@ if ! [ -x "$(command -v firebase)" ]; then
 fi
 
 EMU_START_COMMAND="firebase emulators:start --only auth,database,firestore --project react-native-firebase-testing"
+#EMU_START_COMMAND="sleep 120"
+MAX_RETRIES=3
+MAX_CHECKATTEMPTS=60
+CHECKATTEMPTS_WAIT=1
 
-if [ "$1" == "--no-daemon" ]; then
-  $EMU_START_COMMAND
-else
-  $EMU_START_COMMAND &
-  until curl --output /dev/null --silent --fail http://localhost:8080; do
-    echo "Waiting for Firestore emulator to come online..."
-    sleep 2
-  done
-  echo "Firestore emulator is online!"
-fi
+
+RETRIES=1
+while [ $RETRIES -le $MAX_RETRIES ]; do
+
+  if [ "$1" == "--no-daemon" ]; then
+    echo "Starting Firebase Emulator Suite in foreground."
+    $EMU_START_COMMAND
+  else
+    echo "Starting Firebase Emulator Suite in background."
+    $EMU_START_COMMAND &
+    CHECKATTEMPTS=1
+    while [ $CHECKATTEMPTS -le $MAX_CHECKATTEMPTS ]; do
+      sleep $CHECKATTEMPTS_WAIT
+      if curl --output /dev/null --silent --fail http://localhost:8080; then
+        echo "Firebase Emulator Suite is online!"
+        exit 0;
+      fi
+      echo "Waiting for Firebase Emulator Suite to come online, check $CHECKATTEMPTS of $MAX_CHECKATTEMPTS..."
+      ((CHECKATTEMPTS = CHECKATTEMPTS + 1))
+    done
+  fi
+
+  echo "Firebase Emulator Suite did not come online in $MAX_CHECKATTEMPTS checks. Try $RETRIES of $MAX_RETRIES."
+  ((RETRIES = RETRIES + 1))
+
+done
+echo "Firebase Emulator Suite did not come online after $MAX_RETRIES attempts."
+exit 1
