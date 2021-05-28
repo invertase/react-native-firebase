da65ca51c20f0aba509d129a6d6c4b8ce2f27ed9
Neal Manaktola <nealmanaktola@gmail.com>
2021-05-27 23:55:46 -0400

    call cancellation callback when using ref.on

--- packages/database/lib/DatabaseSyncTree.js
+++ packages/database/lib/DatabaseSyncTree.js
@@ -263,7 +263,8 @@ class DatabaseSyncTree {
 
     if (once) {
       const subscription = SharedEventEmitter.addListener(eventRegistrationKey, event => {
-        this._onOnceRemoveRegistration(eventRegistrationKey, listener, event);
+        const wrappedListener = this._onOnceRemoveRegistration(eventRegistrationKey, listener, event);
+        wrappedListener(event);
         subscription.remove();
       });
     } else {
