c439bf1ab427174f078599c58435c562829f19e5
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-07-07 08:06:46 +0200

    Add perf monitoring plugin

---
+++ packages/perf/plugin/src/android/constants.ts
@@ -0,0 +1,3 @@
+export const perfMonitoringClassPath = 'com.google.firebase:perf-plugin';
+export const perfMonitoringPlugin = 'com.google.firebase.firebase-perf';
+export const perfMonitoringVersion = '1.4.0';
