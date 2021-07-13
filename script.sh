873b6ed1dfa49677c6eae9ae0fc550d9cb753758
dackers86 <ackers86@hotmail.com>
2021-07-13 10:02:10 +0100

    Added script descripton

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# As we currently have two deployments of react-native-firebase. 
+# One is one producetion https://vercel.com/invertase/react-native-firebase
+# Another is new modules located at https://vercel.com/invertase/react-native-firebase-next
+
+# This script when combined with the ignore build step command https://vercel.com/invertase/react-native-firebase-next/settings/git
+# ensures that only the @invertase/next will cause a build on the new modules deployments.
+
+# This does not affect the main production deployment.
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
