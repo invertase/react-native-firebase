1a61d065e04f87d8b2657d4d173a80ab9284ea90
Hanno J. Gödecke <hgoedecke@cuvent.com>
2021-08-06 15:09:26 +0200

    fix: collection on DocumentReference generic param

--- packages/firestore/lib/index.d.ts
+++ packages/firestore/lib/index.d.ts
@@ -237,7 +237,7 @@ export namespace FirebaseFirestoreTypes {
      *
      * @param collectionPath A slash-separated path to a collection.
      */
-    collection(collectionPath: string): CollectionReference;
+    collection<T extends DocumentData = DocumentData>(collectionPath: string): CollectionReference<T>;
 
     /**
      * Deletes the document referred to by this DocumentReference.
