7ae1e788f02c23fcc5ebdfb0f7d20af129d05ad0
Mike Hardy <github@mikehardy.net>
2021-08-15 12:52:17 -0500

    feat(app, config): implement app_data_collection_default_enabled firebase.json key
    implement SDK-wide default data collection toggle capability from firebase.json

--- packages/app/lib/index.d.ts
+++ packages/app/lib/index.d.ts
@@ -122,7 +122,10 @@ export namespace ReactNativeFirebase {
     name?: string;
 
     /**
-     *
+     * Default setting for data collection on startup that affects all Firebase module startup data collection settings,
+     * in the absence of module-specific overrides. This will start as false if you set "app_data_collection_default_enabled"
+     * to false in firebase.json and may be used in opt-in flows, for example a GDPR-compliant app.
+     * If configured false initially, set to true after obtaining consent, then enable module-specific settings as needed afterwards.
      */
     automaticDataCollectionEnabled?: boolean;
 
