cdd8489402c830e76a1b059125833418dddb2028
Serhii Mikheiev <smikheiev@users.noreply.github.com>
2021-09-02 09:14:10 +0300

    fix: correct path to 'Info.plist' in ios build script input #5677
    The path used in the script is `_TARGET_PLIST="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`, but the path specified as "input files" was `$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)`. This caused that sometimes values from `firebase.json` were not taken into account on iOS, because build step "Processing Info.plist" could be executed after "[RNFB] Core Configuration" and it could overwrite the `Info.plist` file.

--- packages/app/react-native.config.js
+++ packages/app/react-native.config.js
@@ -10,7 +10,7 @@ module.exports = {
             name: '[RNFB] Core Configuration',
             path: './ios_config.sh',
             execution_position: 'after_compile',
-            input_files: ['$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)'],
+            input_files: ['$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)'],
           },
         ],
       },
