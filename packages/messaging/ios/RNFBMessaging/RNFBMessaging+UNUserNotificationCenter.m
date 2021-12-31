dbaafebfe4f9a154793766ebfb1bf647745603d8
Adrien Bayles <37045374+baylesa-dev@users.noreply.github.com>
2021-12-31 03:56:01 +0100

    feat(messaging, ios): add provideAppNotificationSettings iOS permission / handler (#5972)
    * feat: add provideAppNotificationSettings iOS perm
    * feat: impl. openSettingsForNotification handler
    * feat: add impl. for quit state
    * doc: improve documentation for feature
    * fix: return silently on android

--- packages/messaging/ios/RNFBMessaging/RNFBMessaging+UNUserNotificationCenter.m
+++ packages/messaging/ios/RNFBMessaging/RNFBMessaging+UNUserNotificationCenter.m
@@ -33,6 +33,7 @@ struct {
   dispatch_once(&once, ^{
     sharedInstance = [[RNFBMessagingUNUserNotificationCenter alloc] init];
     sharedInstance.initialNotification = nil;
+    sharedInstance.didOpenSettingsForNotification = NO;
   });
   return sharedInstance;
 }
@@ -66,6 +67,15 @@ struct {
   }
 
   return nil;
+}
+
+- (NSNumber *)getDidOpenSettingsForNotification {
+  if (_didOpenSettingsForNotification != NO) {
+    _didOpenSettingsForNotification = NO;
+    return @YES;
+  }
+
+  return @NO;
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
@@ -119,7 +129,15 @@ struct {
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center
     openSettingsForNotification:(nullable UNNotification *)notification {
   if (_originalDelegate != nil && originalDelegateRespondsTo.openSettingsForNotification) {
-    [_originalDelegate userNotificationCenter:center openSettingsForNotification:notification];
+    if (@available(iOS 12.0, *)) {
+      [_originalDelegate userNotificationCenter:center openSettingsForNotification:notification];
+    }
+  } else {
+    NSDictionary *notificationDict = [RNFBMessagingSerializer notificationToDict:notification];
+    [[RNFBRCTEventEmitter shared] sendEventWithName:@"messaging_settings_for_notification_opened"
+                                               body:notificationDict];
+
+    _didOpenSettingsForNotification = YES;
   }
 }
 
