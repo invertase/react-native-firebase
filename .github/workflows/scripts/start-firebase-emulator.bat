c66281c492ce24abbc811ea21a476f5e047c0760
Mike Hardy <github@mikehardy.net>
2022-02-27 12:32:10 -0500

    test(e2e, windows): allow android e2e to run on windows
    there was a serious problem with config file discovery for
    the firebase emulator suite, this forces the location, then
    all the tests for android appear to work well
    
    there was a second problem where the emulator UI port was
    taken, this lets it float

--- .github/workflows/scripts/start-firebase-emulator.bat
+++ .github/workflows/scripts/start-firebase-emulator.bat
@@ -1,1 +1,6 @@
-yarn firebase emulators:start --only auth,database,firestore,functions,storage --project react-native-firebase-testing
+@REM this pushd is likely not needed, but just in case
+pushd "%~dp0"
+@REM this is just to see what our current directory is. Should be .github/workflow/scripts
+echo %cd%
+@REM strangely, unless you specify the config file as being right in the current directory, it won't find it, and everything fails
+yarn firebase emulators:start --config %cd%\firebase.json --only auth,database,firestore,functions,storage --project react-native-firebase-testing
