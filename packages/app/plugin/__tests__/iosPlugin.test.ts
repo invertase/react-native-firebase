6f2d7e1608d04613b77461f9647802aa1058e6cc
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-04-26 00:04:53 +0200

    fix(app, expo): Support RN 0.68 Obj-C++ AppDelegate (#6213)
    * [app][expo] Support RN 0.68 Obj-C++ AppDelegate
    * Update `@expo/config-plugins` to `4.1.1`
    * Make AppDelegate plugin testable
    * Update tests

--- packages/app/plugin/__tests__/iosPlugin.test.ts
+++ packages/app/plugin/__tests__/iosPlugin.test.ts
@@ -1,9 +1,15 @@
+import { IOSConfig } from '@expo/config-plugins';
+import { AppDelegateProjectFile } from '@expo/config-plugins/build/ios/Paths';
 import fs from 'fs/promises';
 import path from 'path';
 
-import { modifyObjcAppDelegate } from '../src/ios/appDelegate';
+import { modifyAppDelegateAsync, modifyObjcAppDelegate } from '../src/ios/appDelegate';
 
 describe('Config Plugin iOS Tests', function () {
+  beforeEach(function () {
+    jest.resetAllMocks();
+  });
+
   it('tests changes made to old AppDelegate.m (SDK 42)', async function () {
     const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk42.m'), {
       encoding: 'utf8',
@@ -40,5 +46,43 @@ describe('Config Plugin iOS Tests', function () {
     );
     const result = modifyObjcAppDelegate(appDelegate);
     expect(result).toMatchSnapshot();
+  });
+
+  it('works with AppDelegate.mm (RN 0.68+)', async function () {
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk45.mm'), {
+      encoding: 'utf8',
+    });
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('detects Objective-C++ AppDelegate.mm', async function () {
+    jest.spyOn(fs, 'writeFile').mockImplementation();
+
+    const appDelegatePath = path.join(__dirname, './fixtures/AppDelegate_sdk45.mm');
+    const appDelegateFileInfo = IOSConfig.Paths.getFileInfo(
+      appDelegatePath,
+    ) as AppDelegateProjectFile;
+
+    await modifyAppDelegateAsync(appDelegateFileInfo);
+
+    // expect file contents to be modified
+    expect(fs.writeFile).toHaveBeenCalledWith(
+      appDelegateFileInfo.path,
+      expect.not.stringContaining(appDelegateFileInfo.contents),
+    );
+  });
+
+  it("doesn't support Swift AppDelegate", async function () {
+    jest.spyOn(fs, 'writeFile').mockImplementation();
+
+    const appDelegateFileInfo: AppDelegateProjectFile = {
+      path: '.',
+      language: 'swift',
+      contents: 'some dummy content',
+    };
+
+    await expect(modifyAppDelegateAsync(appDelegateFileInfo)).rejects.toThrow();
+    expect(fs.writeFile).not.toHaveBeenCalled();
   });
 });
