f5c0acf988b3f0d507b8b42e2950fad5c7d0527c
Elliot Hesp <elliot.hesp@gmail.com>
2022-08-02 09:33:37 +0100

    test(firestore): fix failing test

--- packages/firestore/e2e/GeoPoint.e2e.js
+++ packages/firestore/e2e/GeoPoint.e2e.js
@@ -109,10 +109,10 @@ describe('firestore.GeoPoint', function () {
 
   describe('toJSON()', function () {
     it('returns a json representation of the GeoPoint', function () {
-      const geo = new firebase.firestore.GeoPoint(100, 200);
+      const geo = new firebase.firestore.GeoPoint(30, 35);
       geo.toJSON().should.deepEqual({
-        latitude: 100,
-        longitude: 200,
+        latitude: 30,
+        longitude: 35,
       });
     });
   });
