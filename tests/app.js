81040b17eb540c93680547e666f3ae63ca0cf3bd
Mike Hardy <github@mikehardy.net>
2022-10-23 15:51:32 -0500

    test(e2e, functions): use non-deprecated functions emulator method

--- tests/app.js
+++ tests/app.js
@@ -46,7 +46,7 @@ firebase.database().useEmulator('localhost', 9000);
 firebase.auth().useEmulator('http://localhost:9099');
 firebase.firestore().useEmulator('localhost', 8080);
 firebase.storage().useEmulator('localhost', 9199);
-firebase.functions().useFunctionsEmulator('http://localhost:5001');
+firebase.functions().useEmulator('localhost', 5001);
 
 // Firestore caches docuuments locally (a great feature!) and that confounds tests
 // as data from previous runs pollutes following runs until re-install the app. Clear it.
