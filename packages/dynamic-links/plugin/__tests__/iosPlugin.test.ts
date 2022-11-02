e558ad78f62101be3549ab1c605654007b54a38f
Austin Harris <austin43@users.noreply.github.com>
2022-11-02 07:55:03 -0700

    fix(dynamic-links, ios): expo config plugin automates dynamic-links workaround (#6650)
    Related #2660
    
    Co-authored-by: Mike Hardy <github@mikehardy.net>

---
+++ packages/dynamic-links/plugin/__tests__/iosPlugin.test.ts
@@ -0,0 +1,108 @@
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { IOSConfig } from '@expo/config-plugins';
+import { AppDelegateProjectFile } from '@expo/config-plugins/build/ios/Paths';
+import fs from 'fs/promises';
+import path from 'path';
+
+import { modifyAppDelegateAsync, modifyObjcAppDelegate } from '../src/ios/appDelegate';
+
+describe('Config Plugin iOS Tests', function () {
+  beforeEach(function () {
+    jest.resetAllMocks();
+  });
+
+  it('tests changes made to old AppDelegate.m (SDK 42)', async function () {
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk42.m'), {
+      encoding: 'utf8',
+    });
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('tests changes made to AppDelegate.m (SDK 43)', async function () {
+    const appDelegate = await fs.readFile(
+      path.join(__dirname, './fixtures/AppDelegate_bare_sdk43.m'),
+      {
+        encoding: 'utf8',
+      },
+    );
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('tests changes made to AppDelegate.m with Expo ReactDelegate support (SDK 44+)', async function () {
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk44.m'), {
+      encoding: 'utf8',
+    });
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('tests changes made to AppDelegate.m with fallback regex (if the original one fails)', async function () {
+    const appDelegate = await fs.readFile(
+      path.join(__dirname, './fixtures/AppDelegate_fallback.m'),
+      {
+        encoding: 'utf8',
+      },
+    );
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('works with AppDelegate.mm (RN 0.68+)', async function () {
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk45.mm'), {
+      encoding: 'utf8',
+    });
+    const result = modifyObjcAppDelegate(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('detects Objective-C++ AppDelegate.mm', async function () {
+    jest.spyOn(fs, 'writeFile').mockImplementation(async () => {});
+
+    const appDelegatePath = path.join(__dirname, './fixtures/AppDelegate_sdk45.mm');
+    const appDelegateFileInfo = IOSConfig.Paths.getFileInfo(
+      appDelegatePath,
+    ) as AppDelegateProjectFile;
+
+    await modifyAppDelegateAsync(appDelegateFileInfo);
+
+    // expect file contents to be modified
+    expect(fs.writeFile).toHaveBeenCalledWith(
+      appDelegateFileInfo.path,
+      expect.not.stringContaining(appDelegateFileInfo.contents),
+    );
+  });
+
+  it("doesn't support Swift AppDelegate", async function () {
+    jest.spyOn(fs, 'writeFile').mockImplementation(async () => {});
+
+    const appDelegateFileInfo: AppDelegateProjectFile = {
+      path: '.',
+      language: 'swift',
+      contents: 'some dummy content',
+    };
+
+    await expect(modifyAppDelegateAsync(appDelegateFileInfo)).rejects.toThrow();
+    expect(fs.writeFile).not.toHaveBeenCalled();
+  });
+
+  it('does not add the firebase import multiple times', async function () {
+    const singleImport =
+      '#import "AppDelegate.h"\n#import <RNFBDynamicLinksAppDelegateInterceptor.h>';
+    const doubleImport = singleImport + '\n#import <RNFBDynamicLinksAppDelegateInterceptor.h>';
+
+    const appDelegate = await fs.readFile(path.join(__dirname, './fixtures/AppDelegate_sdk45.mm'), {
+      encoding: 'utf8',
+    });
+    expect(appDelegate).not.toContain(singleImport);
+
+    const onceModifiedAppDelegate = modifyObjcAppDelegate(appDelegate);
+    expect(onceModifiedAppDelegate).toContain(singleImport);
+    expect(onceModifiedAppDelegate).not.toContain(doubleImport);
+
+    const twiceModifiedAppDelegate = modifyObjcAppDelegate(onceModifiedAppDelegate);
+    expect(twiceModifiedAppDelegate).toContain(singleImport);
+    expect(twiceModifiedAppDelegate).not.toContain(doubleImport);
+  });
+});
