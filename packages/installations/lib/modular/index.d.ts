3bddaddfad671c2b74e079f95a451c070684469b
Nabeel Parkar <nabeelparkar99@gmail.com>
2023-05-03 13:38:39 +0530

    remove: version from package.json

--- packages/installations/lib/modular/index.d.ts
+++ packages/installations/lib/modular/index.d.ts
@@ -1,10 +1,16 @@
 import { ReactNativeFirebase } from '@react-native-firebase/app';
 import { FirebaseInstallationsTypes } from '../index';
 
+/**
+ * Returns an instance of Installations associated with the given FirebaseApp instance.
+ */
 export function getInstallations(
   app?: ReactNativeFirebase.FirebaseApp,
 ): FirebaseInstallationsTypes.Module;
 
+/**
+ * Deletes the Firebase Installation and all associated data.
+ */
 export function deleteInstallations(
   installations?: FirebaseInstallationsTypes.Module,
 ): Promise<void>;
--- packages/installations/lib/modular/index.js
+++ packages/installations/lib/modular/index.js
@@ -1,10 +1,5 @@
 import { firebase } from '..';
 
-/**
- * Returns an instance of Installations associated with the given FirebaseApp instance.
- * @param app - FirebaseApp. Optional.
- * @returns {Installations}
- */
 export function getInstallations(app) {
   if (app) {
     return firebase.app(app.name).installations();
@@ -12,11 +7,6 @@ export function getInstallations(app) {
   return firebase.app().installations();
 }
 
-/**
- * Deletes the Firebase Installation and all associated data.
- * @param installations
- * @returns {Promise<void>}
- */
 export function deleteInstallations(installations) {
   return installations.delete();
 }
