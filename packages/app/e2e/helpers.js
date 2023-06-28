cb495a8453a98644dbeb1656e80993f5d1cf11c7
Mike Hardy <github@mikehardy.net>
2023-06-28 16:50:39 -0500

    test(app): `localhost` is `::1` on some hosts, `127.0.0.1` is required
    without this, hosts with IPv6 resolve localhost to ::1 and the firebase
    emulator isn't listening there, so our helpers cannot connect

--- packages/app/e2e/helpers.js
+++ packages/app/e2e/helpers.js
@@ -7,5 +7,5 @@ exports.getE2eEmulatorHost = function getE2eEmulatorHost() {
   // localhost and 127.0.0.1 on Android to 10.0.2.2 (the Android emulator host interface)
   // But this specific code is executing in the host context even during E2E test.
   // So no re-write is necessary here.
-  return 'localhost';
+  return '127.0.0.1';
 };
