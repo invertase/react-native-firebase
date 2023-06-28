48b967478f310338ea4d0fcfa444653cccc68e1f
exaby73 <nabeelparkar99@gmail.com>
2023-06-28 14:03:51 +0530

    update docs for FieldPath and FieldValue

--- packages/firestore/lib/modular/FieldValue.d.ts
+++ packages/firestore/lib/modular/FieldValue.d.ts
@@ -1,3 +1,7 @@
+/**
+ * Sentinel values that can be used when writing document fields with `set()`
+ * or `update()`.
+ */
 export declare class FieldValue {
   isEqual(other: FieldValue): boolean;
 }
