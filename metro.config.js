ede9bb6f6e848692c36ee845e1fa65a33a94fa45
Mike Hardy <github@mikehardy.net>
2023-06-28 23:12:03 -0500

    build(deps): react-native 0.72

--- metro.config.js
+++ metro.config.js
@@ -1,1 +1,10 @@
-module.exports = require('./tests/metro.config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = require('./tests/metro.config');
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
