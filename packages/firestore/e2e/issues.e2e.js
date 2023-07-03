d2e4d551761c6a5a687a2103bcd54bc0182305c4
exaby73 <nabeelparkar99@gmail.com>
2023-07-03 14:28:45 +0530

    add modular API tests for issues.e2e.js

--- packages/firestore/e2e/issues.e2e.js
+++ packages/firestore/e2e/issues.e2e.js
@@ -34,125 +34,284 @@ const testNumbers = {
 };
 
 describe('firestore()', function () {
-  describe('issues', function () {
-    before(async function () {
-      await Promise.all([
-        firebase.firestore().doc(`${COLLECTION}/wbXwyLJheRfYXXWlY46j`).set({ index: 2, number: 2 }),
-        firebase.firestore().doc(`${COLLECTION}/kGC5cYPN1nKnZCcAb9oQ`).set({ index: 6, number: 2 }),
-        firebase.firestore().doc(`${COLLECTION}/8Ek8iWCDQPPJ5s2n8PiQ`).set({ index: 4, number: 2 }),
-        firebase.firestore().doc(`${COLLECTION}/mr7MdAygvuheF6AUtWma`).set({ index: 1, number: 1 }),
-        firebase.firestore().doc(`${COLLECTION}/RCO5SvNn4fdoE49OKrIV`).set({ index: 3, number: 1 }),
-        firebase.firestore().doc(`${COLLECTION}/CvVG7VP1hXTtcfdUaeNl`).set({ index: 5, number: 1 }),
-      ]);
-    });
+  describe('v8 compatibility', function () {
+    describe('issues', function () {
+      before(async function () {
+        await Promise.all([
+          firebase
+            .firestore()
+            .doc(`${COLLECTION}/wbXwyLJheRfYXXWlY46j`)
+            .set({ index: 2, number: 2 }),
+          firebase
+            .firestore()
+            .doc(`${COLLECTION}/kGC5cYPN1nKnZCcAb9oQ`)
+            .set({ index: 6, number: 2 }),
+          firebase
+            .firestore()
+            .doc(`${COLLECTION}/8Ek8iWCDQPPJ5s2n8PiQ`)
+            .set({ index: 4, number: 2 }),
+          firebase
+            .firestore()
+            .doc(`${COLLECTION}/mr7MdAygvuheF6AUtWma`)
+            .set({ index: 1, number: 1 }),
+          firebase
+            .firestore()
+            .doc(`${COLLECTION}/RCO5SvNn4fdoE49OKrIV`)
+            .set({ index: 3, number: 1 }),
+          firebase
+            .firestore()
+            .doc(`${COLLECTION}/CvVG7VP1hXTtcfdUaeNl`)
+            .set({ index: 5, number: 1 }),
+        ]);
+      });
 
-    it('returns all results', async function () {
-      const db = firebase.firestore();
-      const ref = db.collection(COLLECTION).orderBy('number', 'desc');
-      const allResultsSnapshot = await ref.get();
-      allResultsSnapshot.forEach((doc, i) => {
-        if (i === 0) {
-          doc.id.should.equal('wbXwyLJheRfYXXWlY46j');
-        }
-        if (i === 1) {
-          doc.id.should.equal('kGC5cYPN1nKnZCcAb9oQ');
-        }
-        if (i === 2) {
-          doc.id.should.equal('8Ek8iWCDQPPJ5s2n8PiQ');
-        }
-        if (i === 3) {
-          doc.id.should.equal('mr7MdAygvuheF6AUtWma');
-        }
-        if (i === 4) {
-          doc.id.should.equal('RCO5SvNn4fdoE49OKrIV');
-        }
-        if (i === 5) {
-          doc.id.should.equal('CvVG7VP1hXTtcfdUaeNl');
-        }
+      it('returns all results', async function () {
+        const db = firebase.firestore();
+        const ref = db.collection(COLLECTION).orderBy('number', 'desc');
+        const allResultsSnapshot = await ref.get();
+        allResultsSnapshot.forEach((doc, i) => {
+          if (i === 0) {
+            doc.id.should.equal('wbXwyLJheRfYXXWlY46j');
+          }
+          if (i === 1) {
+            doc.id.should.equal('kGC5cYPN1nKnZCcAb9oQ');
+          }
+          if (i === 2) {
+            doc.id.should.equal('8Ek8iWCDQPPJ5s2n8PiQ');
+          }
+          if (i === 3) {
+            doc.id.should.equal('mr7MdAygvuheF6AUtWma');
+          }
+          if (i === 4) {
+            doc.id.should.equal('RCO5SvNn4fdoE49OKrIV');
+          }
+          if (i === 5) {
+            doc.id.should.equal('CvVG7VP1hXTtcfdUaeNl');
+          }
+        });
+      });
+
+      it('returns first page', async function () {
+        const db = firebase.firestore();
+        const ref = db.collection(COLLECTION).orderBy('number', 'desc');
+        const firstPageSnapshot = await ref.limit(2).get();
+        should.equal(firstPageSnapshot.docs.length, 2);
+        firstPageSnapshot.forEach((doc, i) => {
+          if (i === 0) {
+            doc.id.should.equal('wbXwyLJheRfYXXWlY46j');
+          }
+          if (i === 1) {
+            doc.id.should.equal('kGC5cYPN1nKnZCcAb9oQ');
+          }
+        });
+      });
+
+      it('returns second page', async function () {
+        const db = firebase.firestore();
+        const ref = db.collection(COLLECTION).orderBy('number', 'desc');
+        const firstPageSnapshot = await ref.limit(2).get();
+        let lastDocument;
+        firstPageSnapshot.forEach(doc => {
+          lastDocument = doc;
+        });
+
+        const secondPageSnapshot = await ref.startAfter(lastDocument).limit(2).get();
+        should.equal(secondPageSnapshot.docs.length, 2);
+        secondPageSnapshot.forEach((doc, i) => {
+          if (i === 0) {
+            doc.id.should.equal('8Ek8iWCDQPPJ5s2n8PiQ');
+          }
+          if (i === 1) {
+            doc.id.should.equal('mr7MdAygvuheF6AUtWma');
+          }
+        });
       });
     });
 
-    it('returns first page', async function () {
-      const db = firebase.firestore();
-      const ref = db.collection(COLLECTION).orderBy('number', 'desc');
-      const firstPageSnapshot = await ref.limit(2).get();
-      should.equal(firstPageSnapshot.docs.length, 2);
-      firstPageSnapshot.forEach((doc, i) => {
-        if (i === 0) {
-          doc.id.should.equal('wbXwyLJheRfYXXWlY46j');
-        }
-        if (i === 1) {
-          doc.id.should.equal('kGC5cYPN1nKnZCcAb9oQ');
-        }
-      });
-    });
+    describe('number type consistency', function () {
+      before(async function () {
+        jsFirebase.initializeApp(FirebaseHelpers.app.config());
+        jsFirebase.firestore().useEmulator(getE2eEmulatorHost(), 8080);
 
-    it('returns second page', async function () {
-      const db = firebase.firestore();
-      const ref = db.collection(COLLECTION).orderBy('number', 'desc');
-      const firstPageSnapshot = await ref.limit(2).get();
-      let lastDocument;
-      firstPageSnapshot.forEach(doc => {
-        lastDocument = doc;
+        // Put one example of each number in our collection using JS SDK
+        await Promise.all(
+          Object.entries(testNumbers).map(([testName, testValue]) => {
+            return jsFirebase
+              .firestore()
+              .doc(`${COLLECTION}/numberTestsJS/cases/${testName}`)
+              .set({ number: testValue });
+          }),
+        );
+
+        // Put one example of each number in our collection using Native SDK
+        await Promise.all(
+          Object.entries(testNumbers).map(([testName, testValue]) => {
+            return firebase
+              .firestore()
+              .doc(`${COLLECTION}/numberTestsNative/cases/${testName}`)
+              .set({ number: testValue });
+          }),
+        );
       });
 
-      const secondPageSnapshot = await ref.startAfter(lastDocument).limit(2).get();
-      should.equal(secondPageSnapshot.docs.length, 2);
-      secondPageSnapshot.forEach((doc, i) => {
-        if (i === 0) {
-          doc.id.should.equal('8Ek8iWCDQPPJ5s2n8PiQ');
-        }
-        if (i === 1) {
-          doc.id.should.equal('mr7MdAygvuheF6AUtWma');
-        }
+      it('types inserted by JS may be queried by native with filters', async function () {
+        const testValues = Object.values(testNumbers);
+        const ref = firebase
+          .firestore()
+          .collection(`${COLLECTION}/numberTestsJS/cases`)
+          .where('number', 'in', testValues);
+        typesSnap = await ref.get();
+        should.deepEqual(typesSnap.docs.map(d => d.id).sort(), Object.keys(testNumbers).sort());
+      });
+
+      it('types inserted by native may be queried by JS with filters', async function () {
+        const testValues = Object.values(testNumbers);
+        const ref = jsFirebase
+          .firestore()
+          .collection(`${COLLECTION}/numberTestsNative/cases`)
+          .where('number', 'in', testValues);
+        typesSnap = await ref.get();
+        should.deepEqual(typesSnap.docs.map(d => d.id).sort(), Object.keys(testNumbers).sort());
       });
     });
   });
 
-  describe('number type consistency', function () {
-    before(async function () {
-      jsFirebase.initializeApp(FirebaseHelpers.app.config());
-      jsFirebase.firestore().useEmulator(getE2eEmulatorHost(), 8080);
+  describe('modular', function () {
+    describe('issues', function () {
+      before(async function () {
+        const { getFirestore, doc, setDoc } = firestoreModular;
+        const db = getFirestore();
 
-      // Put one example of each number in our collection using JS SDK
-      await Promise.all(
-        Object.entries(testNumbers).map(([testName, testValue]) => {
-          return jsFirebase
-            .firestore()
-            .doc(`${COLLECTION}/numberTestsJS/cases/${testName}`)
-            .set({ number: testValue });
-        }),
-      );
+        await Promise.all([
+          setDoc(doc(db, `${COLLECTION}/wbXwyLJheRfYXXWlY46j`), { index: 2, number: 2 }),
+          setDoc(doc(db, `${COLLECTION}/kGC5cYPN1nKnZCcAb9oQ`), { index: 6, number: 2 }),
+          setDoc(doc(db, `${COLLECTION}/8Ek8iWCDQPPJ5s2n8PiQ`), { index: 4, number: 2 }),
+          setDoc(doc(db, `${COLLECTION}/mr7MdAygvuheF6AUtWma`), { index: 1, number: 1 }),
+          setDoc(doc(db, `${COLLECTION}/RCO5SvNn4fdoE49OKrIV`), { index: 3, number: 1 }),
+          setDoc(doc(db, `${COLLECTION}/CvVG7VP1hXTtcfdUaeNl`), { index: 5, number: 1 }),
+        ]);
+      });
 
-      // Put one example of each number in our collection using Native SDK
-      await Promise.all(
-        Object.entries(testNumbers).map(([testName, testValue]) => {
-          return firebase
-            .firestore()
-            .doc(`${COLLECTION}/numberTestsNative/cases/${testName}`)
-            .set({ number: testValue });
-        }),
-      );
+      it('returns all results', async function () {
+        const { getFirestore, collection, query, orderBy, getDocs } = firestoreModular;
+        const db = getFirestore();
+
+        const ref = query(collection(db, COLLECTION), orderBy('number', 'desc'));
+        const allResultsSnapshot = await getDocs(ref);
+        allResultsSnapshot.forEach((doc, i) => {
+          if (i === 0) {
+            doc.id.should.equal('wbXwyLJheRfYXXWlY46j');
+          }
+          if (i === 1) {
+            doc.id.should.equal('kGC5cYPN1nKnZCcAb9oQ');
+          }
+          if (i === 2) {
+            doc.id.should.equal('8Ek8iWCDQPPJ5s2n8PiQ');
+          }
+          if (i === 3) {
+            doc.id.should.equal('mr7MdAygvuheF6AUtWma');
+          }
+          if (i === 4) {
+            doc.id.should.equal('RCO5SvNn4fdoE49OKrIV');
+          }
+          if (i === 5) {
+            doc.id.should.equal('CvVG7VP1hXTtcfdUaeNl');
+          }
+        });
+      });
+
+      it('returns first page', async function () {
+        const { getFirestore, collection, query, orderBy, limit, getDocs } = firestoreModular;
+        const db = getFirestore();
+
+        const ref = query(collection(db, COLLECTION), orderBy('number', 'desc'));
+        const firstPageSnapshot = await getDocs(query(ref, limit(2)));
+        should.equal(firstPageSnapshot.docs.length, 2);
+        firstPageSnapshot.forEach((doc, i) => {
+          if (i === 0) {
+            doc.id.should.equal('wbXwyLJheRfYXXWlY46j');
+          }
+          if (i === 1) {
+            doc.id.should.equal('kGC5cYPN1nKnZCcAb9oQ');
+          }
+        });
+      });
+
+      it('returns second page', async function () {
+        const { getFirestore, collection, query, orderBy, limit, startAfter, getDocs } =
+          firestoreModular;
+        const db = getFirestore();
+
+        const ref = query(collection(db, COLLECTION), orderBy('number', 'desc'));
+        const firstPageSnapshot = await getDocs(query(ref, limit(2)));
+        let lastDocument;
+        firstPageSnapshot.forEach(doc => {
+          lastDocument = doc;
+        });
+
+        const secondPageSnapshot = await getDocs(query(ref, startAfter(lastDocument), limit(2)));
+        should.equal(secondPageSnapshot.docs.length, 2);
+        secondPageSnapshot.forEach((doc, i) => {
+          if (i === 0) {
+            doc.id.should.equal('8Ek8iWCDQPPJ5s2n8PiQ');
+          }
+          if (i === 1) {
+            doc.id.should.equal('mr7MdAygvuheF6AUtWma');
+          }
+        });
+      });
     });
 
-    it('types inserted by JS may be queried by native with filters', async function () {
-      const testValues = Object.values(testNumbers);
-      const ref = firebase
-        .firestore()
-        .collection(`${COLLECTION}/numberTestsJS/cases`)
-        .where('number', 'in', testValues);
-      typesSnap = await ref.get();
-      should.deepEqual(typesSnap.docs.map(d => d.id).sort(), Object.keys(testNumbers).sort());
-    });
+    describe('number type consistency', function () {
+      before(async function () {
+        jsFirebase.initializeApp(FirebaseHelpers.app.config());
+        jsFirebase.firestore().useEmulator(getE2eEmulatorHost(), 8080);
 
-    it('types inserted by native may be queried by JS with filters', async function () {
-      const testValues = Object.values(testNumbers);
-      const ref = jsFirebase
-        .firestore()
-        .collection(`${COLLECTION}/numberTestsNative/cases`)
-        .where('number', 'in', testValues);
-      typesSnap = await ref.get();
-      should.deepEqual(typesSnap.docs.map(d => d.id).sort(), Object.keys(testNumbers).sort());
+        // Put one example of each number in our collection using JS SDK
+        await Promise.all(
+          Object.entries(testNumbers).map(([testName, testValue]) => {
+            return jsFirebase
+              .firestore()
+              .doc(`${COLLECTION}/numberTestsJS/cases/${testName}`)
+              .set({ number: testValue });
+          }),
+        );
+
+        const { getFirestore, doc, setDoc } = firestoreModular;
+
+        // Put one example of each number in our collection using Native SDK
+        await Promise.all(
+          Object.entries(testNumbers).map(([testName, testValue]) => {
+            return setDoc(
+              doc(getFirestore(), `${COLLECTION}/numberTestsNative/cases/${testName}`),
+              {
+                number: testValue,
+              },
+            );
+          }),
+        );
+      });
+
+      it('types inserted by JS may be queried by native with filters', async function () {
+        const { getFirestore, collection, query, where, getDocs } = firestoreModular;
+        const testValues = Object.values(testNumbers);
+        const ref = query(
+          collection(getFirestore(), `${COLLECTION}/numberTestsJS/cases`),
+          where('number', 'in', testValues),
+        );
+        const typesSnap = await getDocs(ref);
+        should.deepEqual(typesSnap.docs.map(d => d.id).sort(), Object.keys(testNumbers).sort());
+      });
+
+      it('types inserted by native may be queried by JS with filters', async function () {
+        const testValues = Object.values(testNumbers);
+        const ref = jsFirebase
+          .firestore()
+          .collection(`${COLLECTION}/numberTestsNative/cases`)
+          .where('number', 'in', testValues);
+        typesSnap = await ref.get();
+        should.deepEqual(typesSnap.docs.map(d => d.id).sort(), Object.keys(testNumbers).sort());
+      });
     });
   });
 });
