b34fabc13bc69349e0c99e6e4eea7456047aaf0d
exaby73 <nabeelparkar99@gmail.com>
2023-07-11 02:55:05 +0530

    add modular tests for get.e2e.js

--- packages/firestore/e2e/Query/get.e2e.js
+++ packages/firestore/e2e/Query/get.e2e.js
@@ -21,58 +21,94 @@ describe('firestore().collection().get()', function () {
   before(function () {
     return wipe();
   });
-  it('throws if get options is not an object', function () {
-    try {
-      firebase.firestore().collection(COLLECTION).get(123);
-      return Promise.reject(new Error('Did not throw an Error.'));
-    } catch (error) {
-      error.message.should.containEql("'options' must be an object is provided");
-      return Promise.resolve();
-    }
-  });
 
-  it('throws if get options.source is not valid', function () {
-    try {
-      firebase.firestore().collection(COLLECTION).get({
-        source: 'foo',
+  describe('v8 compatibility', function () {
+    it('throws if get options is not an object', function () {
+      try {
+        firebase.firestore().collection(COLLECTION).get(123);
+        return Promise.reject(new Error('Did not throw an Error.'));
+      } catch (error) {
+        error.message.should.containEql("'options' must be an object is provided");
+        return Promise.resolve();
+      }
+    });
+
+    it('throws if get options.source is not valid', function () {
+      try {
+        firebase.firestore().collection(COLLECTION).get({
+          source: 'foo',
+        });
+        return Promise.reject(new Error('Did not throw an Error.'));
+      } catch (error) {
+        error.message.should.containEql(
+          "'options' GetOptions.source must be one of 'default', 'server' or 'cache'",
+        );
+        return Promise.resolve();
+      }
+    });
+
+    it('returns a QuerySnapshot', async function () {
+      const docRef = firebase.firestore().collection(COLLECTION).doc('nestedcollection');
+      const colRef = docRef.collection('get');
+      const snapshot = await colRef.get();
+
+      snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
+    });
+
+    it('returns a correct cache setting (true)', async function () {
+      const docRef = firebase.firestore().collection(COLLECTION).doc('nestedcollection');
+      const colRef = docRef.collection('get');
+      const snapshot = await colRef.get({
+        source: 'cache',
       });
-      return Promise.reject(new Error('Did not throw an Error.'));
-    } catch (error) {
-      error.message.should.containEql(
-        "'options' GetOptions.source must be one of 'default', 'server' or 'cache'",
-      );
-      return Promise.resolve();
-    }
-  });
 
-  it('returns a QuerySnapshot', async function () {
-    const docRef = firebase.firestore().collection(COLLECTION).doc('nestedcollection');
-    const colRef = docRef.collection('get');
-    const snapshot = await colRef.get();
-
-    snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
-  });
-
-  it('returns a correct cache setting (true)', async function () {
-    const docRef = firebase.firestore().collection(COLLECTION).doc('nestedcollection');
-    const colRef = docRef.collection('get');
-    const snapshot = await colRef.get({
-      source: 'cache',
+      snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
+      snapshot.metadata.fromCache.should.be.True();
     });
 
-    snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
-    snapshot.metadata.fromCache.should.be.True();
+    it('returns a correct cache setting (false)', async function () {
+      const docRef = firebase.firestore().collection(COLLECTION).doc('nestedcollection');
+      const colRef = docRef.collection('get');
+      await colRef.get(); // Puts it in cache
+      const snapshot = await colRef.get({
+        source: 'server',
+      });
+
+      snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
+      snapshot.metadata.fromCache.should.be.False();
+    });
   });
 
-  it('returns a correct cache setting (false)', async function () {
-    const docRef = firebase.firestore().collection(COLLECTION).doc('nestedcollection');
-    const colRef = docRef.collection('get');
-    await colRef.get(); // Puts it in cache
-    const snapshot = await colRef.get({
-      source: 'server',
+  describe('modular', function () {
+    it('returns a QuerySnapshot', async function () {
+      const { getFirestore, collection, doc, getDocs } = firestoreModular;
+
+      const docRef = doc(collection(getFirestore(), COLLECTION), 'nestedcollection');
+      const colRef = collection(docRef, 'get');
+      const snapshot = await getDocs(colRef);
+
+      snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
     });
 
-    snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
-    snapshot.metadata.fromCache.should.be.False();
+    it('returns a correct cache setting (true)', async function () {
+      const { getFirestore, collection, doc, getDocsFromCache } = firestoreModular;
+      const docRef = doc(collection(getFirestore(), COLLECTION), 'nestedcollection');
+      const colRef = collection(docRef, 'get');
+      const snapshot = await getDocsFromCache(colRef);
+
+      snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
+      snapshot.metadata.fromCache.should.be.True();
+    });
+
+    it('returns a correct cache setting (false)', async function () {
+      const { getFirestore, collection, doc, getDocs, getDocsFromServer } = firestoreModular;
+      const docRef = doc(collection(getFirestore(), COLLECTION), 'nestedcollection');
+      const colRef = collection(docRef, 'get');
+      await getDocs(colRef); // Puts it in cache
+      const snapshot = await getDocsFromServer(colRef);
+
+      snapshot.constructor.name.should.eql('FirestoreQuerySnapshot');
+      snapshot.metadata.fromCache.should.be.False();
+    });
   });
 });
