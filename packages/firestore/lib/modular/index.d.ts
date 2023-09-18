5fbb3c420deb93298bc21061a74a59a2c293590a
exaby73 <nabeelparkar99@gmail.com>
2023-09-19 03:53:20 +0530

    fix(firestore): remove duplicate getFirestore TS declaration

--- packages/firestore/lib/modular/index.d.ts
+++ packages/firestore/lib/modular/index.d.ts
@@ -97,17 +97,6 @@ export type WithFieldValue<T> =
 
 /**
  * Returns the existing default {@link Firestore} instance that is associated with the
- * provided {@link @firebase/app#FirebaseApp}. If no instance exists, initializes a new
- * instance with default settings.
- *
- * @param app - The {@link @firebase/app#FirebaseApp} instance that the returned {@link Firestore}
- * instance is associated with.
- * @returns The {@link Firestore} instance of the provided app.
- */
-export function getFirestore(app: FirebaseApp): Firestore;
-
-/**
- * Returns the existing default {@link Firestore} instance that is associated with the
  * default {@link @firebase/app#FirebaseApp}. If no instance exists, initializes a new
  * instance with default settings.
  *
