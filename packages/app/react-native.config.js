56e92ef2b1c1d0b0b39ed4a6b5bee700d1c115d0
Mike Hardy <github@mikehardy.net>
2023-11-27 11:04:51 -0500

    build(ios): specify our script phases always run
    this removes the ambiguity of whether they need to run
    every time or not, which caused xcodebuild to emit a warning
    
    if ambiguous, xcodebuild runs them every time which is our
    desired behavior, this behavior will continue as before but
    now without the warning message

--- packages/app/react-native.config.js
+++ packages/app/react-native.config.js
@@ -11,6 +11,7 @@ module.exports = {
             path: './ios_config.sh',
             execution_position: 'after_compile',
             input_files: ['$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)'],
+            always_out_of_date: '1',
           },
         ],
       },
--- packages/crashlytics/react-native.config.js
+++ packages/crashlytics/react-native.config.js
@@ -27,6 +27,7 @@ module.exports = {
               '${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${TARGET_NAME}',
               '$(SRCROOT)/$(BUILT_PRODUCTS_DIR)/$(INFOPLIST_PATH)',
             ],
+            always_out_of_date: '1',
           },
         ],
       },
