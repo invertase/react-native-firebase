ca9d49fe1e89dbd351cb218ad422c1f4c9190500
exaby73 <nabeelparkar99@gmail.com>
2024-01-09 17:10:39 +0530

    fix build

--- website-modular/astro.config.mjs
+++ website-modular/astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+// FIXME: use actual adapter for prod
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'hybrid',
+    adapter: node({
+        mode: 'standalone',
+    }),
 });
