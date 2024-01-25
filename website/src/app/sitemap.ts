a69a8798f1449bdae26500a35b9ff5a2227f65a5
Elliot Hesp <elliot.hesp@gmail.com>
2024-01-25 22:56:10 +0000

    -

--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 import glob from 'fast-glob'
 
-export default async function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const pages = await glob('**/*.mdx', { cwd: 'src/app' })
 
   return pages.map((page) => {
