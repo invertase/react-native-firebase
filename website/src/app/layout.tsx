3306072689742d44b710ed0e4223bb0af2b7ae1f
Elliot Hesp <elliot.hesp@gmail.com>
2024-01-25 22:36:57 +0000

    -

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -31,6 +31,12 @@ export default async function RootLayout({
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K2S593S');`}
+        </script>
         <link rel="icon" type="image/png" href="//static.invertase.io/assets/react-native-firebase-favicon.png" />
       </head>
       <body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
