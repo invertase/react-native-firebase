9e9255a8845b1f3f28fb77b0a3b032c13958593f
Mike Hardy <github@mikehardy.net>
2024-02-19 10:28:41 -0500

    docs(app, ios): note pod install required for ios_config changes
    This wastes developer time and deserves a big note

--- packages/app/ios_config.sh
+++ packages/app/ios_config.sh
@@ -14,6 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+##########################################################################
+##########################################################################
+#
+#  NOTE THAT IF YOU CHANGE THIS FILE YOU MUST RUN pod install AFTERWARDS
+#
+#  This file is installed as an Xcode build script in the project file
+#  by cocoapods, and you will not see your changes until you pod install
+#
+##########################################################################
+##########################################################################
+
 set -e
 
 _MAX_LOOKUPS=2;
