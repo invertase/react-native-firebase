e0d3836e55e886ef7d8f4e9824785bcd6d3c212c
Dominik Henneke <dominik@plancraft.de>
2024-04-18 09:41:09 +0200

    feat(firestore): implement the getDoc modular api

--- packages/firestore/lib/modular/query.d.ts
+++ packages/firestore/lib/modular/query.d.ts
@@ -240,6 +240,42 @@ export function startAfter<AppModelType, DbModelType extends DocumentData>(
 export function limit(limit: number): QueryLimitConstraint;
 
 /**
+ * Reads the document referred to by this `DocumentReference`.
+ *
+ * Note: `getDoc()` attempts to provide up-to-date data when possible by waiting
+ * for data from the server, but it may return cached data or fail if you are
+ * offline and the server cannot be reached. To specify this behavior, invoke
+ * {@link getDocFromCache} or {@link getDocFromServer}.
+ *
+ * @param reference - The reference of the document to fetch.
+ * @returns A Promise resolved with a `DocumentSnapshot` containing the
+ * current document contents.
+ */
+export declare function getDoc<T>(reference: DocumentReference<T>): Promise<DocumentSnapshot<T>>;
+
+/**
+ * Reads the document referred to by this `DocumentReference` from cache.
+ * Returns an error if the document is not currently cached.
+ *
+ * @returns A `Promise` resolved with a `DocumentSnapshot` containing the
+ * current document contents.
+ */
+export declare function getDocFromCache<T>(
+  reference: DocumentReference<T>,
+): Promise<DocumentSnapshot<T>>;
+
+/**
+ * Reads the document referred to by this `DocumentReference` from the server.
+ * Returns an error if the network is not available.
+ *
+ * @returns A `Promise` resolved with a `DocumentSnapshot` containing the
+ * current document contents.
+ */
+export declare function getDocFromServer<T>(
+  reference: DocumentReference<T>,
+): Promise<DocumentSnapshot<T>>;
+
+/**
  * Executes the query and returns the results as a `QuerySnapshot`.
  *
  * Note: `getDocs()` attempts to provide up-to-date data when possible by
