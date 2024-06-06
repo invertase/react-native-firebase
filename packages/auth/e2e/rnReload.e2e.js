a0011fac77aa3c1c51daf409f8bc8520703808ad
Mike Hardy <github@mikehardy.net>
2024-06-06 17:35:13 -0500

    test(e2e, auth): skip v8 compat API tests optionally
    there is a problem locally with higher-API emulators where apparently
    running too many tests causes the app to crash, but not related to
    some specific API
    
    A reasonable workaround that has reliable success is simply to run
    fewer tests, so cutting out the v8-compat tests and focusing on
    the modular API works well

--- packages/auth/e2e/rnReload.e2e.js
+++ packages/auth/e2e/rnReload.e2e.js
@@ -4,90 +4,91 @@ const TEST_PASS = 'test1234';
 const { clearAllUsers } = require('./helpers');
 
 describe('auth()', function () {
-  describe('firebase v8 compatibility', function () {
-    before(async function () {
-      try {
-        await clearAllUsers();
-      } catch (e) {
-        throw e;
-      }
-      try {
-        await firebase.auth().createUserWithEmailAndPassword(TEST_EMAIL, TEST_PASS);
-      } catch (e) {
-        // they may already exist, that's fine
-      }
-    });
-
-    beforeEach(async function () {
-      if (firebase.auth().currentUser) {
-        await firebase.auth().signOut();
-        await Utils.sleep(50);
-      }
-    });
-
-    describe('firebase.auth().currentUser', function () {
-      it('exists after reload', async function () {
-        // Detox on iOS crashing app on reloads, documented
-        // https://github.com/wix/detox/blob/master/docs/APIRef.DeviceObjectAPI.md#devicereloadreactnative
-        if (device.getPlatform() === 'ios') {
-          this.skip();
+  !skipCompatTests &&
+    describe('firebase v8 compatibility', function () {
+      before(async function () {
+        try {
+          await clearAllUsers();
+        } catch (e) {
+          throw e;
         }
-        let currentUser;
-        // before reload
-        await firebase.auth().signInAnonymously();
+        try {
+          await firebase.auth().createUserWithEmailAndPassword(TEST_EMAIL, TEST_PASS);
+        } catch (e) {
+          // they may already exist, that's fine
+        }
+      });
 
-        ({ currentUser } = firebase.auth());
-        currentUser.should.be.an.Object();
-        currentUser.uid.should.be.a.String();
-        currentUser.toJSON().should.be.an.Object();
-        should.equal(currentUser.toJSON().email, null);
-        currentUser.isAnonymous.should.equal(true);
-        currentUser.providerId.should.equal('firebase');
-        currentUser.should.equal(firebase.auth().currentUser);
+      beforeEach(async function () {
+        if (firebase.auth().currentUser) {
+          await firebase.auth().signOut();
+          await Utils.sleep(50);
+        }
+      });
 
-        // RELOAD
-        await device.reloadReactNative();
+      describe('firebase.auth().currentUser', function () {
+        it('exists after reload', async function () {
+          // Detox on iOS crashing app on reloads, documented
+          // https://github.com/wix/detox/blob/master/docs/APIRef.DeviceObjectAPI.md#devicereloadreactnative
+          if (device.getPlatform() === 'ios') {
+            this.skip();
+          }
+          let currentUser;
+          // before reload
+          await firebase.auth().signInAnonymously();
 
-        // after reload
-        ({ currentUser } = firebase.auth());
-        currentUser.should.be.an.Object();
-        currentUser.uid.should.be.a.String();
-        currentUser.toJSON().should.be.an.Object();
-        should.equal(currentUser.toJSON().email, null);
-        currentUser.isAnonymous.should.equal(true);
-        currentUser.providerId.should.equal('firebase');
-        currentUser.should.equal(firebase.auth().currentUser);
+          ({ currentUser } = firebase.auth());
+          currentUser.should.be.an.Object();
+          currentUser.uid.should.be.a.String();
+          currentUser.toJSON().should.be.an.Object();
+          should.equal(currentUser.toJSON().email, null);
+          currentUser.isAnonymous.should.equal(true);
+          currentUser.providerId.should.equal('firebase');
+          currentUser.should.equal(firebase.auth().currentUser);
 
-        // test correct user is returned after signing
-        // in with a different user then reloading
-        await firebase.auth().signOut();
+          // RELOAD
+          await device.reloadReactNative();
 
-        await firebase.auth().signInWithEmailAndPassword(TEST_EMAIL, TEST_PASS);
+          // after reload
+          ({ currentUser } = firebase.auth());
+          currentUser.should.be.an.Object();
+          currentUser.uid.should.be.a.String();
+          currentUser.toJSON().should.be.an.Object();
+          should.equal(currentUser.toJSON().email, null);
+          currentUser.isAnonymous.should.equal(true);
+          currentUser.providerId.should.equal('firebase');
+          currentUser.should.equal(firebase.auth().currentUser);
 
-        ({ currentUser } = firebase.auth());
-        currentUser.should.be.an.Object();
-        currentUser.uid.should.be.a.String();
-        currentUser.toJSON().should.be.an.Object();
-        currentUser.toJSON().email.should.eql(TEST_EMAIL);
-        currentUser.isAnonymous.should.equal(false);
-        currentUser.providerId.should.equal('firebase');
-        currentUser.should.equal(firebase.auth().currentUser);
+          // test correct user is returned after signing
+          // in with a different user then reloading
+          await firebase.auth().signOut();
 
-        // RELOAD
-        await device.reloadReactNative();
+          await firebase.auth().signInWithEmailAndPassword(TEST_EMAIL, TEST_PASS);
 
-        // after reload
-        ({ currentUser } = firebase.auth());
-        currentUser.should.be.an.Object();
-        currentUser.uid.should.be.a.String();
-        currentUser.toJSON().should.be.an.Object();
-        currentUser.toJSON().email.should.eql(TEST_EMAIL);
-        currentUser.isAnonymous.should.equal(false);
-        currentUser.providerId.should.equal('firebase');
-        currentUser.should.equal(firebase.auth().currentUser);
-      }).timeout(15000);
+          ({ currentUser } = firebase.auth());
+          currentUser.should.be.an.Object();
+          currentUser.uid.should.be.a.String();
+          currentUser.toJSON().should.be.an.Object();
+          currentUser.toJSON().email.should.eql(TEST_EMAIL);
+          currentUser.isAnonymous.should.equal(false);
+          currentUser.providerId.should.equal('firebase');
+          currentUser.should.equal(firebase.auth().currentUser);
+
+          // RELOAD
+          await device.reloadReactNative();
+
+          // after reload
+          ({ currentUser } = firebase.auth());
+          currentUser.should.be.an.Object();
+          currentUser.uid.should.be.a.String();
+          currentUser.toJSON().should.be.an.Object();
+          currentUser.toJSON().email.should.eql(TEST_EMAIL);
+          currentUser.isAnonymous.should.equal(false);
+          currentUser.providerId.should.equal('firebase');
+          currentUser.should.equal(firebase.auth().currentUser);
+        }).timeout(15000);
+      });
     });
-  });
 
   describe('modular', function () {
     before(async function () {
