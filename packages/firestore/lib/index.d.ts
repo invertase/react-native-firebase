244dbfc0dca44b0352d52842d45c2e0ee04ce78f
russellwheatley <russellwheatley85@gmail.com>
2024-07-15 12:07:30 +0100

    fix(firestore): Filter typing. `Filter.or` & `Filter.and` can accept each other

--- packages/firestore/lib/index.d.ts
+++ packages/firestore/lib/index.d.ts
@@ -78,12 +78,16 @@ export namespace FirebaseFirestoreTypes {
      * The Filter.or() static function used to generate a logical OR query using multiple Filter instances.
      * e.g. Filter.or(Filter('name', '==', 'Ada'), Filter('name', '==', 'Bob'))
      */
-    or(...queries: QueryFilterConstraint[]): QueryCompositeFilterConstraint;
+    or(
+      ...queries: (QueryFilterConstraint | QueryCompositeFilterConstraint)[]
+    ): QueryCompositeFilterConstraint;
     /**
      * The Filter.and() static function used to generate a logical AND query using multiple Filter instances.
      * e.g. Filter.and(Filter('name', '==', 'Ada'), Filter('name', '==', 'Bob'))
      */
-    and(...queries: QueryFilterConstraint[]): QueryCompositeFilterConstraint;
+    and(
+      ...queries: (QueryFilterConstraint | QueryCompositeFilterConstraint)[]
+    ): QueryCompositeFilterConstraint;
   }
   /**
    * The Filter function used to generate an instance of Filter.
