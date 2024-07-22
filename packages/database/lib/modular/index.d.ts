52d2ccdc0b97d45ab28eb14db9691031415b9960
russellwheatley <russellwheatley85@gmail.com>
2024-07-22 11:08:49 +0100

    tests: test every function is exported for modular API

--- packages/database/lib/modular/index.d.ts
+++ packages/database/lib/modular/index.d.ts
@@ -208,6 +208,10 @@ export declare function forceWebSockets(): void;
 export function serverTimestamp(): object;
 
 /**
+ * Returns the current Firebase Database server time as a JavaScript Date object.
+ */
+export function getServerTime(db: Database): Promise<number>;
+/**
  * Returns a placeholder value that can be used to atomically increment the
  * current database value by the provided delta.
  *
