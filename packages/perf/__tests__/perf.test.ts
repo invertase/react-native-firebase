73742873ea62fd2a302cbdd232a20616effaaa5a
russellwheatley <russellwheatley85@gmail.com>
2024-07-23 16:54:33 +0100

    test modular exports

--- packages/perf/__tests__/perf.test.ts
+++ packages/perf/__tests__/perf.test.ts
@@ -1,6 +1,14 @@
 import { describe, expect, it } from '@jest/globals';
 
-import perf, { firebase } from '../lib';
+import perf, {
+  firebase,
+  getPerformance,
+  initializePerformance,
+  trace,
+  httpMetric,
+  newScreenTrace,
+  startScreenTrace,
+} from '../lib';
 
 describe('Performance Monitoring', function () {
   describe('namespace', function () {
@@ -117,6 +125,32 @@ describe('Performance Monitoring', function () {
         );
         return Promise.resolve();
       }
+    });
+  });
+
+  describe('modular', function () {
+    it('`getPerformance` function is properly exposed to end user', function () {
+      expect(getPerformance).toBeDefined();
+    });
+
+    it('`initializePerformance` function is properly exposed to end user', function () {
+      expect(initializePerformance).toBeDefined();
+    });
+
+    it('`trace` function is properly exposed to end user', function () {
+      expect(trace).toBeDefined();
+    });
+
+    it('`httpMetric` function is properly exposed to end user', function () {
+      expect(httpMetric).toBeDefined();
+    });
+
+    it('`newScreenTrace` function is properly exposed to end user', function () {
+      expect(newScreenTrace).toBeDefined();
+    });
+
+    it('`startScreenTrace` function is properly exposed to end user', function () {
+      expect(startScreenTrace).toBeDefined();
     });
   });
 });
