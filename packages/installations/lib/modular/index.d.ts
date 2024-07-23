46662652bed5d6f8b2d543e81563901f58d069a6
russellwheatley <russellwheatley85@gmail.com>
2024-07-23 16:25:41 +0100

    update modular TS

--- packages/installations/lib/modular/index.d.ts
+++ packages/installations/lib/modular/index.d.ts
@@ -1,30 +1,29 @@
 import { ReactNativeFirebase } from '@react-native-firebase/app';
 import { FirebaseInstallationsTypes } from '../index';
 
+import FirebaseInstallations = FirebaseInstallationsTypes.Module;
 /**
  * Returns an instance of Installations associated with the given FirebaseApp instance.
  */
 export declare function getInstallations(
   app?: ReactNativeFirebase.FirebaseApp,
-): FirebaseInstallationsTypes.Module;
+): FirebaseInstallations;
 
 /**
  * Deletes the Firebase Installation and all associated data.
  */
-export declare function deleteInstallations(
-  installations?: FirebaseInstallationsTypes.Module,
-): Promise<void>;
+export declare function deleteInstallations(installations?: FirebaseInstallations): Promise<void>;
 
 /**
  * Creates a Firebase Installation if there isn't one for the app and returns the Installation ID.
  */
-export declare function getId(installations: FirebaseInstallationsTypes.Module): Promise<string>;
+export declare function getId(installations: FirebaseInstallations): Promise<string>;
 
 /**
  * Returns a Firebase Installations auth token, identifying the current Firebase Installation.
  */
 export declare function getToken(
-  installations: FirebaseInstallationsTypes.Module,
+  installations: FirebaseInstallations,
   forceRefresh?: boolean,
 ): Promise<string>;
 
@@ -37,6 +36,6 @@ declare type IdChangeUnsubscribeFn = () => void;
  * Sets a new callback that will get called when Installation ID changes. Returns an unsubscribe function that will remove the callback when called.
  */
 export declare function onIdChange(
-  installations: FirebaseInstallationsTypes.Module,
+  installations: FirebaseInstallations,
   callback: IdChangeCallbackFn,
 ): IdChangeUnsubscribeFn;
