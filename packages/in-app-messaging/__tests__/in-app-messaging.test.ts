d93752e22caed13e5583298b62022b96885e93a8
russellwheatley <russellwheatley85@gmail.com>
2024-07-23 16:18:05 +0100

    test modular exports

---
+++ packages/in-app-messaging/__tests__/in-app-messaging.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from '@jest/globals';
+
+import {
+  firebase,
+  getInAppMessaging,
+  isMessagesDisplaySuppressed,
+  setMessagesDisplaySuppressed,
+  isAutomaticDataCollectionEnabled,
+  setAutomaticDataCollectionEnabled,
+  triggerEvent,
+} from '../lib';
+
+describe('Auth', function () {
+  describe('namespace', function () {
+    it('accessible from firebase.app()', function () {
+      const app = firebase.app();
+      expect(app.auth).toBeDefined();
+      expect(app.auth().useEmulator).toBeDefined();
+    });
+  });
+
+  describe('modular', function () {
+    it('`getInAppMessaging` function is properly exposed to end user', function () {
+      expect(getInAppMessaging).toBeDefined();
+    });
+
+    it('`isMessagesDisplaySuppressed` function is properly exposed to end user', function () {
+      expect(isMessagesDisplaySuppressed).toBeDefined();
+    });
+
+    it('`setMessagesDisplaySuppressed` function is properly exposed to end user', function () {
+      expect(setMessagesDisplaySuppressed).toBeDefined();
+    });
+
+    it('`isAutomaticDataCollectionEnabled` function is properly exposed to end user', function () {
+      expect(isAutomaticDataCollectionEnabled).toBeDefined();
+    });
+
+    it('`setAutomaticDataCollectionEnabled` function is properly exposed to end user', function () {
+      expect(setAutomaticDataCollectionEnabled).toBeDefined();
+    });
+
+    it('`triggerEvent` function is properly exposed to end user', function () {
+      expect(triggerEvent).toBeDefined();
+    });
+  });
+});
