c79ff515c9ae941ed5a65f28d58a354caa55718a
Russell Wheatley <russellwheatley85@gmail.com>
2024-12-20 09:07:17 +0000

    Apply suggestions from code review
    Co-authored-by: Mike Hardy <github@mikehardy.net>

--- packages/app/lib/FirebaseApp.js
+++ packages/app/lib/FirebaseApp.js
@@ -61,7 +61,6 @@ export default class FirebaseApp {
   }
 
   extendApp(extendedProps) {
-    // this method has no modular alternative, send true for param 'noAlternative'
     warnIfNotModularCall(arguments);
     this._checkDestroyed();
     Object.assign(this, extendedProps);
@@ -74,7 +73,6 @@ export default class FirebaseApp {
   }
 
   toString() {
-    // this method has no modular alternative, send true for param 'noAlternative'
     warnIfNotModularCall(arguments);
     return this.name;
   }
