bb66d56c5f8b483fde7036e35b0466db4a66a9cc
russellwheatley <russellwheatley85@gmail.com>
2025-01-27 12:26:08 +0000

    fix: fetch API issue with global polyfill. not needed

--- packages/vertexai/lib/polyfills.ts
+++ packages/vertexai/lib/polyfills.ts
@@ -14,11 +14,6 @@
  * limitations under the License.
  *
  */
-
-// @ts-ignore
-import { polyfill } from 'react-native-polyfill-globals/src/fetch';
-polyfill();
-
 // @ts-ignore
 import { ReadableStream as ReadableStreamPolyfill } from 'web-streams-polyfill/dist/ponyfill';
 // @ts-ignore
