492e6f8149290b5a6cc777507ddd1ee2d173ffd7
Mike Hardy <github@mikehardy.net>
2025-02-03 21:31:59 -0500

    test(deps): react-native update
    - use 0.76: react-native-macos is still on 0.76 so 0.77+ not achievable yet
    - 0.76.6 has a allowRecursiveCommitsWithSynchronousMountOnAndroid pick which fails, so use 0.76.5
    - note new platform minimums: macos 11.0, iOS 15.1, android 24
    
    This defers updates to eslint and typescript types - they are unblocked by the
    react-native bump but they are major changes in their own right and will be performed
    separately

--- tests/android/app/src/main/java/com/invertase/testing/MainApplication.kt
+++ tests/android/app/src/main/java/com/invertase/testing/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import io.invertase.firebase.app.ReactNativeFirebaseApp
 
@@ -37,7 +38,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
