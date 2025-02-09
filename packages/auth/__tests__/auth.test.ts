ada97b8b1506c160b1751d995677d9563065aa91
Mike Hardy <github@mikehardy.net>
2025-02-09 13:14:11 -0500

    refactor(test): include all namespaced API tests in namespace sub-suites
    this will let us treat all namespaced API usages as a group in the sub-suites

--- packages/auth/__tests__/auth.test.ts
+++ packages/auth/__tests__/auth.test.ts
@@ -68,110 +68,112 @@ describe('Auth', function () {
       expect(app.auth).toBeDefined();
       expect(app.auth().useEmulator).toBeDefined();
     });
-  });
 
-  describe('useEmulator()', function () {
-    it('useEmulator requires a string url', function () {
-      // @ts-ignore because we pass an invalid argument...
-      expect(() => auth().useEmulator()).toThrow(
-        'firebase.auth().useEmulator() takes a non-empty string',
-      );
-      expect(() => auth().useEmulator('')).toThrow(
-        'firebase.auth().useEmulator() takes a non-empty string',
-      );
-      // @ts-ignore because we pass an invalid argument...
-      expect(() => auth().useEmulator(123)).toThrow(
-        'firebase.auth().useEmulator() takes a non-empty string',
-      );
-    });
+    describe('useEmulator()', function () {
+      it('useEmulator requires a string url', function () {
+        // @ts-ignore because we pass an invalid argument...
+        expect(() => auth().useEmulator()).toThrow(
+          'firebase.auth().useEmulator() takes a non-empty string',
+        );
+        expect(() => auth().useEmulator('')).toThrow(
+          'firebase.auth().useEmulator() takes a non-empty string',
+        );
+        // @ts-ignore because we pass an invalid argument...
+        expect(() => auth().useEmulator(123)).toThrow(
+          'firebase.auth().useEmulator() takes a non-empty string',
+        );
+      });
 
-    it('useEmulator requires a well-formed url', function () {
-      // No http://
-      expect(() => auth().useEmulator('localhost:9099')).toThrow(
-        'firebase.auth().useEmulator() takes a non-empty string URL',
-      );
-      // No port
-      expect(() => auth().useEmulator('http://localhost')).toThrow(
-        'firebase.auth().useEmulator() unable to parse host and port from URL',
-      );
-    });
+      it('useEmulator requires a well-formed url', function () {
+        // No http://
+        expect(() => auth().useEmulator('localhost:9099')).toThrow(
+          'firebase.auth().useEmulator() takes a non-empty string URL',
+        );
+        // No port
+        expect(() => auth().useEmulator('http://localhost')).toThrow(
+          'firebase.auth().useEmulator() unable to parse host and port from URL',
+        );
+      });
 
-    it('useEmulator -> remaps Android loopback to host', function () {
-      const foo = auth().useEmulator('http://localhost:9099');
-      expect(foo).toEqual(['10.0.2.2', 9099]);
+      it('useEmulator -> remaps Android loopback to host', function () {
+        const foo = auth().useEmulator('http://localhost:9099');
+        expect(foo).toEqual(['10.0.2.2', 9099]);
 
-      const bar = auth().useEmulator('http://127.0.0.1:9099');
-      expect(bar).toEqual(['10.0.2.2', 9099]);
-    });
+        const bar = auth().useEmulator('http://127.0.0.1:9099');
+        expect(bar).toEqual(['10.0.2.2', 9099]);
+      });
 
-    it('useEmulator allows hyphens in the hostname', function () {
-      const result = auth().useEmulator('http://my-host:9099');
-      expect(result).toEqual(['my-host', 9099]);
-    });
-  });
-
-  describe('tenantId', function () {
-    it('should be able to set tenantId ', function () {
-      const auth = firebase.app().auth();
-      auth.setTenantId('test-id').then(() => {
-        expect(auth.tenantId).toBe('test-id');
+      it('useEmulator allows hyphens in the hostname', function () {
+        const result = auth().useEmulator('http://my-host:9099');
+        expect(result).toEqual(['my-host', 9099]);
       });
     });
 
-    it('should throw error when tenantId is a non string object ', async function () {
-      try {
-        await firebase.app().auth().setTenantId(Object());
-        return Promise.reject('It should throw an error');
-      } catch (e: any) {
-        expect(e.message).toBe("firebase.auth().setTenantId(*) expected 'tenantId' to be a string");
-        return Promise.resolve('Error catched');
-      }
-    });
-  });
+    describe('tenantId', function () {
+      it('should be able to set tenantId ', function () {
+        const auth = firebase.app().auth();
+        auth.setTenantId('test-id').then(() => {
+          expect(auth.tenantId).toBe('test-id');
+        });
+      });
 
-  describe('getMultiFactorResolver', function () {
-    it('should return null if no resolver object is found', function () {
-      const unknownError = NativeFirebaseError.fromEvent(
-        {
-          code: 'unknown',
-        },
-        'auth',
-      );
-      const actual = auth.getMultiFactorResolver(auth(), unknownError);
-      expect(actual).toBe(null);
+      it('should throw error when tenantId is a non string object ', async function () {
+        try {
+          await firebase.app().auth().setTenantId(Object());
+          return Promise.reject('It should throw an error');
+        } catch (e: any) {
+          expect(e.message).toBe(
+            "firebase.auth().setTenantId(*) expected 'tenantId' to be a string",
+          );
+          return Promise.resolve('Error catched');
+        }
+      });
     });
 
-    it('should return null if resolver object is null', function () {
-      const unknownError = NativeFirebaseError.fromEvent(
-        {
-          code: 'unknown',
-          resolver: null,
-        },
-        'auth',
-      );
-      const actual = auth.getMultiFactorResolver(firebase.app().auth(), unknownError);
-      expect(actual).toBe(null);
-    });
+    describe('getMultiFactorResolver', function () {
+      it('should return null if no resolver object is found', function () {
+        const unknownError = NativeFirebaseError.fromEvent(
+          {
+            code: 'unknown',
+          },
+          'auth',
+        );
+        const actual = auth.getMultiFactorResolver(auth(), unknownError);
+        expect(actual).toBe(null);
+      });
 
-    it('should return the resolver object if its found', function () {
-      const resolver = { session: '', hints: [] };
-      const errorWithResolver = NativeFirebaseError.fromEvent(
-        {
-          code: 'multi-factor-auth-required',
-          resolver,
-        },
-        'auth',
-      );
-      const actual = auth.getMultiFactorResolver(firebase.app().auth(), errorWithResolver);
-      // Using expect(actual).toEqual(resolver) causes unexpected errors:
-      //  You attempted to use "firebase.app('[DEFAULT]').appCheck" but this module could not be found.
-      expect(actual).not.toBeNull();
-      // @ts-ignore We know actual is not null
-      expect(actual.session).toEqual(resolver.session);
-      // @ts-ignore We know actual is not null
-      expect(actual.hints).toEqual(resolver.hints);
-      // @ts-ignore We know actual is not null
-      expect(actual._auth).not.toBeNull();
+      it('should return null if resolver object is null', function () {
+        const unknownError = NativeFirebaseError.fromEvent(
+          {
+            code: 'unknown',
+            resolver: null,
+          },
+          'auth',
+        );
+        const actual = auth.getMultiFactorResolver(firebase.app().auth(), unknownError);
+        expect(actual).toBe(null);
+      });
+
+      it('should return the resolver object if its found', function () {
+        const resolver = { session: '', hints: [] };
+        const errorWithResolver = NativeFirebaseError.fromEvent(
+          {
+            code: 'multi-factor-auth-required',
+            resolver,
+          },
+          'auth',
+        );
+        const actual = auth.getMultiFactorResolver(firebase.app().auth(), errorWithResolver);
+        // Using expect(actual).toEqual(resolver) causes unexpected errors:
+        //  You attempted to use "firebase.app('[DEFAULT]').appCheck" but this module could not be found.
+        expect(actual).not.toBeNull();
+        // @ts-ignore We know actual is not null
+        expect(actual.session).toEqual(resolver.session);
+        // @ts-ignore We know actual is not null
+        expect(actual.hints).toEqual(resolver.hints);
+        // @ts-ignore We know actual is not null
+        expect(actual._auth).not.toBeNull();
+      });
     });
   });
 
--- packages/functions/__tests__/functions.test.ts
+++ packages/functions/__tests__/functions.test.ts
@@ -15,41 +15,41 @@ describe('Cloud Functions', function () {
       expect(app.functions).toBeDefined();
       expect(app.functions().httpsCallable).toBeDefined();
     });
-  });
 
-  describe('useFunctionsEmulator()', function () {
-    it('useFunctionsEmulator -> uses 10.0.2.2', function () {
-      functions().useEmulator('localhost', 5001);
+    describe('useFunctionsEmulator()', function () {
+      it('useFunctionsEmulator -> uses 10.0.2.2', function () {
+        functions().useEmulator('localhost', 5001);
 
-      // @ts-ignore
-      expect(functions()._useFunctionsEmulatorHost).toBe('10.0.2.2');
+        // @ts-ignore
+        expect(functions()._useFunctionsEmulatorHost).toBe('10.0.2.2');
 
-      functions().useEmulator('127.0.0.1', 5001);
+        functions().useEmulator('127.0.0.1', 5001);
 
-      // @ts-ignore
-      expect(functions()._useFunctionsEmulatorHost).toBe('10.0.2.2');
+        // @ts-ignore
+        expect(functions()._useFunctionsEmulatorHost).toBe('10.0.2.2');
+      });
+
+      it('prefers emulator to custom domain', function () {
+        const app = firebase.app();
+        const customUrl = 'https://test.com';
+        const functions = app.functions(customUrl);
+
+        functions.useFunctionsEmulator('http://10.0.2.2');
+
+        // @ts-ignore
+        expect(functions._useFunctionsEmulatorHost).toBe('10.0.2.2');
+      });
     });
 
-    it('prefers emulator to custom domain', function () {
-      const app = firebase.app();
-      const customUrl = 'https://test.com';
-      const functions = app.functions(customUrl);
+    describe('httpcallable()', function () {
+      it('throws an error with an incorrect timeout', function () {
+        const app = firebase.app();
 
-      functions.useFunctionsEmulator('http://10.0.2.2');
-
-      // @ts-ignore
-      expect(functions._useFunctionsEmulatorHost).toBe('10.0.2.2');
-    });
-  });
-
-  describe('httpcallable()', function () {
-    it('throws an error with an incorrect timeout', function () {
-      const app = firebase.app();
-
-      // @ts-ignore
-      expect(() => app.functions().httpsCallable('example', { timeout: 'test' })).toThrow(
-        'HttpsCallableOptions.timeout expected a Number in milliseconds',
-      );
+        // @ts-ignore
+        expect(() => app.functions().httpsCallable('example', { timeout: 'test' })).toThrow(
+          'HttpsCallableOptions.timeout expected a Number in milliseconds',
+        );
+      });
     });
   });
 
