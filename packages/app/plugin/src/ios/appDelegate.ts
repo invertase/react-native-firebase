0f75c64f7e3fbb00633887e61c4605125bd4dc0e
Sam Gaus <sam@gaus.co.uk>
2025-02-11 20:03:41 +0000

    Support Swift in expo plugin for @react-native-firebase/app

--- packages/app/plugin/src/ios/appDelegate.ts
+++ packages/app/plugin/src/ios/appDelegate.ts
@@ -3,17 +3,17 @@ import { AppDelegateProjectFile } from '@expo/config-plugins/build/ios/Paths';
 import { mergeContents } from '@expo/config-plugins/build/utils/generateCode';
 import fs from 'fs';
 
-const methodInvocationBlock = `[FIRApp configure];`;
-// https://regex101.com/r/mPgaq6/1
-const methodInvocationLineMatcher =
-  /(?:self\.moduleName\s*=\s*@\"([^"]*)\";)|(?:(self\.|_)(\w+)\s?=\s?\[\[UMModuleRegistryAdapter alloc\])|(?:RCTBridge\s?\*\s?(\w+)\s?=\s?\[(\[RCTBridge alloc\]|self\.reactDelegate))/g;
-
-// https://regex101.com/r/nHrTa9/1/
-// if the above regex fails, we can use this one as a fallback:
-const fallbackInvocationLineMatcher =
-  /-\s*\(BOOL\)\s*application:\s*\(UIApplication\s*\*\s*\)\s*\w+\s+didFinishLaunchingWithOptions:/g;
-
 export function modifyObjcAppDelegate(contents: string): string {
+  const methodInvocationBlock = `[FIRApp configure];`;
+  // https://regex101.com/r/mPgaq6/1
+  const methodInvocationLineMatcher =
+    /(?:self\.moduleName\s*=\s*@\"([^"]*)\";)|(?:(self\.|_)(\w+)\s?=\s?\[\[UMModuleRegistryAdapter alloc\])|(?:RCTBridge\s?\*\s?(\w+)\s?=\s?\[(\[RCTBridge alloc\]|self\.reactDelegate))/g;
+
+  // https://regex101.com/r/nHrTa9/1/
+  // if the above regex fails, we can use this one as a fallback:
+  const fallbackInvocationLineMatcher =
+    /-\s*\(BOOL\)\s*application:\s*\(UIApplication\s*\*\s*\)\s*\w+\s+didFinishLaunchingWithOptions:/g;
+
   // Add import
   if (!contents.includes('#import <Firebase/Firebase.h>')) {
     contents = contents.replace(
@@ -68,16 +68,82 @@ export function modifyObjcAppDelegate(contents: string): string {
   }
 }
 
+export function modifySwiftAppDelegate(contents: string): string {
+  const methodInvocationBlock = `FirebaseApp.configure()`;
+  const methodInvocationLineMatcher =
+  /(?:self\.moduleName\s*=\s*"([^"]*)")/g;
+
+  const fallbackInvocationLineMatcher =
+    /\s*override\s+func\s+application\(_ application:\s*UIApplication,\s*didFinishLaunchingWithOptions\s+launchOptions:.+\s*\{/g;
+
+  // Add import
+  if (!contents.includes('import FirebaseCore')) {
+    contents = contents.replace(
+      /import ReactAppDependencyProvider/g,
+      `import ReactAppDependencyProvider
+import FirebaseCore`,
+    );
+  }
+
+  // To avoid potential issues with existing changes from older plugin versions
+  if (contents.includes(methodInvocationBlock)) {
+    return contents;
+  }
+
+  if (
+    !methodInvocationLineMatcher.test(contents) &&
+    !fallbackInvocationLineMatcher.test(contents)
+  ) {
+    WarningAggregator.addWarningIOS(
+      '@react-native-firebase/app',
+      'Unable to determine correct Firebase insertion point in AppDelegate.swift. Skipping Firebase addition.',
+    );
+    return contents;
+  }
+
+  // Add invocation
+  try {
+    return mergeContents({
+      tag: '@react-native-firebase/app-didFinishLaunchingWithOptions',
+      src: contents,
+      newSrc: methodInvocationBlock,
+      anchor: methodInvocationLineMatcher,
+      offset: 0, // new line will be inserted right above matched anchor
+      comment: '//',
+    }).contents;
+  } catch (_: any) {
+    // we fallback to another regex if the first one fails
+    return mergeContents({
+      tag: '@react-native-firebase/app-didFinishLaunchingWithOptions-fallback',
+      src: contents,
+      newSrc: methodInvocationBlock,
+      anchor: fallbackInvocationLineMatcher,
+      // new line will be inserted right below matched anchor
+      offset: 1,
+      comment: '//',
+    }).contents;
+  }
+}
+
 export async function modifyAppDelegateAsync(appDelegateFileInfo: AppDelegateProjectFile) {
   const { language, path, contents } = appDelegateFileInfo;
 
-  if (['objc', 'objcpp'].includes(language)) {
-    const newContents = modifyObjcAppDelegate(contents);
-    await fs.promises.writeFile(path, newContents);
-  } else {
-    // TODO: Support Swift
-    throw new Error(`Cannot add Firebase code to AppDelegate of language "${language}"`);
+  let newContents = contents;
+
+  switch (language) {
+    case 'objc':
+    case 'objcpp': {
+      newContents = modifyObjcAppDelegate(contents);
+      break;
+    }
+    case 'swift': {
+      newContents = modifySwiftAppDelegate(contents);
+      break;
+    }
+    default: throw new Error(`Cannot add Firebase code to AppDelegate of language "${language}"`);
   }
+
+  await fs.promises.writeFile(path, newContents);
 }
 
 export const withFirebaseAppDelegate: ConfigPlugin = config => {
