9e353d371b23d79379884986f1a82f80d614153d
russellwheatley <russellwheatley85@gmail.com>
2025-02-17 10:47:55 +0000

    chore: typo

--- packages/auth/lib/Utils.js
+++ packages/auth/lib/Utils.js
@@ -2,7 +2,7 @@ export function warnDynamicLink(actionCodeSettings) {
   if (actionCodeSettings && actionCodeSettings.dynamicLinkDomain) {
     console.warn(
       'Firebase Dynamic Links is deprecated and will be shut down as early as August * 2025. \
-      Instead, use ActionCodeSettings.linkDomain to set a a custom domain. Learn more at: https://firebase.google.com/support/dynamic-links-faq',
+      Instead, use ActionCodeSettings.linkDomain to set up a custom domain. Learn more at: https://firebase.google.com/support/dynamic-links-faq',
     );
   }
 }
