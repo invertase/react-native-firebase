a1fe5b8e7d58fadc15acb2089c0b4efc92c0b59c
Mike Hardy <github@mikehardy.net>
2025-02-17 10:17:40 -0500

    docs: add new "strict mode" for deprecation warnings
    will throw an error with associated stack if any namespaced API
    usage is detected - this can help locate usage very very rapidly

--- tests/app.js
+++ tests/app.js
@@ -72,8 +72,8 @@ ErrorUtils.setGlobalHandler((err, isFatal) => {
 
 function loadTests(_) {
   describe('React Native Firebase', function () {
-    if (!globalThis.RNFBDebug) {
-      // Only retry tests if not debugging locally,
+    if (!globalThis.RNFBDebug && !globalThis.RNFB_MODULAR_DEPRECATION_STRICT_MODE) {
+      // Only retry tests if not debugging or hunting deprecated API usage locally,
       // otherwise it gets annoying to debug.
       this.retries(4);
     }
