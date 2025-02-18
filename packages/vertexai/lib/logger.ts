3e31ab8ef3c8df8ddd5d0507bfc8820a13ca3864
russellwheatley <russellwheatley85@gmail.com>
2025-02-18 16:27:35 +0000

    chore: update license headers in src/

--- packages/vertexai/lib/logger.ts
+++ packages/vertexai/lib/logger.ts
@@ -1,8 +1,9 @@
-/*
- * Copyright (c) 2016-present Invertase Limited & Contributors
+/**
+ * @license
+ * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this library except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
@@ -12,7 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 // @ts-ignore
 import { Logger } from '@react-native-firebase/app/lib/internal/logger';
