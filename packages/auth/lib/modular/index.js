c014c6497a00d7fc8d64fc387deed09cae5b26f2
MichaelVerdon <mwverdon2000@gmail.com>
2025-03-26 14:18:25 +0000

    fix: formatting

--- packages/auth/lib/modular/index.js
+++ packages/auth/lib/modular/index.js
@@ -615,7 +615,7 @@ export function getCustomAuthDomain(auth) {
 export async function validatePassword(auth, password) {
   if (password === null || password === undefined) {
     throw new Error(
-      "firebase.auth().validatePassword(*) expected 'password' to be a non-null or a defined value."
+      "firebase.auth().validatePassword(*) expected 'password' to be a non-null or a defined value.",
     );
   }
   let passwordPolicy = await fetchPasswordPolicy(auth);
