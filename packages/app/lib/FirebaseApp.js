9052328161c4efcf1f206d1a53a2fba97a1ed426
Mike Hardy <github@mikehardy.net>
2025-04-28 12:27:47 -0500

    fix(app): toString() does not exist in modular API, use .name property

--- packages/app/lib/FirebaseApp.js
+++ packages/app/lib/FirebaseApp.js
@@ -73,7 +73,7 @@ export default class FirebaseApp {
   }
 
   toString() {
-    warnIfNotModularCall(arguments);
+    warnIfNotModularCall(arguments, '.name property');
     return this.name;
   }
 }
