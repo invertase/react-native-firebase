cf6b7e6d3b2b25e0d7ef8070c08f1004b9e2b1fe
Lewis Casewell <lcasewell@moonpay.com>
2025-06-09 22:24:57 +0100

    feat: Add check for expo-notifications notification icon configurations

--- packages/messaging/plugin/__tests__/fixtures/expo-config-example.ts
+++ packages/messaging/plugin/__tests__/fixtures/expo-config-example.ts
@@ -1,15 +1,26 @@
 import { ExpoConfig } from '@expo/config-types';
 
+const notificationConfig = {
+  icon: 'IconAsset',
+  color: '#1D172D',
+};
+
 /**
  * @type {import('@expo/config-types').ExpoConfig}
  */
-const expoConfigExample: ExpoConfig = {
+export const expoConfigExample: ExpoConfig = {
   name: 'FirebaseMessagingTest',
   slug: 'fire-base-messaging-test',
-  notification: {
-    icon: 'IconAsset',
-    color: '#1D172D',
-  },
+  notification: notificationConfig,
 };
 
-export default expoConfigExample;
+/**
+ * @type {import('@expo/config-types').ExpoConfig}
+ */
+export const expoConfigExampleWithExpoNotificationsPlugin: ExpoConfig = {
+  name: 'FirebaseMessagingTest',
+  slug: 'fire-base-messaging-test',
+  notification: notificationConfig,
+  plugins: [['expo-notifications', notificationConfig]],
+};
+
