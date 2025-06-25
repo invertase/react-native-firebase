757fa956c63549285fd7e33a0e52a6d4926eadf2
MichaelVerdon <mwverdon2000@gmail.com>
2025-06-25 12:17:30 +0100

    chore(app-distribution): deprecations for v8 API ahead of future major release

--- packages/app-distribution/__tests__/app-distribution.test.ts
+++ packages/app-distribution/__tests__/app-distribution.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
+import { afterAll, beforeAll, describe, expect, it, beforeEach, jest } from '@jest/globals';
 
 import {
   firebase,
@@ -8,6 +8,22 @@ import {
   checkForUpdate,
   signOutTester,
 } from '../lib';
+
+import {
+  createCheckV9Deprecation,
+  CheckV9DeprecationFunction,
+} from '../../app/lib/common/unitTestUtils';
+
+// @ts-ignore test
+import FirebaseModule from '../../app/lib/internal/FirebaseModule';
+
+// Mock isIOS to be true so the app distribution methods work in tests
+jest.mock('@react-native-firebase/app/lib/common', () => {
+  const actualCommon = jest.requireActual('@react-native-firebase/app/lib/common');
+  return Object.assign({}, actualCommon, {
+    isIOS: true,
+  });
+});
 
 describe('appDistribution()', function () {
   describe('namespace', function () {
@@ -47,6 +63,65 @@ describe('appDistribution()', function () {
 
     it('`signOutTester` function is properly exposed to end user', function () {
       expect(signOutTester).toBeDefined();
+    });
+  });
+
+  describe('test `console.warn` is called for RNFB v8 API & not called for v9 API', function () {
+    let appDistributionV9Deprecation: CheckV9DeprecationFunction;
+
+    beforeEach(function () {
+      appDistributionV9Deprecation = createCheckV9Deprecation(['appDistribution']);
+
+      // @ts-ignore test
+      jest.spyOn(FirebaseModule.prototype, 'native', 'get').mockImplementation(() => {
+        return new Proxy(
+          {},
+          {
+            get: () =>
+              jest.fn().mockResolvedValue({
+                constants: {
+                  isTesterSignedIn: true,
+                },
+              } as never),
+          },
+        );
+      });
+    });
+
+    it('isTesterSignedIn', function () {
+      const appDistribution = getAppDistribution();
+      appDistributionV9Deprecation(
+        () => isTesterSignedIn(appDistribution),
+        () => appDistribution.isTesterSignedIn(),
+        'isTesterSignedIn',
+      );
+    });
+
+    it('signInTester', function () {
+      const appDistribution = getAppDistribution();
+      appDistributionV9Deprecation(
+        () => signInTester(appDistribution),
+        () => appDistribution.signInTester(),
+        'signInTester',
+      );
+    });
+
+    it('checkForUpdate', function () {
+      const appDistribution = getAppDistribution();
+      appDistributionV9Deprecation(
+        () => checkForUpdate(appDistribution),
+        () => appDistribution.checkForUpdate(),
+        'checkForUpdate',
+      );
+    });
+
+    it('signOutTester', function () {
+      const appDistribution = getAppDistribution();
+      appDistributionV9Deprecation(
+        () => signOutTester(appDistribution),
+        () => appDistribution.signOutTester(),
+        'signOutTester',
+      );
     });
   });
 });
