6b521296d0b01e451ba12ae6933c1187dfc2a441
russellwheatley <russellwheatley85@gmail.com>
2025-07-02 15:47:22 +0100

    chore: moving functions to TS

--- packages/functions/types.d.ts
+++ packages/functions/types.d.ts
@@ -1,3 +1,50 @@
 declare module '@react-native-firebase/app/lib/common' {
   export const MODULAR_DEPRECATION_ARG: string;
+  export const isAndroid: boolean;
+  export const isNumber: (value: any) => value is number;
+}
+
+declare module '@react-native-firebase/app/lib/internal' {
+  export function createModuleNamespace(config: any): any;
+  export class FirebaseModule {
+    constructor(...args: any[]);
+    native: any;
+    firebaseJson: any;
+  }
+  export function getFirebaseRoot(): any;
+  export class NativeFirebaseError {
+    static getStackWithMessage(message: string, jsStack?: string): string;
+  }
+}
+
+declare module '@react-native-firebase/app/lib/internal/nativeModule' {
+  export function setReactNativeModule(moduleName: string, module: any): void;
+}
+
+declare module '@react-native-firebase/app/lib/internal/web/firebaseFunctions' {
+  export function getApp(appName: string): any;
+  export function getFunctions(app: any, regionOrCustomDomain?: string): any;
+  export function httpsCallable(functionsInstance: any, name: string, options?: any): any;
+  export function httpsCallableFromURL(functionsInstance: any, url: string, options?: any): any;
+  export function connectFunctionsEmulator(
+    functionsInstance: any,
+    host: string,
+    port: number,
+  ): void;
+}
+
+declare module './HttpsError' {
+  export default class HttpsError extends Error {
+    constructor(code: string, message?: string, details?: any, nativeError?: any);
+  }
+}
+
+declare module './version' {
+  const version: string;
+  export default version;
+}
+
+declare module './web/RNFBFunctionsModule' {
+  const fallBackModule: any;
+  export default fallBackModule;
 }
