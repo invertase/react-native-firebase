aa2a0401ec3ae4958b216987fc48499f26668235
russellwheatley <russellwheatley85@gmail.com>
2025-08-12 14:39:58 +0100

    chore(analytics): fix types for index.ts

---
+++ packages/app/lib/common/index.d.ts
@@ -0,0 +1,5 @@
+declare module '@react-native-firebase/app/lib/common' {
+  export const MODULAR_DEPRECATION_ARG: string;
+  export const isAndroid: boolean;
+  export const isNumber: (value: any) => value is number;
+}
