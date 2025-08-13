6b070ffae4bb55b8d66ce1ec9e3feee3c1b1bd15
russellwheatley <russellwheatley85@gmail.com>
2025-08-13 12:08:52 +0100

    chore: update example app with analytics

---
+++ tests/test-app/examples/analytics/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { AppRegistry, Button, Text, View } from 'react-native';
+import analytics from '@react-native-firebase/analytics';
+
+function App() {
+  return (
+    <View>
+      <Text>text text text</Text>
+      <Text>text text text</Text>
+      <Button
+        title="Send analytics"
+        onPress={async () => {
+          try {
+            await analytics().logAddToCart({
+              currency: 'usd',
+              value: 1000,
+              items: [
+                {
+                  item_brand: 'Item Brand',
+                  item_category: 'shoe',
+                  item_id: 'S0158DF0FF20000U',
+                  item_name: 'Espadrille',
+                  item_variant: 'S0158DF0FF20000U-35',
+                },
+                {
+                  item_brand: 'Item Brand',
+                  item_category: 'shoe',
+                  item_id: 'S0158DF0FF2WERKCREV',
+                  item_name: 'Espadrille-2',
+                  item_variant: 'S0158DF0FF20000U-39058506',
+                },
+              ],
+            });
+            console.log('Sent');
+          } catch (e) {
+            console.log('ERROR', e);
+          }
+        }}
+      />
+    </View>
+  );
+}
+
+AppRegistry.registerComponent('testing', () => App);
--- tests/test-app/index.js
+++ tests/test-app/index.js
@@ -4,5 +4,6 @@
  * comment out all imports but the one you wish to run
  */
 require('./examples/database/index');
+require('./examples/analytics/index');
 //require('./examples/vertexai/index');
 //require('./examples/firestore/onSnapshotInSync');
