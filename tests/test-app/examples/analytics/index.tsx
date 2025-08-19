9d1f92f4ffef04cde5aeb372a088bf28929356d3
russellwheatley <russellwheatley85@gmail.com>
2025-08-19 11:52:37 +0100

    chore: update example for testing

--- tests/test-app/examples/analytics/index.tsx
+++ tests/test-app/examples/analytics/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppRegistry, Button, Text, View } from 'react-native';
-import analytics, { firebase } from '@react-native-firebase/analytics';
+import analytics, { firebase, logEvent } from '@react-native-firebase/analytics';
 
 function App() {
   return (
@@ -12,7 +12,10 @@ function App() {
         onPress={async () => {
           try {
             const app = firebase.app();
+            const analyticsInstance = analytics();
             console.log('used to test typing for firebase and analytics()', app);
+            logEvent(analyticsInstance, "screen_view", {screen_name: 'screenName', screen_class: 'screenClass'});
+
             await analytics().logAddToCart({
               currency: 'usd',
               value: 1000,
