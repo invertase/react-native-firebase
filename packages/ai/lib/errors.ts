45f111075f659498c10e4254f6d86707c6c83c51
Russell Wheatley <russellwheatley85@gmail.com>
2025-09-05 17:51:50 +0100

    Discard changes to packages/ai/lib/errors.ts

--- packages/ai/lib/errors.ts
+++ packages/ai/lib/errors.ts
@@ -20,7 +20,7 @@ import { AIErrorCode, CustomErrorData } from './types';
 import { AI_TYPE } from './constants';
 
 /**
- * Error class for the Firebase AI SDK.
+ * Error class for the Vertex AI in Firebase SDK.
  *
  * @public
  */
@@ -28,7 +28,7 @@ export class AIError extends FirebaseError {
   /**
    * Constructs a new instance of the `AIError` class.
    *
-   * @param code - The error code from {@link (AIErrorCode:type)}.
+   * @param code - The error code from <code>{@link AIErrorCode}</code>.
    * @param message - A human-readable message describing the error.
    * @param customErrorData - Optional error data.
    */
@@ -43,20 +43,6 @@ export class AIError extends FirebaseError {
     const fullMessage = `${service}: ${message} (${fullCode})`;
     super(code, fullMessage);
 
-    // FirebaseError initializes a stack trace, but it assumes the error is created from the error
-    // factory. Since we break this assumption, we set the stack trace to be originating from this
-    // constructor.
-    // This is only supported in V8.
-    if (Error.captureStackTrace) {
-      // Allows us to initialize the stack trace without including the constructor itself at the
-      // top level of the stack trace.
-      Error.captureStackTrace(this, AIError);
-    }
-
-    // Allows instanceof AIError in ES5/ES6
-    // https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
-    // TODO(dlarocque): Replace this with `new.target`: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html#support-for-newtarget
-    //                   which we can now use since we no longer target ES5.
     Object.setPrototypeOf(this, AIError.prototype);
 
     // Since Error is an interface, we don't inherit toString and so we define it ourselves.
