1d86149822849154380d39e920f72d8ac79fb4b3
MichaelVerdon <mwverdon2000@gmail.com>
2026-02-02 14:46:54 +0000

    fix: iOS debug build configuration in detox

--- tests/.detoxrc.js
+++ tests/.detoxrc.js
@@ -14,13 +14,13 @@ module.exports = {
       type: 'ios.app',
       binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/testing.app',
       build:
-        'set -o pipefail && xcodebuild VALID_ARCHS="`uname -m`"  CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++ -workspace ios/testing.xcworkspace -scheme testing -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build | xcbeautify',
+        'export RCT_NO_LAUNCH_PACKAGER=true && set -o pipefail && xcodebuild VALID_ARCHS="`uname -m`"  CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++ -workspace ios/testing.xcworkspace -scheme testing -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build | xcbeautify',
     },
     'ios.release': {
       type: 'ios.app',
       binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/testing.app',
       build:
-        'export RCT_NO_LAUNCH_PACKAGER=true && set -o pipefail | xcodebuild  CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++ -workspace ios/testing.xcworkspace -scheme testing -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcbeautify',
+        'export RCT_NO_LAUNCH_PACKAGER=true && set -o pipefail && xcodebuild  CC=clang CPLUSPLUS=clang++ LD=clang LDPLUSPLUS=clang++ -workspace ios/testing.xcworkspace -scheme testing -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcbeautify',
     },
     'android.debug': {
       type: 'android.apk',
