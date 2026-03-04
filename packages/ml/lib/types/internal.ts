fbf793b26a6f79ec254e469682e6cc0775119b14
MichaelVerdon <mwverdon2000@gmail.com>
2026-03-04 10:48:06 +0000

    feat: add ml types/internal with native module augmentation

---
+++ packages/ml/lib/types/internal.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this library except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * Wrapped native module interface for ML.
+ * ML Kit module exposes no instance methods beyond the base FirebaseModule.
+ */
+export interface RNFBMLModule {
+  // No additional methods; ML uses base FirebaseModule only.
+}
+
+declare module '@react-native-firebase/app/dist/module/internal/NativeModules' {
+  interface ReactNativeFirebaseNativeModules {
+    RNFBMLModule: RNFBMLModule;
+  }
+}
