6f8e9630dfa06ceacb5f5f521c931754f73068a8
russellwheatley <russellwheatley85@gmail.com>
2026-04-13 13:06:57 +0100

    refactor(database): port leaf reference helpers to typescript

---
+++ packages/database/lib/DatabaseDataSnapshot.ts
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {
+  createDeprecationProxy,
+  isArray,
+  isFunction,
+  isNumber,
+  isObject,
+  isString,
+  MODULAR_DEPRECATION_ARG,
+} from '@react-native-firebase/app/dist/module/common';
+import { deepGet } from '@react-native-firebase/app/dist/module/common/deeps';
+
+import type { FirebaseDatabaseTypes } from './types/namespaced';
+
+type DatabaseSnapshotPriority = string | number | null;
+
+type DatabaseSnapshotChildPriorities = Record<string, string | number | null | undefined>;
+
+interface SerializedDatabaseSnapshotInternal {
+  value: unknown;
+  key: string | null;
+  exists: boolean;
+  childKeys: string[];
+  priority?: DatabaseSnapshotPriority;
+  childPriorities?: DatabaseSnapshotChildPriorities;
+}
+
+type ReferenceWithDeprecationArg = FirebaseDatabaseTypes.Reference & {
+  child(path: string, deprecationArg?: string): FirebaseDatabaseTypes.Reference;
+};
+
+function ap(reference: FirebaseDatabaseTypes.Reference): ReferenceWithDeprecationArg {
+  return reference as ReferenceWithDeprecationArg;
+}
+
+export default class DatabaseDataSnapshot implements FirebaseDatabaseTypes.DataSnapshot {
+  _snapshot: SerializedDatabaseSnapshotInternal;
+  _ref: FirebaseDatabaseTypes.Reference;
+
+  constructor(
+    reference: FirebaseDatabaseTypes.Reference,
+    snapshot: SerializedDatabaseSnapshotInternal,
+  ) {
+    this._snapshot = snapshot;
+
+    if (reference.key !== snapshot.key && isString(snapshot.key)) {
+      this._ref = ap(reference.ref).child.call(
+        reference.ref,
+        snapshot.key,
+        MODULAR_DEPRECATION_ARG,
+      );
+    } else {
+      this._ref = reference;
+    }
+  }
+
+  get key(): string | null {
+    return this._snapshot.key;
+  }
+
+  get ref(): FirebaseDatabaseTypes.Reference {
+    return this._ref;
+  }
+
+  child(path: string): FirebaseDatabaseTypes.DataSnapshot {
+    if (!isString(path)) {
+      throw new Error("snapshot().child(*) 'path' must be a string value");
+    }
+
+    const rootValue = this._snapshot.value as Record<string, unknown> | unknown[];
+    let value = deepGet(rootValue, path);
+
+    if (value === undefined) {
+      value = null;
+    }
+
+    const childRef = ap(this._ref).child.call(this._ref, path, MODULAR_DEPRECATION_ARG);
+
+    let childPriority: DatabaseSnapshotPriority = null;
+    if (this._snapshot.childPriorities) {
+      const childPriorityValue = this._snapshot.childPriorities[childRef.key as string];
+      if (isString(childPriorityValue) || isNumber(childPriorityValue)) {
+        childPriority = childPriorityValue;
+      }
+    }
+
+    return createDeprecationProxy(
+      new DatabaseDataSnapshot(childRef, {
+        value,
+        key: childRef.key,
+        exists: value !== null,
+        childKeys: isObject(value) ? Object.keys(value as Record<string, unknown>) : [],
+        priority: childPriority,
+      }),
+    ) as FirebaseDatabaseTypes.DataSnapshot;
+  }
+
+  exists(): boolean {
+    return this._snapshot.exists;
+  }
+
+  exportVal(): any {
+    let { value } = this._snapshot;
+
+    if (isObject(value) || isArray(value)) {
+      value = JSON.parse(JSON.stringify(value));
+    }
+
+    return {
+      '.value': value,
+      '.priority': this._snapshot.priority,
+    };
+  }
+
+  forEach(action: (child: FirebaseDatabaseTypes.DataSnapshot) => true | undefined): boolean {
+    if (!isFunction(action)) {
+      throw new Error("snapshot.forEach(*) 'action' must be a function.");
+    }
+
+    const iterate = action as (
+      child: FirebaseDatabaseTypes.DataSnapshot,
+      index?: number,
+    ) => true | undefined;
+
+    if (isArray(this._snapshot.value)) {
+      return this._snapshot.value.some(
+        (_value, i) => iterate(this.child(i.toString()), i) === true,
+      );
+    }
+
+    if (!this._snapshot.childKeys.length) {
+      return false;
+    }
+
+    let cancelled = false;
+
+    for (let i = 0; i < this._snapshot.childKeys.length; i++) {
+      const key = this._snapshot.childKeys[i];
+      if (key === undefined) {
+        continue;
+      }
+      const snapshot = this.child(key);
+      const actionReturn = iterate(snapshot, i);
+
+      if (actionReturn === true) {
+        cancelled = true;
+        break;
+      }
+    }
+
+    return cancelled;
+  }
+
+  getPriority(): string | number | null {
+    return this._snapshot.priority ?? null;
+  }
+
+  hasChild(path: string): boolean {
+    if (!isString(path)) {
+      throw new Error("snapshot.hasChild(*) 'path' must be a string value.");
+    }
+
+    const rootValue = this._snapshot.value as Record<string, unknown> | unknown[];
+    return deepGet(rootValue, path) !== undefined;
+  }
+
+  hasChildren(): boolean {
+    return this.numChildren() > 0;
+  }
+
+  numChildren(): number {
+    const { value } = this._snapshot;
+    if (isArray(value)) {
+      return value.length;
+    }
+    if (!isObject(value)) {
+      return 0;
+    }
+    return Object.keys(value as Record<string, unknown>).length;
+  }
+
+  toJSON(): object | null {
+    return this.val() as object | null;
+  }
+
+  val(): any {
+    const { value } = this._snapshot;
+
+    if (isObject(value) || isArray(value)) {
+      return JSON.parse(JSON.stringify(value));
+    }
+
+    return value;
+  }
+}
---
+++ packages/database/lib/DatabaseOnDisconnect.ts
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2016-present Invertase Limited & Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {
+  isFunction,
+  isNull,
+  isNumber,
+  isObject,
+  isString,
+  isUndefined,
+  isValidPath,
+  promiseWithOptionalCallback,
+} from '@react-native-firebase/app/dist/module/common';
+
+import type { DatabaseModuleInternal } from './types/internal';
+import type { FirebaseDatabaseTypes } from './types/namespaced';
+
+interface DatabaseOnDisconnectReferenceInternal {
+  readonly path: string;
+  readonly _database: DatabaseModuleInternal;
+}
+
+export default class DatabaseOnDisconnect implements FirebaseDatabaseTypes.OnDisconnect {
+  _ref: DatabaseOnDisconnectReferenceInternal;
+
+  constructor(reference: DatabaseOnDisconnectReferenceInternal) {
+    this._ref = reference;
+  }
+
+  cancel(onComplete?: (error: Error | null) => void): Promise<void> {
+    if (!isUndefined(onComplete) && !isFunction(onComplete)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().cancel(*) 'onComplete' must be a function if provided.",
+      );
+    }
+
+    return promiseWithOptionalCallback(
+      this._ref._database.native.onDisconnectCancel(this._ref.path),
+      onComplete,
+    );
+  }
+
+  remove(onComplete?: (error: Error | null) => void): Promise<void> {
+    if (!isUndefined(onComplete) && !isFunction(onComplete)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().remove(*) 'onComplete' must be a function if provided.",
+      );
+    }
+
+    return promiseWithOptionalCallback(
+      this._ref._database.native.onDisconnectRemove(this._ref.path),
+      onComplete,
+    );
+  }
+
+  set(value: any, onComplete?: (error: Error | null) => void): Promise<void> {
+    if (isUndefined(value)) {
+      throw new Error("firebase.database().ref().value(*) 'value' must be defined.");
+    }
+
+    if (!isUndefined(onComplete) && !isFunction(onComplete)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().set(_, *) 'onComplete' must be a function if provided.",
+      );
+    }
+
+    return promiseWithOptionalCallback(
+      this._ref._database.native.onDisconnectSet(this._ref.path, { value }),
+      onComplete,
+    );
+  }
+
+  setWithPriority(
+    value: any,
+    priority: string | number | null,
+    onComplete?: (error: Error | null) => void,
+  ): Promise<void> {
+    if (isUndefined(value)) {
+      throw new Error("firebase.database().ref().setWithPriority(*) 'value' must be defined.");
+    }
+
+    if (!isNumber(priority) && !isString(priority) && !isNull(priority)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().setWithPriority(_, *) 'priority' must be a number, string or null value.",
+      );
+    }
+
+    if (!isUndefined(onComplete) && !isFunction(onComplete)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().setWithPriority(_, _, *) 'onComplete' must be a function if provided.",
+      );
+    }
+
+    return promiseWithOptionalCallback(
+      this._ref._database.native.onDisconnectSetWithPriority(this._ref.path, { value, priority }),
+      onComplete,
+    );
+  }
+
+  update(
+    values: { [key: string]: any },
+    onComplete?: (error: Error | null) => void,
+  ): Promise<void> {
+    if (!isObject(values)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().update(*) 'values' must be an object.",
+      );
+    }
+
+    if (!Object.keys(values as Record<string, unknown>).length) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().update(*) 'values' must be an object containing multiple values.",
+      );
+    }
+
+    const keys = Object.keys(values);
+    for (let i = 0; i < keys.length; i++) {
+      if (!isValidPath(keys[i])) {
+        throw new Error(
+          'firebase.database().onDisconnect().update(*) \'values\' contains an invalid path. Paths must be non-empty strings and can\'t contain ".", "#", "$", "[", or "]"',
+        );
+      }
+    }
+
+    if (!isUndefined(onComplete) && !isFunction(onComplete)) {
+      throw new Error(
+        "firebase.database().ref().onDisconnect().update(_, *) 'onComplete' must be a function if provided.",
+      );
+    }
+
+    return promiseWithOptionalCallback(
+      this._ref._database.native.onDisconnectUpdate(this._ref.path, { values }),
+      onComplete,
+    );
+  }
+}
