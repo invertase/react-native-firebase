419bd46a03a2bfcbb4af8724d9d3f8d2babcd200
russellwheatley <russellwheatley85@gmail.com>
2026-05-13 11:11:39 +0100

    chore: remove duplicate methods

--- packages/analytics/plugin/src/ios/podfile.ts
+++ packages/analytics/plugin/src/ios/podfile.ts
@@ -12,43 +12,44 @@ const FLAG = '$RNFirebaseAnalyticsWithoutAdIdSupport = true';
 const TAG_ODM = '@react-native-firebase/analytics-googleAppMeasurementOnDeviceConversion';
 const FLAG_ODM = '$RNFirebaseAnalyticsGoogleAppMeasurementOnDeviceConversion = true';
 
-export function setAnalyticsPodfileWithoutAdIdSupport(
+function setAnalyticsPodfileFlag(
   src: string,
+  tag: string,
+  flag: string,
   enabled: boolean = false,
 ): string {
   if (!enabled) {
-    return removeGeneratedContents(src, TAG) ?? src;
+    return removeGeneratedContents(src, tag) ?? src;
   }
 
   return mergeContents({
     src,
-    newSrc: FLAG,
-    tag: TAG,
+    newSrc: flag,
+    tag,
     anchor: ANCHOR,
     offset: 1,
     comment: '#',
   }).contents;
+}
+
+export function setAnalyticsPodfileWithoutAdIdSupport(
+  src: string,
+  enabled: boolean = false,
+): string {
+  return setAnalyticsPodfileFlag(src, TAG, FLAG, enabled);
 }
 
 export function setAnalyticsPodfileGoogleAppMeasurementOnDeviceConversion(
   src: string,
   enabled: boolean = false,
 ): string {
-  if (!enabled) {
-    return removeGeneratedContents(src, TAG_ODM) ?? src;
-  }
-
-  return mergeContents({
-    src,
-    newSrc: FLAG_ODM,
-    tag: TAG_ODM,
-    anchor: ANCHOR,
-    offset: 1,
-    comment: '#',
-  }).contents;
+  return setAnalyticsPodfileFlag(src, TAG_ODM, FLAG_ODM, enabled);
 }
 
-export const withIosGoogleAppMeasurementOnDeviceConversion: ConfigPlugin<PluginConfigType> = (config, props) => {
+export const withIosGoogleAppMeasurementOnDeviceConversion: ConfigPlugin<PluginConfigType> = (
+  config,
+  props,
+) => {
   return withPodfile(config, config => {
     config.modResults.contents = setAnalyticsPodfileGoogleAppMeasurementOnDeviceConversion(
       config.modResults.contents,
