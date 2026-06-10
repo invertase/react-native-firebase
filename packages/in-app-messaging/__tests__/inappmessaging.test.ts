6d3c224749d84bb1c12d53acb1b02a7a00c44506
Mike Hardy <github@mikehardy.net>
2026-06-09 21:00:34 -0500

    test(in-app-messaging): add test coverage of property setters

--- packages/in-app-messaging/__tests__/inappmessaging.test.ts
+++ packages/in-app-messaging/__tests__/inappmessaging.test.ts
@@ -38,6 +38,31 @@ describe('in-app-messaging', function () {
   });
 
   describe('modular', function () {
+    beforeEach(async function () {
+      // @ts-ignore test
+      jest.spyOn(FirebaseModule.prototype, 'native', 'get').mockImplementation(() => {
+        return new Proxy(
+          {},
+          {
+            get: (_target, prop) => {
+              if (
+                prop === 'isMessagesDisplaySuppressed' ||
+                prop === 'isAutomaticDataCollectionEnabled'
+              ) {
+                return false;
+              }
+
+              return jest.fn().mockResolvedValue(null as never);
+            },
+          },
+        );
+      });
+
+      const inAppMessaging = getInAppMessaging();
+      await setMessagesDisplaySuppressed(inAppMessaging, false);
+      await setAutomaticDataCollectionEnabled(inAppMessaging, false);
+    });
+
     it('`getInAppMessaging` function is properly exposed to end user', function () {
       expect(getInAppMessaging).toBeDefined();
     });
@@ -60,6 +85,32 @@ describe('in-app-messaging', function () {
 
     it('`triggerEvent` function is properly exposed to end user', function () {
       expect(triggerEvent).toBeDefined();
+    });
+
+    it('updates isMessagesDisplaySuppressed synchronously after setMessagesDisplaySuppressed', function () {
+      const inAppMessaging = getInAppMessaging();
+      expect(isMessagesDisplaySuppressed(inAppMessaging)).toBe(false);
+      setMessagesDisplaySuppressed(inAppMessaging, true);
+      expect(isMessagesDisplaySuppressed(inAppMessaging)).toBe(true);
+    });
+
+    it('updates isMessagesDisplaySuppressed after setMessagesDisplaySuppressed resolves', async function () {
+      const inAppMessaging = getInAppMessaging();
+      await setMessagesDisplaySuppressed(inAppMessaging, true);
+      expect(isMessagesDisplaySuppressed(inAppMessaging)).toBe(true);
+    });
+
+    it('updates isAutomaticDataCollectionEnabled synchronously after setAutomaticDataCollectionEnabled', function () {
+      const inAppMessaging = getInAppMessaging();
+      expect(isAutomaticDataCollectionEnabled(inAppMessaging)).toBe(false);
+      setAutomaticDataCollectionEnabled(inAppMessaging, true);
+      expect(isAutomaticDataCollectionEnabled(inAppMessaging)).toBe(true);
+    });
+
+    it('updates isAutomaticDataCollectionEnabled after setAutomaticDataCollectionEnabled resolves', async function () {
+      const inAppMessaging = getInAppMessaging();
+      await setAutomaticDataCollectionEnabled(inAppMessaging, true);
+      expect(isAutomaticDataCollectionEnabled(inAppMessaging)).toBe(true);
     });
   });
 
