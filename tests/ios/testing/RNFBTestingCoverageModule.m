545ad54812acbcf13510b0071d96bed7b94c8927
Oskar Eichler <oskar@songstats.com>
2026-08-30 00:02:17 +0200

    fix(messaging, ios): preserve existing FIRMessaging delegate

--- tests/ios/testing/RNFBTestingCoverageModule.m
+++ tests/ios/testing/RNFBTestingCoverageModule.m
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import <FirebaseMessaging/FirebaseMessaging.h>
 #import <React/RCTBridgeModule.h>
 
 #import "RNFBTestingCoverageProfile.h"
@@ -18,6 +19,21 @@ RCT_EXPORT_METHOD(flush : (RCTPromiseResolveBlock)resolve rejecter : (RCTPromise
   } else {
     reject(@"coverage_flush_failed", @"Failed to write LLVM profile data", nil);
   }
+}
+
+RCT_EXPORT_METHOD(messagingPreservesExistingDelegate
+                  : (RCTPromiseResolveBlock)resolve rejecter
+                  : (RCTPromiseRejectBlock)reject)
+{
+  NSString *probeKey = @"rnfb_testing_messaging_delegate_called";
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  [defaults removeObjectForKey:probeKey];
+
+  id<FIRMessagingDelegate> delegate = [FIRMessaging messaging].delegate;
+  [delegate messaging:[FIRMessaging messaging] didReceiveRegistrationToken:@"rnfb-delegate-test"];
+
+  resolve(@([defaults boolForKey:probeKey]));
+  [defaults removeObjectForKey:probeKey];
 }
 
 @end
