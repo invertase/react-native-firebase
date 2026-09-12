bfa1dfe927ca71923a4764c24016c9ebec903e25
Davyd NRB <4661784+retyui@users.noreply.github.com>
2026-09-12 19:21:54 +0200

    Enhance fetchConfig with expiration duration parameter
    Added optional expiration duration parameter to fetchConfig function.

--- packages/remote-config/lib/index.ts
+++ packages/remote-config/lib/index.ts
@@ -576,8 +576,8 @@ export function fetchAndActivate(remoteConfig: RemoteConfig): Promise<boolean> {
 /**
  * Fetches and caches configuration from the Remote Config service.
  */
-export function fetchConfig(remoteConfig: RemoteConfig): Promise<void> {
-  return rc(remoteConfig).fetch();
+export function fetchConfig(remoteConfig: RemoteConfig, expirationDurationSeconds?: number): Promise<void> {
+  return rc(remoteConfig).fetch(expirationDurationSeconds);
 }
 
 /**
